7f3d9f13d449ceb3db7106dea439a8ce77f3596d
bonpra <benoit.prady@colombbus.org>
2018-04-10 23:13:43 +0200

    Set rootURL in production

--- config/environment.js
+++ config/environment.js
@@ -46,6 +46,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/editor2/';
   }
 
   return ENV;
