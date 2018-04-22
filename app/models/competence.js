d6ba10d30abc1ab6b036d9cb92778943802b5f32
bonpra <benoit.prady@colombbus.org>
2018-04-22 18:31:52 +0200

    competence model now handle challenge and skills ordering

--- app/models/competence.js
+++ app/models/competence.js
@@ -8,12 +8,14 @@ export default DS.Model.extend({
   init() {
     this._super(...arguments);
     this.skills = [];
+    this.workbenchSkills = [];
+    this.sortedChallenges = {production:[], workbench:[], noSkill:[]};
   },
-  area: DS.belongsTo('area'),
-  name: DS.attr('string', { readOnly: true }),
+  area: DS.belongsTo("area"),
+  name: DS.attr("string", { readOnly: true }),
   code: DS.attr(),
-  tubes:computed('skills', function() {
-    const skills = this.get('skills');
+  tubes:computed("skills", function() {
+    const skills = this.get("skills");
     let set = skills.reduce((current, skill) => {
       let result = findTubeName.exec(skill.get("name"));
       if (result && result[1] && result[2]) {
@@ -30,5 +32,91 @@ export default DS.Model.extend({
       current.push({name:key, skills:set[key]});
       return current;
     }, []);
+  }),
+  skillIds:computed("skills", function() {
+    return this.get("skills").reduce((current, skill) => {
+      current.push(skill.get("id"));
+      return current;
+    }, []);
+  }),
+  productionChallengeIds:computed("skills", function() {
+    return this.get("skills").reduce((current, skill) => {
+      return current.concat(skill.get("challengeIds"));
+    }, []);
+  }),
+  workbenchChallengeIds:computed("workbenchSkills", function() {
+    return this.get("workbenchSkills").reduce((current, skill) => {
+      return current.concat(skill.get("challengeIds"));
+    }, []);
+  }),
+
+  challenges:computed("skills", "workbenchSkills", "sortedChallenges", function() {
+    const productionChallenges = this.get("sortedChallenges.production");
+    const workbenchChallenges = this.get("sortedChallenges.workbench");
+    const noSkillChallenges = this.get("sortedChallenges.noSkill");
+    let skills = this.get("skills");
+    let orderedChallenges = productionChallenges.reduce((current, challenge) => {
+      current[challenge.get("id")] = challenge;
+      return current;
+    }, {});
+    let orderedWorkbenchChallenges = workbenchChallenges.reduce((current, challenge) => {
+      current[challenge.get("id")] = challenge;
+      return current;
+    }, {});
+    let workbenchChallengeIdsBySkill = this.get("workbenchSkills").reduce((current, workbenchSkill) => {
+      let challengeIds = workbenchSkill.get("challengeIds");
+      if (challengeIds) {
+        current[workbenchSkill.get("skillId")] = challengeIds;
+      }
+      return current;
+    }, {});
+
+    skills.forEach((skill) => {
+      let template = null;
+      let alternativeCount = 0;
+      let alternatives = [];
+      let ids = skill.get("challengeIds");
+      if (ids) {
+        let set = ids.reduce((current, id) => {
+          let challenge = orderedChallenges[id];
+          if (challenge) {
+            current.push(challenge);
+            if (challenge.get("template")) {
+              if (!template || challenge.get("validated")) {
+                template = challenge;
+              }
+            } else {
+              alternatives.push(challenge);
+              alternativeCount++;
+            }
+          }
+          return current;
+        }, []);
+        skill.set("challenges", set);
+        if (template) {
+          skill.set("template", template);
+        }
+        skill.set("alternativeCount", alternativeCount);
+        let workbenchAlternatives = [];
+        if (workbenchChallengeIdsBySkill[skill.get("id")]) {
+          workbenchAlternatives = workbenchChallengeIdsBySkill[skill.get("id")].reduce((current, id) => {
+            if (orderedWorkbenchChallenges[id]) {
+              current.push(orderedWorkbenchChallenges[id]);
+            }
+            return current;
+          }, []);
+          skill.set("workbenchChallenges", workbenchAlternatives);
+        }
+        if (template) {
+          template.set("orderedAlternatives", {production:alternatives, workbench:workbenchAlternatives});
+        }
+      }
+    })
+    return noSkillChallenges.reduce((current, value) => {
+      current.push(value);
+      return current;
+    }, Object.values(orderedChallenges).filter((value) => {
+      return value.get("template");
+    }));
   })
 });
--- app/routes/competence.js
+++ app/routes/competence.js
@@ -5,7 +5,7 @@ import {inject as service} from "@ember/service";
 export default Route.extend({
   paginatedQuery:service(),
   model(params) {
-    let competence, skills, workbenchChallengeIdsBySkill;
+    let competence;
     let store = this.get("store");
     return store.findRecord("competence", params.competence_id)
     .then(data => {
@@ -14,38 +14,19 @@ export default Route.extend({
       return store.query("skill", {filterByFormula:"FIND('"+competence.get("code")+"', {Compétence})", sort: [{field: "Nom", direction: "asc"}]});
     })
     .then(data => {
-      skills = data;
-      competence.set("skills", skills);
-      let skillIds = skills.reduce((current, skill) => {
-        current.push(skill.get("id"));
-        return current;
-      }, []);
-      let idsFormula = "OR({Acquis prod} = '"+skillIds.join("',{Acquis prod} ='")+"')";
+      competence.set("skills", data);
+      let idsFormula = "OR({Acquis prod} = '"+competence.get("skillIds").join("',{Acquis prod} ='")+"')";
       // get workbench skills
       return store.query("workbenchSkill", {filterByFormula:idsFormula});
     })
     .then(data => {
+      competence.set("workbenchSkills", data);
       // get challenges linked to skills
-      let challengeIds = skills.reduce((current, skill) => {
-        return current.concat(skill.get("challengeIds"));
-      }, []);
-      let recordsText = "OR(RECORD_ID() = '"+challengeIds.join("',RECORD_ID() ='")+"')";
+      let recordsText = "OR(RECORD_ID() = '"+competence.get("productionChallengeIds").join("',RECORD_ID() ='")+"')";
       let bySkillFilter = "AND("+recordsText+", Statut != 'archive')";
 
       // get challenges linked to workbench skills
-      workbenchChallengeIdsBySkill = data.reduce((current, workbenchSkill) => {
-        let challengeIds = workbenchSkill.get("challengeIds");
-        if (challengeIds) {
-          current[workbenchSkill.get("skillId")] = challengeIds;
-        }
-        return current;
-      }, {});
-
-      let workbenchChallengeIds = Object.values(workbenchChallengeIdsBySkill).reduce((current, ids) => {
-        return current.concat(ids);
-      }, []);
-
-      let workbenchRecordsText = "OR(RECORD_ID() = '"+workbenchChallengeIds.join("',RECORD_ID() ='")+"')";
+      let workbenchRecordsText = "OR(RECORD_ID() = '"+competence.get("workbenchChallengeIds").join("',RECORD_ID() ='")+"')";
       //TODO: replace Décliné 1 by generic "alternate" status
       let byWorkbenchSkillFilter = "AND("+workbenchRecordsText+", Statut != 'archive', {Généalogie} = 'Décliné 1')";
 
@@ -60,67 +41,11 @@ export default Route.extend({
       });
     })
     .then(data => {
-      const skillChallenges = data.bySkillProd;
-      const workbenchChallenges = data.bySkillWorkbench;
-      let orderedChallenges = skillChallenges.reduce((current, challenge) => {
-        current[challenge.get("id")] = challenge;
-        return current;
-      }, {});
-      let orderedWorkbenchChallenges = workbenchChallenges.reduce((current, challenge) => {
-        current[challenge.get("id")] = challenge;
-        return current;
-      }, {});
-      skills.forEach((skill) => {
-        let template = null;
-        let alternativeCount = 0;
-        let alternatives = [];
-        let ids = skill.get("challengeIds");
-        if (ids) {
-          let set = ids.reduce((current, id) => {
-            let challenge = orderedChallenges[id];
-            if (challenge) {
-              current.push(challenge);
-              if (challenge.get("template")) {
-                if (!template || challenge.get("validated")) {
-                  template = challenge;
-                }
-              } else {
-                alternatives.push(challenge);
-                alternativeCount++;
-              }
-            }
-            return current;
-          }, []);
-          skill.set("challenges", set);
-          if (template) {
-            skill.set("template", template);
-          }
-          skill.set('alternativeCount', alternativeCount);
-          let workbenchAlternatives = [];
-          if (workbenchChallengeIdsBySkill[skill.get("id")]) {
-            workbenchAlternatives = workbenchChallengeIdsBySkill[skill.get("id")].reduce((current, id) => {
-              if (orderedWorkbenchChallenges[id]) {
-                current.push(orderedWorkbenchChallenges[id]);
-              }
-              return current;
-            }, []);
-            skill.set("workbenchChallenges", workbenchAlternatives);
-          }
-          if (template) {
-            template.set("orderedAlternatives", {production:alternatives, workbench:workbenchAlternatives});
-          }
-        }
-      })
-      let challenges = data.byCompetenceProd.reduce((current, value) => {
-        current.push(value);
-        return current;
-      }, Object.values(orderedChallenges).filter((value) => {
-        return value.get("template");
-      }));
+      competence.set("sortedChallenges", {production:data.bySkillProd, workbench:data.bySkillWorkbench, noSkill: data.byCompetenceProd});
+
       return {
         competence:competence,
-        challenges:challenges,
-        workbenchChallenges:workbenchChallenges
+        challenges:competence.get("challenges")
       }
     });
   },
