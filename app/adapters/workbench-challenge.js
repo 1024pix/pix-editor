f4cfab1c4e2cd0ab30123a18ea97130c7d8b0845
Benoit <benoit.prady@pix.fr>
2018-04-30 15:52:08 +0200

    typo

--- app/adapters/workbench-challenge.js
+++ app/adapters/workbench-challenge.js
@@ -5,8 +5,8 @@ import {computed} from "@ember/object";
 export default ApplicationAdapter.extend({
   config:service(),
   namespace:computed("config.airtableWorkbenchBase", function() {
-  // API Version + Base ID
-  return "v0/"+this.get("config").get("airtableWorkbenchBase");
+    // API Version + Base ID
+    return "v0/"+this.get("config").get("airtableWorkbenchBase");
   }),
   pathForType() {
     return "Epreuves";
