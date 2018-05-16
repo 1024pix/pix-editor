437d184a5aceb8fcfa11c9d5efc6889bee4ad8df
Benoit <benoit.prady@pix.fr>
2018-05-16 16:55:38 +0200

    Changed root url in production

--- config/environment.js
+++ config/environment.js
@@ -46,7 +46,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/editor2/';
+    ENV.rootURL = '/';
     ENV.locationType = 'hash';
   }
 
