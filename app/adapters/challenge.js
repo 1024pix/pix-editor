3b7b57996155e1950a3efef11b1e1811a6f4cfce
bonpra <benoit.prady@colombbus.org>
2018-09-25 17:41:38 +0200

    remove todo

--- app/adapters/challenge.js
+++ app/adapters/challenge.js
@@ -1,7 +1,5 @@
 import ApplicationAdapter from './application';
 
-//TODO: handle airtable pagination (for more than 100 records)
-
 export default ApplicationAdapter.extend({
   pathForType() {
     return "Epreuves";
