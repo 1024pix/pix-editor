e4d1949e696eefa1c107264114076ba765e5057c
Benoit <benoit.prady@pix.fr>
2018-12-13 17:17:43 +0100

    Simplify search

--- app/components/main-sidebar.js
+++ app/components/main-sidebar.js
@@ -2,7 +2,6 @@ import Component from '@ember/component';
 import ENV from "pixeditor/config/environment";
 import {inject as service} from "@ember/service";
 import {computed} from "@ember/object";
-import RSVP from 'rsvp';
 
 export default Component.extend({
   tagName:"",
@@ -34,36 +33,28 @@ export default Component.extend({
     let query = settings.urlData.query;
     if (query.substr(0,1) === "@") {
       this.get("store").query("skill", {filterByFormula:"FIND('"+query+"', Nom)", maxRecords:20, sort: [{field: "Nom", direction: "asc"}]})
-      .then((result) => {
-        let skills = result.reduce((current, skill) => {
+      .then(skills => {
+        let results = skills.reduce((current, skill) => {
           let name = skill.get("name");
           current.push({title:name, url:this.get("router").urlFor("skill", name)});
           return current;
         }, []);
         callback({
           success:true,
-          results:skills
+          results:results
         });
       });
     } else {
-      RSVP.hash({
-        production:this.get("store").query("challenge", {filterByFormula:"AND(FIND('"+query+"', RECORD_ID()) , Statut != 'archive')", maxRecords:20}),
-        workbench:this.get("store").query("workbenchChallenge", {filterByFormula:"AND(FIND('"+query+"', RECORD_ID()) , Statut != 'archive')", maxRecords:20})
-      })
-      .then((result) => {
-        let challenges = result.production.reduce((current, challenge) => {
+      return this.get("store").query("challenge", {filterByFormula:"AND(FIND('"+query+"', RECORD_ID()) , Statut != 'archive')", maxRecords:20})
+      .then(challenges => {
+        const results = challenges.reduce((current, challenge) => {
           let id = challenge.get("id");
           current.push({title:id, url:this.get("router").urlFor("challenge", id)});
           return current;
         }, []);
-        challenges = result.workbench.reduce((current, challenge) => {
-          let id = challenge.get("id");
-          current.push({title:id, url:this.get("router").urlFor("challenge", id)});
-          return current;
-        }, challenges);
         callback({
           success:true,
-          results:challenges
+          results:results
         });
       });
     }
