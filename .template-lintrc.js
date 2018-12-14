70b39613499b54a7d15e4ecefcfd87d7dd384aad
Benoit <benoit.prady@pix.fr>
2018-12-14 18:25:20 +0100

    Deactivate some lint rules for now

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    "block-indentation":false,
+    "attribute-indentation":false,
+    "no-invalid-interactive":false,
+    "no-inline-styles":false,
+    "style-concatenation":false
+  }
 };
