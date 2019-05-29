4145c03ba837e6014628e9e9883167f4d000b950
Benoit <benoit.prady@pix.fr>
2019-05-29 15:43:53 +0200

    Add production skills and tubes count

--- app/models/competence.js
+++ app/models/competence.js
@@ -17,6 +17,22 @@ export default DS.Model.extend({
         })
     });
   }),
+  productionTubes:computed('rawTubes.[]', function() {
+    let allTubes;
+    return DS.PromiseArray.create({
+      promise:this.get('tubes')
+        .then(tubes => {
+          allTubes = tubes;
+          const testProduction = tubes.map(tube => tube.get('hasProductionChallenge'));
+          return Promise.all(testProduction);
+        })
+        .then(tests => {
+          return allTubes.filter((tube, index) => {
+            return tests[index];
+          });
+        })
+    });
+  }),
   sortedTubes:computed('tubes.[]', function() {
     return DS.PromiseArray.create({
       promise:this.get('tubes')
@@ -44,14 +60,31 @@ export default DS.Model.extend({
         .then(tubes => tubes.length)
     });
   }),
+  productionTubeCount:computed('tubes', function() {
+    return DS.PromiseObject.create({
+      promise:this.get('productionTubes')
+        .then(tubes => tubes.length)
+    });
+  }),
   skillCount:computed('tubes.@each.skillCount', function() {
     return DS.PromiseObject.create({
       promise:this.get('tubes')
         .then(tubes => {
-          let getCounts = tubes.reduce((promises, tube) => {
-            promises.push(tube.get('skillCount'));
-            return promises;
-          }, []);
+          const getCounts = tubes.map(tube => tube.get('skillCount'));
+          return Promise.all(getCounts);
+        })
+        .then(counts => {
+          return counts.reduce((count, value)=> {
+            return count+value;
+          },0);
+        })
+    });
+  }),
+  productionSkillCount:computed('tubes.@each.productionSkillCount', function() {
+    return DS.PromiseObject.create({
+      promise:this.get('tubes')
+        .then(tubes => {
+          const getCounts = tubes.map(tube => tube.get('productionSkillCount'));
           return Promise.all(getCounts);
         })
         .then(counts => {
