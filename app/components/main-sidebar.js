d7263c7d117c5f297b42edbe20c903faa4eab59f
bonpra <benoit.prady@colombbus.org>
2019-06-05 18:00:04 +0200

    plain text search

--- app/components/main-sidebar.js
+++ app/components/main-sidebar.js
@@ -1,21 +1,21 @@
 import Component from '@ember/component';
-import ENV from "pixeditor/config/environment";
-import {inject as service} from "@ember/service";
-import {alias} from "@ember/object/computed";
-import $ from "jquery";
+import ENV from 'pixeditor/config/environment';
+import {inject as service} from '@ember/service';
+import {alias} from '@ember/object/computed';
+import $ from 'jquery';
 
 export default Component.extend({
-  tagName:"",
+  tagName:'',
   version:ENV.APP.version,
   config:service(),
   store:service(),
   router:service(),
-  query:"",
+  query:'',
   init() {
     this._super(...arguments);
     let that = this;
     this.searchErrors = {
-      noResults: "Pas de résultat"
+      noResults: 'Pas de résultat'
     };
     this.searchAPISettings = {
       responseAsync: function(settings, callback) {
@@ -24,31 +24,32 @@ export default Component.extend({
     };
 
   },
-  searching:alias("searchResults.isPending"),
-  author:alias("config.author"),
+  searching:alias('searchResults.isPending'),
+  author:alias('config.author'),
   getSearchResults(settings, callback) {
     let query = settings.urlData.query;
-    if (query.substr(0,1) === "@") {
-      this.get("store").query("skill", {filterByFormula:"FIND('"+query+"', Nom)", maxRecords:20, sort: [{field: "Nom", direction: "asc"}]})
+    if (query.substr(0,1) === '@') {
+      this.get('store').query('skill', {filterByFormula:`FIND('${query}', Nom)`, maxRecords:20, sort: [{field: 'Nom', direction: 'asc'}]})
       .then(skills => {
-        let results = skills.reduce((current, skill) => {
-          let name = skill.get("name");
-          current.push({title:name, url:this.get("router").urlFor("skill", name)});
-          return current;
-        }, []);
+        const results = skills.map(skill => ({title:skill.get('name'), url:this.get('router').urlFor('skill', skill.get('name'))}));
+        callback({
+          success:true,
+          results:results
+        });
+      });
+    } else if (query.substr(0,3) === 'rec') {
+      this.get('store').query('challenge', {filterByFormula:`AND(FIND('${query}', RECORD_ID()) , Statut != 'archive')`, maxRecords:20})
+      .then(challenges => {
+        const results = challenges.map(challenge => ({title:challenge.get('id'), url:this.get('router').urlFor('challenge', challenge.get('id'))}));
         callback({
           success:true,
           results:results
         });
       });
     } else {
-      return this.get("store").query("challenge", {filterByFormula:`AND(FIND('${query}', RECORD_ID()) , Statut != 'archive')`, maxRecords:20})
+      this.get('store').query('challenge', {filterByFormula:`AND(FIND('${query.toLowerCase()}', LOWER(CONCATENATE(Consigne,Propositions,{Embed URL}))) , Statut != 'archive')`, maxRecords:20})
       .then(challenges => {
-        const results = challenges.reduce((current, challenge) => {
-          let id = challenge.get("id");
-          current.push({title:id, url:this.get("router").urlFor("challenge", id)});
-          return current;
-        }, []);
+        const results = challenges.map(challenge => ({title:challenge.get('instructions').substr(0,100), url:this.get('router').urlFor('challenge', challenge.get('id'))}));
         callback({
           success:true,
           results:results
