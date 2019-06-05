391fa243ba0bf8687b3b738831f81b35f9af7184
bonpra <benoit.prady@colombbus.org>
2019-06-05 17:38:05 +0200

    Search for a skill now displays corresponding template if in template mode

--- app/routes/competence/skill/index.js
+++ app/routes/competence/skill/index.js
@@ -4,13 +4,25 @@ export default Route.extend({
   model(params) {
     return this.get("store").findRecord("skill", params.skill_id);
   },
+  afterModel(model) {
+    const skillMode = this.controllerFor("competence").get("skillMode");
+    if (!skillMode) {
+      return model.get('productionTemplate')
+      .then(template => {
+        if (template) {
+          this.transitionTo("competence.templates.single", this.modelFor('competence'), template);
+        } else {
+          this.transitionTo("competence.templates.list", this.modelFor('competence'), model);
+        }
+      });
+    }
+  },
   setupController(controller) {
     this._super(...arguments);
     controller.set("maximized", false);
     controller.set("edition", false);
     controller.set("areas", this.modelFor('application'));
     controller.set("competence", this.modelFor('competence'));
-    this.controllerFor("competence").set("skillMode", true);
   },
   actions: {
     willTransition(transition) {
