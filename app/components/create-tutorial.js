69c6b03d30e8f8c4976892d2f91be166cf308dae
jeremie <jeremie.jade@pix.fr>
2019-07-24 14:24:03 +0200

    save in airetable

--- app/components/create-tutorial.js
+++ app/components/create-tutorial.js
@@ -1,18 +1,41 @@
 import PopinBase from "./popin-base";
+import {inject as service} from '@ember/service';
 
 
 export default PopinBase.extend({
-  isCrush:false,
+  isCrush: false,
+  edition:true,
+  isSaving: false,
+  options:{
+    'format':['Vidéo', 'site'],
+    'level':[1,2,3,4,5,6,7,8,9,10],
+    'tags':['t0', 't1', 't2']
+  },
+  store: service(),
+  item: {},
   willInitSemantic(settings) {
     this._super(...arguments);
     settings.closable = false;
   },
   actions: {
-    saveTutorial(){
-      console.log('saveTuto')
+
+    saveTutorial(item) {
+      console.log(item);
+      if(!this.isSaving){
+        this.set('isSaving', true);
+        let isCrush = this.get('isCrush');
+        const date = new Date();
+        item.date = `${date.getFullYear()} ${date.getMonth() + 1}`;
+        item.crush = isCrush?'yes':"";
+        this.store.createRecord('tutorial', item).save().then(() => {
+          alert('saved');
+          this.set('isSaving', false);
+        })
+      }
+
     },
-    toCrush(){
-     let isCrush = !this.get('isCrush');
+    toCrush() {
+      let isCrush = !this.get('isCrush');
       this.set('isCrush', isCrush);
     }
 
