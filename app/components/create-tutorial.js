ec7cfc6bdf0f08999960cca5189de108075a33ae
jeremie <jeremie.jade@pix.fr>
2019-07-24 16:41:36 +0200

    add loading and alert for saving tuto data

--- app/components/create-tutorial.js
+++ app/components/create-tutorial.js
@@ -5,10 +5,9 @@ import {inject as service} from '@ember/service';
 export default PopinBase.extend({
   isCrush: false,
   edition:true,
-  isSaving: false,
   options:{
     'format':['Vidéo', 'site'],
-    'level':[1,2,3,4,5,6,7,8,9,10],
+    'level':['1','2','3','4','5','6','7','8','9','10'],
     'tags':['t0', 't1', 't2']
   },
   store: service(),
@@ -20,20 +19,26 @@ export default PopinBase.extend({
   actions: {
 
     saveTutorial(item) {
-      console.log(item);
-      if(!this.isSaving){
-        this.set('isSaving', true);
-        let isCrush = this.get('isCrush');
+      this.get("application").send("isLoading");
+      let isCrush = this.get('isCrush');
         const date = new Date();
-        item.date = `${date.getFullYear()} ${date.getMonth() + 1}`;
+        item.date = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
         item.crush = isCrush?'yes':"";
-        this.store.createRecord('tutorial', item).save().then(() => {
-          alert('saved');
-          this.set('isSaving', false);
+        console.log(item);
+        this.store.createRecord('tutorial', item).save()
+          .then(() => {
+          this.get("application").send("finishedLoading");
+          this.get("application").send("showMessage", "Tutoriel créé", true);
+          this.set('item', {})
         })
+          .catch((error)=>{
+            console.error(error);
+            this.get("application").send("finishedLoading");
+            this.get("application").send("showMessage", "Erreur lors de la création de l'acquis", true);
+          })
       }
 
-    },
+    ,
     toCrush() {
       let isCrush = !this.get('isCrush');
       this.set('isCrush', isCrush);
