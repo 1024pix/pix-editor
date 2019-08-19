506794faf8b9941874d352064986d2eee99e9c45
jeremie <jeremie.jade@pix.fr>
2019-08-19 14:03:54 +0200

    tag in description for tuto search

--- app/components/form-tutorials.js
+++ app/components/form-tutorials.js
@@ -35,18 +35,21 @@ export default Component.extend({
           return Promise.all(titleLoad)
             .then(() => tutorials);
         })
-        .then((tutorials) =>  {
+        .then((tutorials) => {
 
-            const results = tutorials.map((tutorial) => {
-              return {title: tutorial.get('title'), description: `TAG : ${tutorial.get('tagsTitle').content}`, id: tutorial.get('id')}
-            });
-            results.push({title: 'Nouveau <i class="add icon"></i>', description: 'Ajouter un tutoriel', id: 'create'});
-            callback({
-              success: true,
-              results: results
-            });
-          }
-        )
+          const results = tutorials.map((tutorial) => {
+            return {
+              title: tutorial.get('title'),
+              description: `TAG : ${tutorial.get('tagsTitle').content}`,
+              id: tutorial.get('id')
+            }
+          });
+          results.push({title: 'Nouveau <i class="add icon"></i>', description: 'Ajouter un tutoriel', id: 'create'});
+          callback({
+            success: true,
+            results: results
+          });
+        })
     } else {
       this.get('store').query('tutorial', {
         filterByFormula: `FIND('${query}', Titre)`,
@@ -54,7 +57,21 @@ export default Component.extend({
         sort: [{field: 'Titre', direction: 'asc'}]
       })
         .then((tutorials) => {
-          const results = tutorials.map(tutorial => ({title: tutorial.get('title'), id: tutorial.get('id')}));
+          const titleLoad = tutorials.map(tutorial => tutorial.get('tagsTitle'));
+          return Promise.all(titleLoad)
+            .then(() => tutorials);
+        })
+        .then((tutorials) => {
+          const results = tutorials.map((tutorial) => {
+            const haveTags = tutorial.get('tagsTitle').content !== null && tutorial.get('tagsTitle').content !== '';
+            console.log(tutorial.get('tagsTitle').content !== null && tutorial.get('tagsTitle').content !== '',tutorial.get('tagsTitle'))
+
+            return {
+              title: tutorial.get('title'),
+              description: haveTags ? `TAG : ${tutorial.get('tagsTitle').content}` : '',
+              id: tutorial.get('id')
+            }
+          });
           results.push({title: 'Nouveau <i class="add icon"></i>', description: 'Ajouter un tutoriel', id: 'create'});
           callback({
             success: true,
