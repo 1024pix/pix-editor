c107009f65feb82417b048030ca6694a8ebb6b1c
jeremie <jeremie.jade@pix.fr>
2019-08-28 11:58:35 +0200

    show log in table

--- app/routes/events-log.js
+++ app/routes/events-log.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(params){
+    return this.get('store').query('note',{
+      maxRecords: 10,
+      sort: [{field: 'Date', direction: 'asc'}]
+    })
+  }
 });
