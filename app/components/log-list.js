a8051d82b2567e77fc2a0957274b7ddf685ff621
jeremie <jeremie.jade@pix.fr>
2019-08-30 10:57:59 +0200

    link log to skill or competence

--- app/components/log-list.js
+++ app/components/log-list.js
@@ -7,7 +7,7 @@ export default Component.extend({
   listType: "template-list",
   scrollLeft: 0,
   store: service(),
-  router:service(),
+  router: service(),
   init() {
     this._super();
     this.currentCount = 0;
@@ -33,24 +33,21 @@ export default Component.extend({
       this.set('scrollLeft', scrollLeft);
       this.set('scrollTop', scrollTop);
     },
-    linkToCompetence(competenceNumber, skillName) {
-      console.log(competenceNumber, skillName)
-      this.get('store').findAll('competence', {
-        // filterByFormula:`FIND('5.1', Sous-domaine)`,
-        fields: ['Sous-domaine']
-      }).then((competence) => {
-        const competenceId = competence.map(competence => {
-          return {code:competence.get('code'), id:competence.get('id')}
-        })
-          .filter(competence => {
-            return competence.code === competenceNumber
+    linkToCompetence(competenceNumber, skillId) {
+      this.get('store').query('competence', {
+        filterByFormula: `FIND('${competenceNumber}', {Sous-domaine})`,
+        // fields: ['Sous-domaine']
+      })
+        .then((competence) => {
+          const competenceId = competence.map(competence => {
+            return {code: competence.get('code'), id: competence.get('id')}
           });
-        if(!skillName){
-          this.get('router').transitionTo('competence', competenceId[0].id)
-        }else{
-
-        }
-      }).catch(e=>console.log(e));
+          if (!skillId) {
+            this.get('router').transitionTo('competence', competenceId[0].id)
+          } else {
+            this.get('router').transitionTo('competence.templates.single', competenceId[0].id, skillId)
+          }
+        }).catch(e => console.log(e));
     }
     // fetch(pageOffset, pageSize, stats) {
     //   // function which returns a "thenable" (*required*)
