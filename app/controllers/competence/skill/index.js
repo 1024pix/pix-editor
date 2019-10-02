1d3c8e688eb79b4f292f13809a1aad3adb54fcef
jeremie <jeremie.jade@pix.fr>
2019-10-02 12:52:41 +0200

    save&cancel challenge quality

--- app/controllers/competence/skill/index.js
+++ app/controllers/competence/skill/index.js
@@ -1,29 +1,29 @@
 import Controller from '@ember/controller';
-import { inject as controller } from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { alias } from '@ember/object/computed';
+import {inject as controller} from '@ember/controller';
+import {inject as service} from '@ember/service';
+import {alias} from '@ember/object/computed';
 import $ from "jquery";
-import { computed } from '@ember/object';
+import {computed} from '@ember/object';
 
 export default Controller.extend({
-  maximized:false,
-  parentController:controller("competence"),
-  application:controller(),
-  config:service(),
-  access:service(),
-  skill:alias("model"),
-  wasMaximized:false,
-  edition:false,
-  skillName:computed("skill", function() {
+  maximized: false,
+  parentController: controller("competence"),
+  application: controller(),
+  config: service(),
+  access: service(),
+  skill: alias("model"),
+  wasMaximized: false,
+  edition: false,
+  skillName: computed("skill", function () {
     return `${this.get('skill.id')} (${this.get('skill.name')})`;
   }),
-  mayEdit:computed("config.access", function() {
+  mayEdit: computed("config.access", function () {
     return this.get("access").mayEditSkills();
   }),
-  mayAccessAirtable:computed("config.access", function() {
+  mayAccessAirtable: computed("config.access", function () {
     return this.get("access").mayAccessAirtable();
   }),
-  mayMove:computed("config.access", "skill", "skill.productionTemplate", function() {
+  mayMove: computed("config.access", "skill", "skill.productionTemplate", function () {
     return this.get('access').mayMoveSkill(this.get('skill'));
   }),
   actions: {
@@ -41,14 +41,14 @@ export default Controller.extend({
     },
     preview() {
       this.get("skill.productionTemplate")
-      .then(challenge => {
-        window.open(challenge.get("preview"), challenge.get("id"));
-      });
+        .then(challenge => {
+          window.open(challenge.get("preview"), challenge.get("id"));
+        });
     },
     openAirtable() {
       let skill = this.get("skill");
       let config = this.get("config");
-      window.open(config.get("airtableUrl")+config.get("tableSkills")+"/"+skill.get("id"), "airtable");
+      window.open(config.get("airtableUrl") + config.get("tableSkills") + "/" + skill.get("id"), "airtable");
     },
     edit() {
       let state = this.get("maximized");
@@ -61,26 +61,40 @@ export default Controller.extend({
       this.set("edition", false);
       let skill = this.get("skill");
       skill.rollbackAttributes();
-      let previousState = this.get("wasMaximized");
-      if (!previousState) {
-        this.send("minimize");
-      }
-      this.get("application").send("showMessage", "Modification annulée", true);
+      this.get('skill.productionTemplate')
+        .then((challenge) => {
+          challenge.rollbackAttributes();
+          let previousState = this.get("wasMaximized");
+          if (!previousState) {
+            this.send("minimize");
+          }
+          this.get("application").send("showMessage", "Modification annulée", true);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.get("application").send("showMessage", "erreur", true);
+        });
     },
     save() {
       this.get("application").send("isLoading");
       let skill = this.get("skill");
-      skill.save()
-      .then(()=> {
-        this.set("edition", false);
-        this.get("application").send("finishedLoading");
-        this.get("application").send("showMessage", "Acquis mis à jour", true);
-      })
-      .catch((error) => {
-        console.error(error);
-        this.get("application").send("finishedLoading");
-        this.get("application").send("showMessage", "Erreur lors de la mise à jour de l'acquis", true);
-      });
+      this.get('skill.productionTemplate')
+        .then((challenge)=>{
+          challenge.save();
+        })
+        .then(()=>{
+          skill.save()
+        })
+        .then(() => {
+          this.set("edition", false);
+          this.get("application").send("finishedLoading");
+          this.get("application").send("showMessage", "Acquis mis à jour", true);
+        })
+        .catch((error) => {
+          console.error(error);
+          this.get("application").send("finishedLoading");
+          this.get("application").send("showMessage", "Erreur lors de la mise à jour de l'acquis", true);
+        });
     },
     moveSkill() {
       $('.skill-select-location').modal('show');
@@ -92,16 +106,16 @@ export default Controller.extend({
       skill.set('level', level);
       skill.set('competence', [competence.get('id')]);
       return skill.save()
-      .then(() => {
-        this.get("application").send("finishedLoading");
-        this.get("application").send("showMessage", "Acquis mis à jour", true);
-        this.transitionToRoute("competence.skill.index", competence, skill);
-      })
-      .catch((error) => {
-        console.error(error);
-        this.get("application").send("finishedLoading");
-        this.get("application").send("showMessage", "Erreur lors de la mise à jour de l'acquis", true);
-      });
+        .then(() => {
+          this.get("application").send("finishedLoading");
+          this.get("application").send("showMessage", "Acquis mis à jour", true);
+          this.transitionToRoute("competence.skill.index", competence, skill);
+        })
+        .catch((error) => {
+          console.error(error);
+          this.get("application").send("finishedLoading");
+          this.get("application").send("showMessage", "Erreur lors de la mise à jour de l'acquis", true);
+        });
     }
   }
 });
