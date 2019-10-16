5901432559269ac1afb9318454ec9a86f5fcb6b8
jeremie <jeremie.jade@pix.fr>
2019-10-16 11:58:19 +0200

    serialized new tube model fields

--- app/serializers/tube.js
+++ app/serializers/tube.js
@@ -3,7 +3,10 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
   attrs:{
     name:"Nom",
+    title:"Titre",
     description:"Description",
+    practicalTitle:"Titre pratique",
+    practicalDescription: "Description pratique",
     competence:"Competences",
     rawSkills:"Acquis"
   },
