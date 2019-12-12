6f64202dfdd1d11de2e60210cb47291a26275f08
Benoit <benoit.prady@pix.fr>
2019-12-12 17:56:40 +0100

    Fix competences sorting

--- app/components/target-profile/area-profile.js
+++ app/components/target-profile/area-profile.js
@@ -3,15 +3,15 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
 
-  filterCompetences:computed('area.competences.@each.selectedProductionTubeCount', 'filter', function(){
+  filterCompetences:computed('area.sortedCompetences.@each.selectedProductionTubeCount', 'filter', function(){
     const area = this.get('area');
     const filter = this.get('filter');
     if(filter){
-      return  area.get('competences').filter(competence=>{
+      return  area.get('sortedCompetences').filter(competence=>{
         return competence.selectedProductionTubeCount !== 0
       })
     }
-    return area.get('competences')
+    return area.get('sortedCompetences')
   })
 
 });
