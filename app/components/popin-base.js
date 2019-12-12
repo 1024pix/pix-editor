002c91c6c8a3ba2b44c822af3c6a7cc377757a5d
Benoit <benoit.prady@pix.fr>
2019-12-12 01:47:55 +0100

    remove element when popin is destroyed

--- app/components/popin-base.js
+++ app/components/popin-base.js
@@ -5,13 +5,14 @@ export default UiModal.extend({
   classNameBindings: ['class'],
   willInitSemantic(settings) {
     this._super(...arguments);
-    // remove any previously created modal with same class name
+    settings.detachable=true;
+  },
+  didDestroyElement() {
     try {
       $(`.${this.get('class')}`).remove();
     } catch(e)
     {
       console.log('could not remove element');
     }
-    settings.detachable = true;
   }
 });
