bf20b4243f16e7889837da2731a870e45eafa0ee
Benoit <benoit.prady@pix.fr>
2019-12-13 12:24:43 +0100

    Enhance redirection from skill to template and from template to skill

--- app/routes/competence/templates/list.js
+++ app/routes/competence/templates/list.js
@@ -2,13 +2,24 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.get("store").findRecord("skill", params.skill_id);
+    return this.get('store').findRecord('skill', params.skill_id);
   },
   setupController(controller) {
     this._super(...arguments);
-    controller.set("competence", this.modelFor("competence"));
+    controller.set('competence', this.modelFor('competence'));
     const competenceController = this.controllerFor('competence');
-    competenceController.set("view", "workbench");
-    competenceController.set("firstMaximized", false);
+    competenceController.set('section', 'challenges');
+    competenceController.set('view', 'workbench');
+    competenceController.set('firstMaximized', false);
+  },
+  actions: {
+    willTransition(transition) {
+      if (transition.targetName === 'competence.skills.index') {
+        return this.transitionTo('competence.skills.single', this.controllerFor('competence').get('model'), this.controller.get('model'));
+      } else if (transition.targetName === 'competence.quality.index' && this.controller.get('model.productionTemplate')) {
+        return this.transitionTo('competence.quality.single', this.controllerFor('competence').get('model'), this.controller.get('model'));
+      }
+      return true;
+    }
   }
 });
