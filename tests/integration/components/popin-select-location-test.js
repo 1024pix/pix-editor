9b524c6d012e99a7e9ad5a1193ffee8a9354545c
jeremie <jeremie.jade@pix.fr>
2020-01-31 18:30:36 +0100

    add jquery integration

--- tests/integration/components/popin-select-location-test.js
+++ tests/integration/components/popin-select-location-test.js
@@ -2,7 +2,6 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-//import $ from 'jquery';
 
 module('Integration | Component | popin-select-location', function(hooks) {
   setupRenderingTest(hooks);
