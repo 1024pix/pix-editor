4433fdf2ddaca4367d241170d19be119864d3106
Benoit <benoit.prady@pix.fr>
2020-02-06 22:08:54 +0100

    remove unused code

--- app/components/popin-changelog.js
+++ app/components/popin-changelog.js
@@ -1,13 +1,7 @@
 import classic from 'ember-classic-decorator';
-import { action } from '@ember/object';
 import Component from '@ember/component';
 
 @classic
 export default class PopinChangelog extends Component {
   value = "";
-
-  @action
-  confirm() {
-    this.get("approve")(this.get("value"));
-  }
 }
