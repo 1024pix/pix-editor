4afd0b238e6088da30f53ce090e40f91cfb72f60
Benoit <benoit.prady@pix.fr>
2020-02-07 11:29:35 +0100

    toggle checkbox when clicking on labels

--- app/components/custom-checkbox.js
+++ app/components/custom-checkbox.js
@@ -1,14 +1,16 @@
 import classic from 'ember-classic-decorator';
 import Component from '@ember/component';
+import {action} from '@ember/object';
 
 @classic
 export default class CustomCheckbox extends Component {
-  name = null;
-  label = null;
-  disabled = false;
-
   init() {
     super.init(...arguments);
     this.ignorableAttrs = ['checked', 'label', 'disabled'];
+  }
+
+  @action
+  toggle() {
+    this.set('checked', !this.get('checked'));
   }
 }
