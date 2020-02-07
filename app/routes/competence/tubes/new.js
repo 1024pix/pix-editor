b806ccdd30b6f76c93aa7fe65a92f35a92938998
Benoit <benoit.prady@pix.fr>
2020-02-07 11:05:01 +0100

    Use id generator when creating new tube

--- app/routes/competence/tubes/new.js
+++ app/routes/competence/tubes/new.js
@@ -1,12 +1,16 @@
 import classic from 'ember-classic-decorator';
 import Tube from './single';
+import { inject as service } from '@ember/service';
 
 @classic
 export default class NewRoute extends Tube {
   templateName = "competence/tubes/single";
 
+  @service
+  idGenerator;
+
   model() {
-    return this.get("store").createRecord("tube");
+    return this.get("store").createRecord("tube", {pixId:this.get('idGenerator').newId()});
   }
 
   setupController(controller) {
