b75f8b9fe020b2f5dca840e98ea9c34a17a7a4e3
Benoit <benoit.prady@pix.fr>
2020-02-24 12:33:42 +0100

    Rewrite competence-actions

--- app/components/competence/competence-actions.js
+++ app/components/competence/competence-actions.js
@@ -1,23 +1,14 @@
-import classic from 'ember-classic-decorator';
-import { classNames, classNameBindings } from '@ember-decorators/component';
-import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('ui', 'top', 'attached', 'borderless', 'labelled', 'icon', 'menu')
-@classNameBindings('hiddenClass', 'skillCkass')
 export default class CompetenceActions extends Component {
-  @service
-  config;
+  @service config;
 
-  @computed('hidden')
   get hiddenClass() {
-    return this.get('hidden')?'hidden':'';
+    return this.args.hidden?'hidden':'';
   }
 
-  @computed('view')
   get skillClass() {
-    return (this.get('view')==='skills')?'skill-mode':'';
+    return this.args.section==='skills'?'skill-mode':'';
   }
 }
