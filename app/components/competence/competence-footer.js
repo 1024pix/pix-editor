2733116c49ec137038df894ce83d50db8c3344fc
Benoit <benoit.prady@pix.fr>
2020-02-24 12:53:23 +0100

    Rewrite competence-footer

--- app/components/competence/competence-footer.js
+++ app/components/competence/competence-footer.js
@@ -1,52 +1,34 @@
-import classic from 'ember-classic-decorator';
-import { classNames, classNameBindings } from '@ember-decorators/component';
-import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('ui', 'borderless', 'bottom', 'attached', 'labelled', 'icon', 'menu')
-@classNameBindings('hiddenClass', 'skillClass')
 export default class CompetenceFooter extends Component {
-  @service
-  config;
 
-  @service
-  access;
+  @service access;
 
-  @computed('hidden')
   get hiddenClass() {
-    return this.get('hidden')?'hidden':'';
+    return this.args.hidden?' hidden ':'';
   }
 
-  @computed('section')
   get skillClass() {
-    return (this.get('section') === 'skills')?'skill-mode':'';
+    return this.args.section === 'skills'?' skill-mode ':'';
   }
 
-  @computed('section', 'view')
   get displayWorkbenchViews() {
-    const section = this.get('section');
-    const view = this.get('view');
-    return section === 'challenges' && view !== 'production';
+    return this.args.section === 'challenges' && this.args.view !== 'production';
   }
 
-  @computed('section', 'view')
   get displayProductionStats() {
-    const section = this.get('section');
-    const view = this.get('view');
-    return section === 'quality' || (section === 'challenges' && view === 'production');
+    const section = this.args.section;
+    return section === 'quality' || (section === 'challenges' && this.args.view === 'production');
   }
 
-  @computed('section', 'config.access')
   get mayCreateTube() {
-    return this.get('section') === 'skills' && this.get('access').mayCreateTube();
+    return this.args.section === 'skills' && this.access.mayCreateTube();
   }
 
-  @computed('section', 'view', 'config.access')
   get mayCreateTemplate() {
-    const section = this.get('section');
-    const view = this.get('view');
-    return section === 'challenges' && (view === 'workbench' || view === 'workbench-list') && this.get('access').mayCreateTemplate();
+    const section = this.args.section;
+    const view = this.args.view;
+    return section === 'challenges' && (view === 'workbench' || view === 'workbench-list') && this.access.mayCreateTemplate();
   }
 }
