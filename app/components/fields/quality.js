77ef8c6267b705cfb4b566271f12c5a666933c10
Benoit <benoit.prady@pix.fr>
2020-02-26 11:56:52 +0100

    Rewrite fields select and quality

--- app/components/fields/quality.js
+++ app/components/fields/quality.js
@@ -1,15 +1,11 @@
-import classic from 'ember-classic-decorator';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
+
 export default class Quality extends Component {
-  init() {
-    super.init(...arguments);
-    this.options = {
-      'accessibility1':['RAS','OK', 'Acquis Non Pertinent', 'KO', 'A tester'],
-      'accessibility2':['RAS','OK','KO'],
-      'responsive':['Tablette', 'Smartphone', 'Tablette/Smartphone', 'Non'],
-      'spoil':['Non Sp', 'Difficilement Sp', 'Facilement Sp']
-    }
+  options = {
+    'accessibility1':['RAS','OK', 'Acquis Non Pertinent', 'KO', 'A tester'],
+    'accessibility2':['RAS','OK','KO'],
+    'responsive':['Tablette', 'Smartphone', 'Tablette/Smartphone', 'Non'],
+    'spoil':['Non Sp', 'Difficilement Sp', 'Facilement Sp']
   }
 }
--- app/components/fields/select.js
+++ app/components/fields/select.js
@@ -1,9 +1,5 @@
-import classic from 'ember-classic-decorator';
-import { classNames } from '@ember-decorators/component';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('field')
 export default class Select extends Component {
   value = false;
 }
