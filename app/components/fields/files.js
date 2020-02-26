dcff97f123b92d48d7133f43117b6794eda1424f
Benoit <benoit.prady@pix.fr>
2020-02-26 11:03:47 +0100

    Rewrite file field

--- app/components/fields/files.js
+++ app/components/fields/files.js
@@ -1,32 +1,24 @@
-import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
-import { classNames } from '@ember-decorators/component';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('field')
 export default class Files extends Component {
-  @service
-  filePath;
+  @service filePath;
 
   @action
   remove(index) {
     // clone instead of removeAt, so that rollbackAttributes
     // may work
-    let list = this.get('value').slice();
+    let list = this.args.value.slice();
     list.splice(index, 1);
-    this.set('value', list);
+    this.args.setValue(list);
   }
 
   @action
   add(file) {
-    const value = this.get('value');
+    const value = this.args.value;
     let list = value?value.slice():[];
-    list.unshift({file:file, url:'', filename:file.get('name')});
-    this.set('value', list);
-    if (this.get('baseName') == null) {
-      this.set('baseName', this.get('filePath').getBaseName(file.get('name')));
-    }
+    list.unshift({file:file, url:'', filename:file.name});
+    this.args.setValue(list);
   }
 }
