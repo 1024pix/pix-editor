b9f89a0494c9896d0ca3d8286429e09d1a8f01a3
Benoit <benoit.prady@pix.fr>
2020-02-26 11:08:06 +0100

    Rewrite illustration field

--- app/components/fields/illustration.js
+++ app/components/fields/illustration.js
@@ -1,18 +1,14 @@
-import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
-import { classNames } from '@ember-decorators/component';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('field')
 export default class Illustration extends Component {
   @action
   remove() {
-    this.set('value', []);
+    this.args.setValue([]);
   }
 
   @action
   add(file) {
-    this.set('value', [{file:file}]);
+    this.args.setValue([{file:file}]);
   }
 }
