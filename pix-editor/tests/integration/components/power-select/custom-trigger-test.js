3735631fc71d3d2ba961674891791e43009eda67
celineung <celine.ung@zenika.com>
2020-02-27 10:42:20 +0100

    Move pix-editor in new directory

---
+++ pix-editor/tests/integration/components/power-select/custom-trigger-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | power-select-custom-trigger', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{power-select/custom-trigger}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    assert.dom('.search.icon.custom-trigger').exists();
+
+  });
+});
