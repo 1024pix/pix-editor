e19874405877971d715e24a16754b2822abebcd1
Benoit <benoit.prady@pix.fr>
2020-03-04 13:31:06 +0100

    rewrite route skill

--- app/routes/skill.js
+++ app/routes/skill.js
@@ -1,16 +1,14 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 
-@classic
 export default class SkillRoute extends Route {
   model(params) {
-    return this.get('store').query('skill', {filterByFormula:`FIND('${params.skill_name}', {Nom})`, maxRecords:1});
+    return this.store.query('skill', {filterByFormula:`FIND('${params.skill_name}', {Nom})`, maxRecords:1});
   }
 
   afterModel(model) {
-    if (model.get('length') > 0) {
-      let skill = model.get('firstObject');
-      this.transitionTo('competence.skills.single', skill.get('competence')[0], skill.get('id'));
+    if (model.length > 0) {
+      let skill = model.firstObject;
+      this.transitionTo('competence.skills.single', skill.competence[0], skill.id);
     } else {
       // redirect to home page
       this.transitionTo('index');
