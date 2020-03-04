fa3e140c085e286cc20f5abbd81ace3f1b0452f1
Benoit <benoit.prady@pix.fr>
2020-03-04 12:53:45 +0100

    Rewrite area-profile component

--- app/components/target-profile/area-profile.js
+++ app/components/target-profile/area-profile.js
@@ -1,18 +1,12 @@
-import classic from 'ember-classic-decorator';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
 export default class AreaProfile extends Component {
-  @computed('area.sortedCompetences.@each.selectedProductionTubeCount', 'filter')
-  get filterCompetences() {
-    const area = this.get('area');
-    const filter = this.get('filter');
-    if(filter){
-      return  area.get('sortedCompetences').filter(competence=>{
-        return competence.selectedProductionTubeCount !== 0
-      })
+
+  get filteredCompetences() {
+    const area = this.args.area;
+    if (this.args.filter) {
+      return  area.sortedCompetences.filter(competence => competence.selectedProductionTubeCount > 0)
     }
-    return area.get('sortedCompetences')
+    return area.sortedCompetences;
   }
 }
