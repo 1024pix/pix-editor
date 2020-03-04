7bc0ad67691eebbb4886b655a1629b724af29499
Benoit <benoit.prady@pix.fr>
2020-03-04 17:06:45 +0100

    rewrite tube controllers

--- app/controllers/competence/tubes/new.js
+++ app/controllers/competence/tubes/new.js
@@ -1,39 +1,37 @@
-import classic from 'ember-classic-decorator';
-import { action } from '@ember/object';
 import Tube from './single';
+import { action } from '@ember/object';
 
-@classic
 export default class NewController extends Tube {
   competence = null;
   creation = true;
 
   @action
   save() {
-    this.get('application').send('isLoading');
-    let tube = this.get('tube');
-    let competence = this.get('competence');
-    tube.set('competence', competence);
+    this.application.send('isLoading');
+    let tube = this.tube;
+    let competence = this.competence;
+    tube.competence = competence;
     return tube.save()
     .then(() => {
-      this.set('edition', false);
-      this.get('application').send('finishedLoading');
-      this.get('application').send('showMessage', 'Tube créé', true);
+      this.edition = false;
+      this.application.send('finishedLoading');
+      this.application.send('showMessage', 'Tube créé', true);
     })
     .then(() => {
       this.transitionToRoute('competence.tubes.single', competence, tube);
     })
     .catch((error) => {
       console.error(error);
-      this.get('application').send('finishedLoading');
-      this.get('application').send('showMessage', 'Erreur lors de la création du tube', true);
+      this.application.send('finishedLoading');
+      this.application.send('showMessage', 'Erreur lors de la création du tube', true);
     });
   }
 
   @action
   cancelEdit() {
-    this.get('store').deleteRecord(this.get('tube'));
-    this.set('edition', false);
-    this.get('application').send('showMessage', 'Création annulée', true);
-    this.get('parentController').send('closeChildComponent');
+    this.store.deleteRecord(this.tube);
+    this.edition = false;
+    this.application.send('showMessage', 'Création annulée', true);
+    this.parentController.send('closeChildComponent');
   }
 }
--- app/controllers/competence/tubes/single.js
+++ app/controllers/competence/tubes/single.js
@@ -1,20 +1,20 @@
-import classic from 'ember-classic-decorator';
-import { action, computed } from '@ember/object';
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
-import Controller from '@ember/controller';
 import { inject as controller } from '@ember/controller';
 import {scheduleOnce} from '@ember/runloop';
 import {tracked} from '@glimmer/tracking';
 
-@classic
 export default class SingleController extends Controller {
 
   @tracked edition = false;
+  @tracked displaySelectLocation = false;
+  @tracked areas = null;
+  @tracked competence = null;
 
   creation = false;
   wasMaximized = false;
-  displaySelectLocation = false;
 
   @alias('model')
   tube;
@@ -28,28 +28,19 @@ export default class SingleController extends Controller {
   @alias('parentController.firstMaximized')
   maximized;
 
-  areas = null;
-  competence = null;
+  @service access;
+  @service config;
 
-  @service
-  access;
-
-  @service
-  config;
-
-  @computed('config.access')
   get mayAccessAirtable() {
-    return this.get('access').mayAccessAirtable();
+    return this.access.mayAccessAirtable();
   }
 
-  @computed('config.access')
   get mayEdit() {
-    return this.get('access').mayEditSkills();
+    return this.access.mayEditSkills();
   }
 
-  @computed('config.access', 'tube', 'tube.hasProductionChallenge')
   get mayMove() {
-    return this.get('access').mayMoveTube(this.get('tube'));
+    return this.access.mayMoveTube(this.tube);
   }
 
   _scrollToTop() {
@@ -61,91 +52,88 @@ export default class SingleController extends Controller {
 
   @action
   maximize() {
-    this.set('maximized', true);
+    this.maximized = true;
   }
 
   @action
   minimize() {
-    this.set('maximized', false);
+    this.maximized = false;
   }
 
   @action
   close() {
-    this.set('maximized', false);
-    this.get('parentController').send('closeChildComponent');
+    this.maximized = false;
+    this.parentController.send('closeChildComponent');
   }
 
   @action
   edit() {
-    const state = this.get('maximized');
-    this.set('wasMaximized', state);
+    this.wasMaximized = this.maximized;
     this.send('maximize');
-    this.set('edition', true);
+    this.edition = true;
     scheduleOnce('afterRender', this, this._scrollToTop);
   }
 
   @action
   cancelEdit() {
-    this.set('edition', false);
-    let tube = this.get('tube');
+    this.edition = false;
+    let tube = this.tube;
     tube.rollbackAttributes();
-    let previousState = this.get('wasMaximized');
+    let previousState = this.wasMaximized;
     if (!previousState) {
       this.send('minimize');
     }
-    this.get('application').send('showMessage', 'Modification annulée', true);
+    this.application.send('showMessage', 'Modification annulée', true);
   }
 
   @action
   save() {
-    this.get('application').send('isLoading');
-    let tube = this.get('tube');
+    this.application.send('isLoading');
+    let tube = this.tube;
     return tube.save()
     .then(()=> {
-      this.set('edition', false);
-      this.get('application').send('finishedLoading');
-      this.get('application').send('showMessage', 'Tube mis à jour', true);
+      this.edition = false;
+      this.application.send('finishedLoading');
+      this.application.send('showMessage', 'Tube mis à jour', true);
       return tube.hasMany('rawSkills').reload();
     })
     .catch((error) => {
       console.error(error);
-      this.get('application').send('finishedLoading');
-      this.get('application').send('showMessage', 'Erreur lors de la mise à jour du tube', true);
+      this.application.send('finishedLoading');
+      this.application.send('showMessage', 'Erreur lors de la mise à jour du tube', true);
     });
   }
 
   @action
   openAirtable() {
-    let tube = this.get('tube');
-    let config = this.get('config');
-    window.open(`${config.get('airtableUrl')}${config.get('tableTubes')}/${tube.get('id')}`, 'airtable');
+    window.open(`${this.config.airtableUrl}${this.config.tableTubes}/${this.tube.id}`, 'airtable');
   }
 
   @action
   selectCompetence() {
-   this.set('displaySelectLocation', true);
+   this.displaySelectLocation = true;
   }
 
   @action
-  closeSelectLocation() {
-    this.set('displaySelectLocation', false);
+  closeSelectCompetence() {
+    this.displaySelectLocation = false;
   }
 
   @action
   setCompetence(newCompetence) {
-    let tube = this.get('tube');
-    this.get('application').send('isLoading');
-    tube.set('competence', newCompetence);
+    let tube = this.tube;
+    this.application.send('isLoading');
+    tube.competence = newCompetence;
     return tube.save()
     .then(() => {
-      this.get('application').send('finishedLoading');
-      this.get('application').send('showMessage', 'Tube mis à jour', true);
+      this.application.send('finishedLoading');
+      this.application.send('showMessage', 'Tube mis à jour', true);
       this.transitionToRoute('competence.tubes.single', newCompetence, tube);
     })
     .catch((error) => {
       console.error(error);
-      this.get('application').send('finishedLoading');
-      this.get('application').send('showMessage', 'Erreur lors de la mise à jour du tube', true);
+      this.application.send('finishedLoading');
+      this.application.send('showMessage', 'Erreur lors de la mise à jour du tube', true);
     });
   }
 }
