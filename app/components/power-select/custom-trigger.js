41c3efaaae8656fd9929492355ecf880ee89808f
Benoit <benoit.prady@pix.fr>
2020-03-04 13:17:37 +0100

    move custom-trigger into power-select directory

--- app/components/power-select/custom-trigger.js
+++ app/components/power-select/custom-trigger.js
@@ -1,4 +1,4 @@
 import classic from 'ember-classic-decorator';
 import TriggerComponent from 'ember-power-select/components/power-select/trigger';
 @classic
-export default class PowerSelectCustomTrigger extends TriggerComponent {}
+export default class CustomTrigger extends TriggerComponent {}
--- tests/integration/components/power-select-custom-trigger-test.js
+++ tests/integration/components/power-select-custom-trigger-test.js
@@ -10,7 +10,7 @@ module('Integration | Component | power-select-custom-trigger', function(hooks) 
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{power-select-custom-trigger}}`);
+    await render(hbs`{{power-select/custom-trigger}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
