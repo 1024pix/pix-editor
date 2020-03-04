2e1e5faa594f79eacba82c4c2b7eb49e09b566e1
Benoit <benoit.prady@pix.fr>
2020-03-04 13:29:45 +0100

    Rewrite route index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,3 @@
-import classic from 'ember-classic-decorator';
-import { classNames } from '@ember-decorators/component';
 import Route from '@ember/routing/route';
 
-@classic
-@classNames('index')
 export default class IndexRoute extends Route {}
