73f7999063ad963fc6a60d4e500814344ab3eac6
Benoit <benoit.prady@pix.fr>
2020-03-04 09:54:20 +0100

    Rewrite select location pop-in

--- app/components/pop-in/select-location.js
+++ app/components/pop-in/select-location.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
-import DS from 'ember-data';
 import { tracked } from '@glimmer/tracking';
 import { A } from '@ember/array';
 
@@ -10,6 +9,7 @@ export default class PopinSelectLocation extends Component {
   @tracked _selectedTube = null;
   @tracked tubesLoaded = false;
   @tracked levelsLoaded = false;
+  @tracked selectedLevels = null;
 
   _tubes = A([]);
   _levels = [];
@@ -53,7 +53,6 @@ export default class PopinSelectLocation extends Component {
       this._loadTubes();
       return A([]);
     } else {
-      console.log('loaded');
       return this.tubes.map(tube => ({
         label:tube.name,
         data:tube
@@ -63,10 +62,8 @@ export default class PopinSelectLocation extends Component {
 
   _loadTubes() {
     const competence = this.selectedCompetence.data;
-    console.log('ici');
     competence.get('rawTubes')
     .then(() => {
-      console.log('raw tubes loaded');
       this._tubes = competence.get('sortedTubes');
       this.tubesLoaded = true;
     });
@@ -76,8 +73,7 @@ export default class PopinSelectLocation extends Component {
     if (this._selectedTube) {
       return this._selectedTube;
     }
-    console.debug(this.tubeList, this.tubeList.find(item => (item.data == this.args.tube)));
-    return this.tubeList.find(item => (item.data == this.args.tube));
+    return this.tubeList.find(item => (item.data == this.args.tube.content));
   }
 
   set selectedTube(value) {
@@ -86,7 +82,7 @@ export default class PopinSelectLocation extends Component {
   }
 
   get levels() {
-    if (!this.levelsLoaded && this.tubesLoaded) {
+    if (!this.levelsLoaded && this.tubesLoaded && this.selectedTube) {
       const selectedTube = this.selectedTube.data;
       return selectedTube.rawSkills
       .then(() => {
@@ -111,19 +107,21 @@ export default class PopinSelectLocation extends Component {
   @action
   selectCompetence(item) {
     this.selectedCompetence = item;
+    this.selectedTube = null;
     this.tubesLoaded = false;
     this.levelsLoaded = false;
+    this.selectedLevels = null;
   }
 
   @action
   selectTube(item) {
     this.selectedTube = item;
     this.levelsLoaded = false;
+    this.selectedLevels = null;
   }
 
   @action
   selectLevels(levels) {
-    console.debug(levels);
     this.selectedLevels = levels;
   }
 
@@ -132,33 +130,26 @@ export default class PopinSelectLocation extends Component {
     this.args.close();
     const competence = this.selectedCompetence.data;
     if (this.args.selectTubeLevel) {
+      const tube = this.selectedTube.data;
       if (this.args.selectEmptyLevels) {
-        return this.get('selectedTube')
-          .then(tube => {
-            this.get('onChange')(competence, tube, this.get('selectedLevel'));
-            this._reset();
-          });
+        this.args.onChange(competence, tube, this.selectedLevels);
       } else {
         let levels;
         if (this.args.multipleLevel) {
-          levels = this.get('selectedLevel');
+          levels = this.selectedLevels;
         } else {
-          levels = [this.get('selectedLevel')];
+          levels = [this.selectedLevels];
         }
-        return this.get('selectedTube')
-          .then(tube => tube.get('filledSkills'))
-          .then(skills => {
-            let selectedSkills = levels.map(level => {
-              return skills[level - 1];
-            });
-            this.get('onChange')(selectedSkills);
-            this._reset();
-          });
+        const tubeSkills = this.selectedTube.data.filledSkills;
+        const selectedSkills = levels.map(level => {
+          return tubeSkills[level-1];
+        })
+        this.args.onChange(selectedSkills);
       }
     } else {
-      this.get('onChange')(competence);
-      this._reset();
+      this.args.onChange(competence);
     }
+    this._reset();
   }
 
   @action
@@ -170,6 +161,7 @@ export default class PopinSelectLocation extends Component {
   _reset() {
     this._selectedCompetence = null;
     this._selectedTube = null;
+    this.selectedLevels = null;
     this.tubesLoaded = false;
     this.levelsLoaded = false;
     this._tubes = A([]);
