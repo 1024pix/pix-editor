2a95239b63630d69531d7dd4f5aa474832a54f22
Benoit <benoit.prady@pix.fr>
2020-03-05 12:07:48 +0100

    Simplify quality controller

--- app/controllers/competence/quality/single.js
+++ app/controllers/competence/quality/single.js
@@ -1,10 +1,4 @@
 import Skill from '../skills/single';
-import { action } from '@ember/object';
 
 export default class SingleController extends Skill {
-  @action
-  close() {
-    this.maximized = false;
-    this.transitionToRoute('competence.quality', this.competence);
-  }
 }
