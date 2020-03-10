0b8c49bca7e5e3d70f9242abb06e0a8e7408dc5a
Benoit <benoit.prady@pix.fr>
2020-03-10 18:13:42 +0100

    Add route competence index

---
+++ app/routes/competence/index.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class CompetenceIndexRoute extends Route {
+  @service currentData;
+
+  afterModel() {
+    super.afterModel(...arguments);
+    this.currentData.setTemplate(null);
+  }
+
+}
