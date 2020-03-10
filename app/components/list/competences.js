85af2ddc0c03a4b1c7581713ae5ec88285751904
Benoit <benoit.prady@pix.fr>
2020-03-10 18:14:50 +0100

    Use Ember Table for competence list

--- app/components/list/competences.js
+++ app/components/list/competences.js
@@ -1,29 +1,65 @@
 import { action } from '@ember/object';
 import SortedList from './sorted';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class CompetencesList extends SortedList {
   listType = 'template-list';
-  scrollLeft = 0;
-  currentCount = 0;
-  scrollValue = 0;
+  @service router;
+  @service currentData;
 
-  get scrollTop() {
-    let count = this.args.itemCount;
-    if (count !== this.currentCount) {
-      this.currentCount = count;
-      return 0;
-    }
-    return this.scrollValue;
-  }
+  @tracked sorts = [];
 
-  set scrollTop(value) {
-    this.scrollValue = value;
-    return value;
+  headers = [{
+    name:'Auteur',
+    valuePath:'authorText',
+    className:'authorText',
+    maxWidth:150
+  },{
+    name:'Consigne',
+    valuePath:'instructions',
+    className:'instructions'
+  },{
+    name:'Type',
+    valuePath:'type',
+    className:'type',
+    maxWidth:150
+  },{
+    name:'Statut',
+    valuePath:'status',
+    className:'status',
+    maxWidth:150
+  }];
+
+  sortTypes = {
+    'authorText':'string',
+    'instructions':'string',
+    'type':'string',
+    'status':'string'
   }
 
   @action
-  scrollChange(scrollLeft, scrollTop) {
-    this.scrollLeft = scrollLeft;
-    this.scrollTop = scrollTop;
+  selectRow(row) {
+    this.router.transitionTo(this.args.link, this.args.competenceModel, row);
+  }
+
+  get current(){
+    return this.currentData.getTemplate();
+  }
+
+  @action
+  testSort(params) {
+    if (params.length > 0) {
+      const field = params[0].valuePath;
+      this.sortField = field;
+      this.ascending = params[0].isAscending;
+      this.sorts = params;
+    } else {
+      this.ascending = !this.ascending;
+      this.sorts = [{
+        valuePath:this.sortField,
+        isAscending:this.ascending
+      }]
+    }
   }
 }
