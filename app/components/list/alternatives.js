04db65e7da98331d537aa4c7725b67b3cadbe4ac
Benoit <benoit.prady@pix.fr>
2020-03-10 19:00:15 +0100

    Use Ember Table for alternatives list

--- app/components/list/alternatives.js
+++ app/components/list/alternatives.js
@@ -1,10 +1,31 @@
-import SortedList from './sorted';
+import SortedList from './competences';
+import { action } from '@ember/object';
 
 export default class AlternativesList extends SortedList {
+
   listType = 'alternative-list';
 
-  get hiddenClass() {
-    return this.args.hidden?' hidden ':'';
+  headers = [{
+    name:'Indice',
+    valuePath:'alternativeVersion',
+    maxWidth:150,
+  },{
+    name:'Consigne',
+    valuePath:'instructions',
+  },{
+    name:'Auteur',
+    valuePath:'author',
+    maxWidth:150,
+  },{
+    name:'Statut',
+    valuePath:'status',
+    maxWidth:150,
+    style:true
+  }];
+
+  @action
+  selectRow(row) {
+    this.router.transitionTo('competence.templates.single.alternatives.single', row);
   }
 
 }
