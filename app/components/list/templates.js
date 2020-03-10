13b4cd2ceb598a710d8ad582463880414865ee71
Benoit <benoit.prady@pix.fr>
2020-03-10 18:51:39 +0100

    Use EmberTable for template list

--- app/components/list/templates.js
+++ app/components/list/templates.js
@@ -1,10 +1,37 @@
-import SortedList from './sorted';
+import CompetenceList from './competences';
+import { action } from '@ember/object';
 
-export default class TemplatesList extends SortedList {
+export default class TemplatesList extends CompetenceList {
   listType = 'template-list';
 
-  get hiddenClass() {
-    return this.args.hidden?' hidden ':'';
+  headers = [{
+    name:'Version',
+    valuePath:'version',
+    maxWidth:150,
+  },{
+    name:'Consigne',
+    valuePath:'instructions',
+  },{
+    name:'Auteur',
+    valuePath:'author',
+    maxWidth:150,
+  },{
+    name:'Statut',
+    valuePath:'status',
+    maxWidth:150,
+    style:true
+  }];
+
+  sortTypes = {
+    'Version':'string',
+    'instructions':'string',
+    'type':'string',
+    'status':'string'
+  }
+
+  @action
+  selectRow(row) {
+    this.router.transitionTo('competence.templates.single', row);
   }
 
 }
