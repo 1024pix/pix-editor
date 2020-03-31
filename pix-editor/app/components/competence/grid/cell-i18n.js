3b494aa7a09283fe2323e145608c0fae0c6c6cef
jeremie_jade <jeremie.jade@pix.fr>
2020-03-31 18:28:19 +0200

    display language flags on i18n cell

--- pix-editor/app/components/competence/grid/cell-i18n.js
+++ pix-editor/app/components/competence/grid/cell-i18n.js
@@ -1,4 +1,39 @@
 import Component from '@glimmer/component';
 
 export default class CompetenceGridCellI18nComponent extends Component {
+
+
+  get skillFlags(){
+    const languages = this._skillLanguages;
+   return  languages.reduce((flags, language)=>{
+      return [...flags, this._flagByLanguage(language)]
+    },[])
+  }
+
+  get _skillLanguages() {
+    const alternatives = this.args.skill.alternatives;
+    return alternatives.reduce((languages, alternative) => {
+      if (!alternative.language) {
+        return languages;
+      }
+      return languages.includes(alternative.language) ? languages : [...languages, alternative.language];
+    }, []);
+  }
+
+  _flagByLanguage(language) {
+    switch (language) {
+      case "Francophone" :
+        return "fr";
+      case "Franco Français":
+        return "fr fr-fr";
+      case "Anglais":
+        return "gb uk";
+      case "Espagnol":
+        return "es";
+      case "Italie":
+        return "it";
+      case "Allemand":
+        return "de";
+    }
+  }
 }
