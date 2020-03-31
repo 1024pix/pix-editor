70274edc4183ed49b1fbe354c58052180f795b3d
jeremie_jade <jeremie.jade@pix.fr>
2020-03-31 17:03:49 +0200

    I18n section display only skills with productionTemplate

--- pix-editor/app/components/competence/competence-grid.js
+++ pix-editor/app/components/competence/competence-grid.js
@@ -2,8 +2,6 @@ import Component from '@glimmer/component';
 
 export default class CompetenceGrid extends Component {
   get displayAllTubes() {
-    return this.args.section === 'skills'
-           || this.args.section === 'i18n'
-           || this.args.view === 'workbench';
+    return this.args.section === 'skills' || this.args.view === 'workbench';
   }
 }
--- pix-editor/app/components/competence/grid/grid-cell.js
+++ pix-editor/app/components/competence/grid/grid-cell.js
@@ -36,7 +36,7 @@ export default class GridCell extends Component {
         }
         break;
       case 'i18n' :
-        if (skill) {
+        if (skill  && skill.productionTemplate) {
           return 'i18n';
         }
         break;
