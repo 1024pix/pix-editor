bc5703903a542c25b421c48145898e9be9f8ab28
jeremie_jade <jeremie.jade@pix.fr>
2020-03-31 18:29:29 +0200

    details skill i18n

--- pix-editor/app/controllers/competence/i18n/single.js
+++ pix-editor/app/controllers/competence/i18n/single.js
@@ -1,10 +1,9 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
 import {alias} from '@ember/object/computed';
 import {inject as controller} from '@ember/controller';
 
 export default class CompetenceI18nSingleController extends Controller {
-
-  wasMaximized = false;
 
   @controller('competence')
   parentController;
@@ -14,4 +13,19 @@ export default class CompetenceI18nSingleController extends Controller {
 
   @alias('model')
   skill;
+
+  @action
+  maximize() {
+    this.parentController.maximizeLeft(true);
+  }
+
+  @action
+  minimize() {
+    this.parentController.maximizeLeft(false);
+  }
+
+  @action
+  close() {
+    this.parentController.send('closeChildComponent');
+  }
 }
