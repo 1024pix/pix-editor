1b6ac3341279f26ef964ccad166945052c6194d6
jeremie_jade <jeremie.jade@pix.fr>
2020-04-03 16:24:26 +0200

    display all template in production for one skill in i18n section

--- pix-editor/app/controllers/competence/i18n/single.js
+++ pix-editor/app/controllers/competence/i18n/single.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {alias} from '@ember/object/computed';
 import {inject as controller} from '@ember/controller';
+import { htmlSafe } from '@ember/template';
+
 
 export default class CompetenceI18nSingleController extends Controller {
 
@@ -13,6 +15,10 @@ export default class CompetenceI18nSingleController extends Controller {
 
   @alias('model')
   skill;
+
+  get safeHelpContent() {
+    return htmlSafe(this.args.helpContent);
+  }
 
   @action
   maximize() {
@@ -28,4 +34,5 @@ export default class CompetenceI18nSingleController extends Controller {
   close() {
     this.parentController.send('closeChildComponent');
   }
+
 }
