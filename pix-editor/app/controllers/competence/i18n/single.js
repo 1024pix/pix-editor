6d1284c136f35c885e74a3c7051ed65ca5e087c4
jeremie_jade <jeremie.jade@pix.fr>
2020-04-15 11:38:40 +0200

    Remove useless declaration of safeHelpContent helper

--- pix-editor/app/controllers/competence/i18n/single.js
+++ pix-editor/app/controllers/competence/i18n/single.js
@@ -2,8 +2,6 @@ import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {alias} from '@ember/object/computed';
 import {inject as controller} from '@ember/controller';
-import { htmlSafe } from '@ember/template';
-
 
 export default class CompetenceI18nSingleController extends Controller {
 
@@ -15,10 +13,6 @@ export default class CompetenceI18nSingleController extends Controller {
 
   @alias('model')
   skill;
-
-  get safeHelpContent() {
-    return htmlSafe(this.args.helpContent);
-  }
 
   @action
   maximize() {
