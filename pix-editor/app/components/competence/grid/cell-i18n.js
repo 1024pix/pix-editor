44e09fd18f47ac53711a4293e337ac54b0103af4
jeremie_jade <jeremie.jade@pix.fr>
2020-04-16 11:59:17 +0200

    change skillFlags to skillFlagsAndLanguages to add title on flag icon

--- pix-editor/app/components/competence/grid/cell-i18n.js
+++ pix-editor/app/components/competence/grid/cell-i18n.js
@@ -2,28 +2,24 @@ import Component from '@glimmer/component';
 
 export default class CompetenceGridCellI18nComponent extends Component {
 
-
-  get skillFlags() {
+  get skillFlagsAndLanguages() {
     const languages = this._skillLanguages.sort();
-    return languages.map(language => this._flagByLanguage(language));
+    return languages.map(language => {
+      return {language, flag: this._flagByLanguage(language)}
+    });
   }
 
   get _skillLanguages() {
     const templates = this.args.skill.productionTemplates;
     return templates.reduce((languages, template) => {
-      if (!template.language) {
-        return languages;
+      if (template.languages && template.languages.length !== 0) {
+        template.languages.forEach(language => {
+          if (!languages.includes(language)) {
+            languages.push(language);
+          }
+        });
       }
-      //todo when alternative.language is alternative.languages
-      // if (alternative.languages || alternative.languages.length !== 0) {
-      //   alternative.languages.forEach(language=>{
-      //     if(!languages.includes(language)){
-      //       languages.push(language);
-      //     }
-      //   });
-      // }
-      // return languages;
-      return languages.includes(template.language) ? languages : [...languages, template.language];
+      return languages;
     }, []);
   }
 
