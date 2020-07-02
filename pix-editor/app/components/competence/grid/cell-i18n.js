2e10d369ab242036c4d7384886a3429817d5d0f2
jeremie_jade <jeremie.jade@pix.fr>
2020-07-02 18:14:54 +0200

    Refacto get skillLanguage

--- pix-editor/app/components/competence/grid/cell-i18n.js
+++ pix-editor/app/components/competence/grid/cell-i18n.js
@@ -14,14 +14,18 @@ export default class CompetenceGridCellI18nComponent extends Component {
   get _skillLanguages() {
     const challenges = this.args.skill.productionChallenges;
     return challenges.reduce((languages, challenge) => {
-      if (challenge.languages && challenge.languages.length !== 0) {
-        challenge.languages.forEach(language => {
-          if (!languages.includes(language)) {
-            languages.push(language);
-          }
-        });
-      }
-      return languages;
+      return this._extractLanguagesFromChallenge(challenge.languages,languages);
     }, []);
+  }
+
+  _extractLanguagesFromChallenge(challengeLanguages, extractedLanguages){
+    if (challengeLanguages && challengeLanguages.length !== 0) {
+      challengeLanguages.forEach(language => {
+        if (!extractedLanguages.includes(language)) {
+          extractedLanguages.push(language);
+        }
+      });
+    }
+    return extractedLanguages;
   }
 }
