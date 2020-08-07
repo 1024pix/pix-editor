faf2c9059d1079b6d3485c1a0697f5a0986961e6
Benoit <benoit.prady@pix.fr>
2020-08-07 10:30:33 +0200

    Handle size and closing of archive challenge

--- pix-editor/app/controllers/competence/skills/single/archive.js
+++ pix-editor/app/controllers/competence/skills/single/archive.js
@@ -1,12 +1,16 @@
 import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CompetenceSkillsSingleArchiveController extends Controller {
   @alias('model')
   skill;
 
   @service router;
+
+  @tracked rightMaximized = false;
 
   get size() {
     if (this.router.currentRouteName == 'competence.skills.single.archive.index') {
@@ -37,6 +41,18 @@ export default class CompetenceSkillsSingleArchiveController extends Controller 
 
   get prototypesCount() {
     return this.skill.challenges.filter(challenge => challenge.isPrototype).length;
+  }
+
+  maximizeRight(value) {
+    if (this.rightMaximized != value) {
+      this.rightMaximized = value;
+    }
+  }
+
+  @action
+  closeChildComponent() {
+    this.maximizeRight(false);
+    this.transitionToRoute('competence.skills.single.archive');
   }
 
 }
