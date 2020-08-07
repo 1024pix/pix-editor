ed4384a451449f7a3d7fac1cdfd1e2130d1b58ec
Benoit <benoit.prady@pix.fr>
2020-08-07 10:26:19 +0200

    Simplify single archive controller

--- pix-editor/app/controllers/competence/skills/single/archive/single.js
+++ pix-editor/app/controllers/competence/skills/single/archive/single.js
@@ -1,13 +1,10 @@
 import PrototypeController from '../../../prototypes/single';
 import { action } from '@ember/object';
-import { scheduleOnce } from '@ember/runloop';
 import { inject as controller } from '@ember/controller';
 import { alias } from '@ember/object/computed';
 
 export default class SingleController extends PrototypeController {
 
-  popinImageClass = 'archive-popin-image';
-  copyZoneId = 'copyZoneArchive';
   elementClass = 'archive-challenge';
 
   @controller('competence.skills.single.archive')
@@ -17,45 +14,11 @@ export default class SingleController extends PrototypeController {
   maximized;
 
   get challengeTitle() {
-    if (this.creation) {
-      return 'Nouvelle déclinaison';
+    if (this.challenge.isPrototype) {
+      return 'Prototype n°' + this.challenge.version;
     } else {
-      const index = this.challenge.alternativeVersion;
-      return 'Déclinaison n°' + index;
+      return `Version n°${this.challenge.version} - Déclinaison n°${this.challenge.alternativeVersion}`;
     }
-  }
-
-  _executeCopy() {
-    const element = document.getElementById(this.copyZoneId);
-    element.select();
-    try {
-      var successful = document.execCommand('copy');
-      if (!successful) {
-        this.notify.error('Erreur lors de la copie');
-      } else {
-        this.notify.message('lien copié');
-      }
-    } catch (err) {
-      this.notify.error('Erreur lors de la copie');
-    }
-    this.set('copyOperation', false);
-  }
-
-  @action
-  preview() {
-    const challenge = this.challenge;
-    window.open(challenge.preview, challenge.id);
-  }
-
-  @action
-  openAirtable() {
-    window.open(this.config.airtableUrl + this.config.tableChallenges + '/' + this.challenge.id, 'airtable');
-  }
-
-  @action
-  copyLink() {
-    this.copyOperation = true;
-    scheduleOnce('afterRender', this, this._executeCopy);
   }
 
   @action
