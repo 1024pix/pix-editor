4752f3717ff8b790a7cf2ad3dd16f0280923351c
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-10-05 17:18:43 +0200

    Add acceptance tests for airtable proxy route

---
+++ api/tests/acceptance/application/airtable-proxy-controller_test.js
@@ -0,0 +1,43 @@
+const nock = require('nock');
+const { expect, databaseBuilder, generateAuthorizationHeader } = require('../../test-helper');
+const createServer = require('../../../server');
+
+describe('Acceptance | Controller | airtable-proxy-controller', () => {
+
+  describe('GET /api/airtable/content/Competences', () => {
+    context('nominal case', () => {
+      let user;
+      beforeEach(async function() {
+        user = databaseBuilder.factory.buildUser({ name: 'User', trigram: 'ABC', access: 'admin', apiKey:'11b2cab8-050e-4165-8064-29a1e58d8997' });
+        await databaseBuilder.commit();
+        nock('https://api.airtable.com')
+          .get('/v0/test-base/Competences?key=value')
+          .reply(200, 'ok');
+      });
+
+      afterEach(function() {
+        nock.cleanAll();
+      });
+
+      it('should proxy request to airtable', async () => {
+        // Given
+        const server = await createServer();
+
+        // When
+        const response = await server.inject({
+          method: 'GET',
+          url: '/api/airtable/content/Competences?key=value',
+          headers: generateAuthorizationHeader(user)
+        });
+
+        // Then
+        expect(response.statusCode).to.equal(200);
+        expect(response.result).to.equal('ok');
+      });
+
+    });
+
+  });
+
+});
+
