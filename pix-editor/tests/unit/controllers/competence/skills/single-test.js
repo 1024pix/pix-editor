0db674b638efa9d4b93c03827fe344843a54403a
jeremie <jeremie.jade@pix.fr>
2020-10-05 12:30:42 +0200

    Add test for changelog

--- pix-editor/tests/unit/controllers/competence/skills/single-test.js
+++ pix-editor/tests/unit/controllers/competence/skills/single-test.js
@@ -1,12 +1,65 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Sinon from 'sinon';
 
-module('Unit | Controller | competence/skills/single', function(hooks) {
+module('Unit | Controller | competence/skills/single', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:competence/skills/single');
-    assert.ok(controller);
+  let storeStub, controller;
+
+  hooks.beforeEach(function() {
+    controller = this.owner.lookup('controller:competence/skills/single');
+
+    const note = { save: Sinon.stub().resolves() };
+    storeStub = { createRecord: Sinon.stub().returns(note) };
+    controller.store = storeStub;
+  });
+
+  test('it should create a skill changelogEntry', async function (assert) {
+    //given
+    const changelogValue = 'Acquis modifié';
+    const skill = {
+      pixId: 'rec123456',
+      name: 'skillName',
+    };
+
+    const expectedChangelog = {
+      author: Sinon.match.any,
+      text: changelogValue,
+      recordId: skill.pixId,
+      createdAt: Sinon.match.any,
+      elementType: 'acquis'
+    };
+
+    //when
+    await controller._handleSkillChangelog(skill, changelogValue);
+
+    //then
+    Sinon.assert.calledWithMatch(storeStub.createRecord, 'changelogEntry', expectedChangelog);
+    assert.expect(0);
+  });
+
+  test('it should create a challenge changelogEntry', async function (assert) {
+    //given
+    const changelogValue = 'Epreuve modifié';
+    const challenge = {
+      pixId: 'rec123456',
+      name: 'challengeName',
+    };
+
+    const expectedChangelog = {
+      author: Sinon.match.any,
+      text: changelogValue,
+      recordId: challenge.pixId,
+      createdAt: Sinon.match.any,
+      elementType: 'épreuve'
+    };
+
+    //when
+    await controller._handleChallengeChangelog(challenge, changelogValue);
+
+    //then
+    Sinon.assert.calledWithMatch(storeStub.createRecord, 'changelogEntry', expectedChangelog);
+    assert.expect(0);
   });
 });
