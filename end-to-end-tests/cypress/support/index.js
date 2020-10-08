279a243e9ecfc688d4117b60bfa8d7350f4a769d
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-10-08 08:22:18 +0200

    Add end-to-end tests for challenge modification

--- end-to-end-tests/cypress/support/index.js
+++ end-to-end-tests/cypress/support/index.js
@@ -16,5 +16,13 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  if (err.errors[0].code === 401) {
+    // We do not want to fail the test when authentication failed
+    return false;
+  }
+  return true;
+});
+
