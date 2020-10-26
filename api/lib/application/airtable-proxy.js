e167186fa192fcd5b7693892ea50c41dbc158978
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-10-26 15:14:17 +0100

    Add a route to proxy to airtable with `v0` prefix in the url.
    This route is the exact same as the route without `v0` prefix
    This is needed by pix-api that uses the official `airtable` client
    This client forces the use of version in the url

--- api/lib/application/airtable-proxy.js
+++ api/lib/application/airtable-proxy.js
@@ -9,16 +9,26 @@ exports.register = async function(server) {
       path: '/api/airtable/content/{path*}',
       config: {
         handler: async function(request, h) {
-          return _proxyRequestToAirtable(request,h, config.airtable.base); 
+          return _proxyRequestToAirtable(request, h, config.airtable.base);
         }
       },
-    },
-    {
+    }, {
+      // This route is the exact same as the above route but it adds a `v0` prefix
+      // This is needed by pix-api that uses the official `airtable` client
+      // This client forces the use of version in the url
+      method: 'GET',
+      path: '/api/airtable/v0/content/{path*}',
+      config: {
+        handler: async function(request, h) {
+          return _proxyRequestToAirtable(request, h, config.airtable.base);
+        }
+      },
+    }, {
       method: ['GET', 'POST', 'PATCH', 'DELETE'],
       path: '/api/airtable/changelog/{path*}',
       config: {
         handler: async function(request, h) {
-          return _proxyRequestToAirtable(request,h, config.airtable.editorBase);
+          return _proxyRequestToAirtable(request, h, config.airtable.editorBase);
         }
       },
     }
@@ -29,7 +39,7 @@ exports.name = 'airtable-proxy';
 
 async function _proxyRequestToAirtable(request, h, airtableBase) {
   const response = await axios.request(`${AIRTABLE_BASE_URL}/${airtableBase}/${request.params.path}`,
-    { headers: { 'Authorization': `Bearer ${config.airtable.apiKey}`, 'Content-Type': 'application/json' }, 
+    { headers: { 'Authorization': `Bearer ${config.airtable.apiKey}`, 'Content-Type': 'application/json' },
       params: request.query,
       method: request.method,
       data: request.payload,
