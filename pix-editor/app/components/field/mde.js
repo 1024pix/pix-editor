7c023a2471d71a07f1c819ceb5f64a4d2c2b1447
jeremie <jeremie.jade@pix.fr>
2020-10-28 16:46:47 +0100

    Change markdown-it to showdown library

--- pix-editor/app/components/field/mde.js
+++ pix-editor/app/components/field/mde.js
@@ -1,17 +1,10 @@
 import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/template';
-import MarkdownIt from 'markdown-it';
 
 export default class Mde extends Component {
 
   get safeHelpContent() {
     return htmlSafe(this.args.helpContent);
-  }
-
-  get previewContent() {
-    const markdown = new MarkdownIt({ html: true });
-    const preview = markdown.render(this.args.value);
-    return htmlSafe(preview);
   }
 
 }
