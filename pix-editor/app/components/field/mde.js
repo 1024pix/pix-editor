3b1f90d3ad7e946d53e1b6044ff2bba9fa252d68
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-10-28 16:26:56 +0100

    WIP [skip-ci]: Run `ember-cli-update --to=3.22`
    Markdown editor (simplemde) is no more maintained, try to use TOAST UI

--- pix-editor/app/components/field/mde.js
+++ pix-editor/app/components/field/mde.js
@@ -1,16 +1,17 @@
 import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/template';
+import MarkdownIt from 'markdown-it';
 
 export default class Mde extends Component {
 
-  simpleMDEOptions = {
-    status:false,
-    spellChecker:false,
-    hideIcons: ['heading', 'image', 'guide', 'side-by-side']
-  };
-
   get safeHelpContent() {
     return htmlSafe(this.args.helpContent);
+  }
+
+  get previewContent() {
+    const markdown = new MarkdownIt({ html: true });
+    const preview = markdown.render(this.args.value);
+    return htmlSafe(preview);
   }
 
 }
--- pix-editor/tests/integration/components/field/mde-test.js
+++ pix-editor/tests/integration/components/field/mde-test.js
@@ -9,8 +9,8 @@ module('Integration | Component | form-mde.hbs', function(hooks) {
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{field/mde}}`);
+    this.value = 'some text';
+    await render(hbs`<Field::mde @value=this.value />`);
 
     assert.dom('.field').exists();
 
