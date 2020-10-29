2f35133d063772a84986993c61936cf39efb749b
Thomasevano <thomas-evano@hotmail.fr>
2020-10-29 15:15:06 +0100

    Remove `ember-classic-decorator`

--- pix-editor/app/components/power-select/custom-trigger.js
+++ pix-editor/app/components/power-select/custom-trigger.js
@@ -1,4 +1,2 @@
-import classic from 'ember-classic-decorator';
-import TriggerComponent from 'ember-power-select/components/power-select/trigger';
-@classic
-export default class CustomTrigger extends TriggerComponent {}
+import Component from '@glimmer/component';
+export default class CustomTrigger extends Component {}
