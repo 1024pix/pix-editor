dc20a416dd490295dad1226b98c4936357ac0c69
jeremie <jeremie.jade@pix.fr>
2020-11-10 16:21:55 +0100

    Add test

---
+++ pix-editor/tests/integration/components/target-profile/threshold-row-test.js
@@ -0,0 +1,86 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | target-profile/threshold-row', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should render a row with a total skill count by level and a threshold', async function(assert) {
+    //given
+    const selectedSkillsLevel1 = [
+      {
+        id: 'rec123456',
+        name: 'tube1SelectedSkill1',
+        isActive: true,
+        level: 1
+      }, {
+        id: 'rec223457',
+        name: 'tube2SelectedSkill1',
+        isActive: true,
+        level: 1
+      }, {
+        id: 'rec423451',
+        name: 'tube2SelectedSkill1',
+        isActive: true,
+        level: 1
+      }
+    ];
+    const selectedSkillsOtherLevel = [
+      {
+        id: 'rec123457',
+        name: 'tube1SelectedSkill2',
+        isActive: true,
+        level: 2
+      }, {
+        id: 'rec123458',
+        name: 'tube1SelectedSkill3',
+        isActive: true,
+        level: 3
+      }, {
+        id: 'rec223456',
+        name: 'tube2SelectedSkill4',
+        isActive: true,
+        level: 4
+      }, {
+        id: 'rec223458',
+        name: 'tube2SelectedSkill6',
+        isActive: true,
+        level: 6
+      }, {
+        id: 'rec323456',
+        name: 'tube2SelectedSkill7',
+        isActive: true,
+        level: 7
+      }, {
+        id: 'rec323457',
+        name: 'tube2SelectedSkill8',
+        isActive: true,
+        level: 8
+      }, {
+        id: 'rec423456',
+        name: 'tube2SelectedSkill7',
+        isActive: true,
+        level: 7
+      }, {
+        id: 'rec423457',
+        name: 'tube2SelectedSkill8',
+        isActive: true,
+        level: 8
+      }
+    ];
+
+    const expectedCountResult = '3';
+    const expectedThresholdResult = '27%';
+
+    this.selectedSkills = [...selectedSkillsLevel1, ...selectedSkillsOtherLevel];
+    this.level = 1;
+
+    //when
+    await render(hbs`<TargetProfile::ThresholdRow @level={{this.level}} @selectedSkills={{this.selectedSkills}}/>`);
+
+    //then
+    assert.equal(this.element.querySelector('[data-test-skill-count]').textContent.trim(), expectedCountResult);
+    assert.equal(this.element.querySelector('[data-test-threshold]').textContent.trim(), expectedThresholdResult);
+  });
+});
