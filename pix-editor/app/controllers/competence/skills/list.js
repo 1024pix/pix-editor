c69a992ccf29245bcab09945bc5bab9061db61c6
jeremie <jeremie.jade@pix.fr>
2020-12-01 16:44:33 +0100

    Design skill list

--- pix-editor/app/controllers/competence/skills/list.js
+++ pix-editor/app/controllers/competence/skills/list.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { alias } from '@ember/object/computed';
 import { inject as controller } from '@ember/controller';
 import { inject as service } from '@ember/service';
 
@@ -8,9 +7,6 @@ export default class CompetenceHistoryListController extends Controller {
 
   @controller('competence')
   parentController;
-
-  @alias('parentController.leftMaximized')
-  isMaximized;
 
   @service access;
   @service currentData;
@@ -28,16 +24,6 @@ export default class CompetenceHistoryListController extends Controller {
     const tube = this.model.tube;
     const level = parseInt(this.firstSkill.level) - 1;
     this.transitionToRoute('competence.skills.new', this.currentData.getCompetence(), tube.id, level);
-  }
-
-  @action
-  maximize() {
-    this.parentController.maximizeLeft(true);
-  }
-
-  @action
-  minimize() {
-    this.parentController.maximizeLeft(false);
   }
 
   @action
