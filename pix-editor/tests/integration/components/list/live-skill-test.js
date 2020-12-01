109cffa733465b093a9b5a40851f4709c8a7a93f
jeremie <jeremie.jade@pix.fr>
2020-12-01 17:24:02 +0100

    Add test

---
+++ pix-editor/tests/integration/components/list/live-skill-test.js
@@ -0,0 +1,43 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | list/live-skill', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('should display a list of skills', async function(assert) {
+    // given
+    const challenge1  = {
+      id: 'recChallenge1'
+    };
+    const challenge2  = {
+      id: 'recChallenge2'
+    };
+    const challenge3  = {
+      id: 'recChallenge3'
+    };
+    const skill1 = {
+      id: 'recSkill1',
+      date: '14/07/1986 à 11:20',
+      challenges: [challenge1, challenge2],
+      status: 'en construction',
+      statusCSS: 'suggested',
+    };
+    const skill2 = {
+      id: 'recSkill2',
+      date: '11/01/2020 à 11:20',
+      challenges: [challenge3],
+      status: 'actif',
+      statusCSS: 'validated',
+    };
+    this.skills = [skill2, skill1];
+
+    // when
+    await render(hbs`<List::LiveSkill @list={{this.skills}}/>`);
+
+    // then
+    assert.equal(this.element.querySelectorAll('[data-test-live-skill-row]').length, this.skills.length);
+
+  });
+});
