7f74e8716aff52dcefff1168f528f531c62342fc
jeremie <jeremie.jade@pix.fr>
2020-12-10 12:16:30 +0100

    Unbind isThematicResult from model

--- pix-editor/app/components/pop-in/tube-thematic-result-level.js
+++ pix-editor/app/components/pop-in/tube-thematic-result-level.js
@@ -4,15 +4,15 @@ import { action } from '@ember/object';
 
 export default class PopinThematicResultTubeLevel extends Component {
 
-  get skills() {
+  get skillsAndThematicResult() {
     const tube = this.args.tube;
     const selectedTubeLevel = tube.selectedLevel;
     const skills = tube.productionSkills;
     return skills.reduce((orderedSkills, skill) => {
       const level = skill.level;
       if (level <= selectedTubeLevel) {
-        skill._isThematicResult = this._isSelectedThematicResultSkill(level);
-        orderedSkills[level - 1] = skill;
+        const isThematicResult = this._isSelectedThematicResultSkill(level);
+        orderedSkills[level - 1] = {skill, isThematicResult};
       }
       return orderedSkills;
     }, [null, null, null, null, null, null, null, null]);
