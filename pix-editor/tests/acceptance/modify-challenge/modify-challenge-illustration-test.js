75957c15472526c829b96528a8de454c0d32e3c1
Jonas Grillères <grilleres.j@gmail.com>
2021-01-21 16:53:55 +0100

    Cleanup acceptance tests modify-challenge

--- pix-editor/tests/acceptance/modify-challenge/modify-challenge-illustration-test.js
+++ pix-editor/tests/acceptance/modify-challenge/modify-challenge-illustration-test.js
@@ -7,7 +7,7 @@ import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { selectFiles } from 'ember-file-upload/test-support';
 import Service from '@ember/service';
 
-module('Acceptance | Modify-Challenge', function(hooks) {
+module('Acceptance | Modify-Challenge-Illustration', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
   let apiKey;
@@ -27,24 +27,6 @@ module('Acceptance | Modify-Challenge', function(hooks) {
     this.server.create('competence', { id: 'recCompetence2.1', pixId: 'pixId recCompetence2.1', rawTubeIds: ['recTube2'] });
     this.server.create('area', { id: 'recArea1', name: '1. Information et données', code: '1', competenceIds: ['recCompetence1.1'] });
     this.server.create('area', { id: 'recArea2', name: '2. Communication et collaboration', code: '2', competenceIds: ['recCompetence2.1'] });
-  });
-
-  test('visiting /', async function(assert) {
-    // when
-    await visit('/');
-    await click(findAll('[data-test-area-item]')[0]);
-    await click(findAll('[data-test-competence-item]')[0]);
-    await click(findAll('[data-test-skill-cell]')[0]);
-    await click(find('[data-test-modify-challenge-button]'));
-    // Ugly hack to wait for ToastUI to be ready
-    // otherwise test is flacky and fails with error message
-    // Attempted to access the computed <pixeditor@component:tui-editor::ember393>.options on a destroyed object, which is not allowed
-    await later(this, async () => {}, 200);
-    await click(find('[data-test-save-challenge-button]'));
-    await click(find('[data-test-save-changelog-button]'));
-
-    // then
-    assert.dom('[data-test-main-message]').hasText('Épreuve mise à jour');
   });
 
   test('adding illustration', async function(assert) {
@@ -163,103 +145,5 @@ module('Acceptance | Modify-Challenge', function(hooks) {
     assert.equal(newIllustration.url, 'data:,');
   });
 
-  test('adding attachments', async function(assert) {
-    // given
-    class StorageServiceStub extends Service {
-      uploadFile() {}
-    }
-
-    this.owner.register('service:storage', StorageServiceStub);
-    const storageServiceStub = this.owner.lookup('service:storage');
-    sinon.stub(storageServiceStub, 'uploadFile').resolves({ url: 'data:,', filename: 'attachment-name' });
-
-    // when
-    await visit('/');
-    await click(findAll('[data-test-area-item]')[0]);
-    await click(findAll('[data-test-competence-item]')[0]);
-    await click(findAll('[data-test-skill-cell]')[0]);
-    await click(find('[data-test-modify-challenge-button]'));
-
-    const file = new File([], 'challenge-illustration.png', { type: 'image/png' });
-    await selectFiles('[data-test-file-input-attachment] input', file);
-
-    await later(this, async () => {}, 400);
-    await click(find('[data-test-save-challenge-button]'));
-    await click(find('[data-test-save-changelog-button]'));
-
-    const store = this.owner.lookup('service:store');
-    const attachments = await store.peekAll('attachment');
-
-    // then
-    assert.dom('[data-test-main-message]').hasText('Épreuve mise à jour');
-    assert.ok(storageServiceStub.uploadFile.calledOnce);
-    assert.ok(attachments.every(record => !record.isNew));
-    assert.equal(attachments.length, 1);
-  });
-
-  test('delete attachment', async function(assert) {
-    // given
-    this.server.create('challenge', {
-      id: 'recChallenge2',
-      attachments: [{
-        'id': 'attd74YR8ga7IOfWp',
-        'url': 'https://dl.airtable.com/.attachments/b60304a44214d5b6f94d63df59d3516a/d1f1b65b/attachment.png',
-        'filename': 'attachment.png',
-        'size': 178629,
-        'type': 'image/png'
-      }],
-      filesIds: ['recAttachment1'],
-    });
-    this.server.create('attachment', { id: 'recAttachment1', type: 'attachment', challengeId: 'recChallenge2', filename: 'attachment.png' });
-
-    // when
-    await visit('/competence/recCompetence2.1/prototypes/recChallenge2');
-    await click(find('[data-test-modify-challenge-button]'));
-    await click(find('[data-test-delete-attachment-button]'));
-
-    await later(this, async () => {}, 200);
-    await click(find('[data-test-save-challenge-button]'));
-    await click(find('[data-test-save-changelog-button]'));
-
-    const store = this.owner.lookup('service:store');
-    const attachments = await store.peekAll('attachment');
-
-    // then
-    assert.dom('[data-test-main-message]').hasText('Épreuve mise à jour');
-    assert.equal(attachments.length, 0);
-    assert.ok(attachments.every(record => !record.isDeleted));
-  });
-
-  test('cancel adding an attachment', async function(assert) {
-    // given
-    this.server.create('challenge', {
-      id: 'recChallenge2',
-      attachments: [{
-        'id': 'attd74YR8ga7IOfWp',
-        'url': 'https://dl.airtable.com/.attachments/b60304a44214d5b6f94d63df59d3516a/d1f1b65b/attachment.png',
-        'filename': 'attachment.png',
-        'size': 178629,
-        'type': 'image/png'
-      }],
-      filesIds: ['recAttachment1'],
-    });
-    this.server.create('attachment', { id: 'recAttachment1', type: 'attachment', challengeId: 'recChallenge2', filename: 'attachment.png' });
-
-    // when
-    await visit('/competence/recCompetence2.1/prototypes/recChallenge2');
-    await click(find('[data-test-modify-challenge-button]'));
-    await click(find('[data-test-delete-attachment-button]'));
-
-    await later(this, async () => {}, 200);
-    await click(find('[data-test-cancel-challenge-button]'));
-
-    const store = this.owner.lookup('service:store');
-    const attachments = await store.peekAll('attachment');
-
-    // then
-    assert.dom('[data-test-main-message]').hasText('Modification annulée');
-    assert.equal(attachments.length, 1);
-    assert.ok(attachments.every(record => !record.isDeleted));
-  });
 });
 
