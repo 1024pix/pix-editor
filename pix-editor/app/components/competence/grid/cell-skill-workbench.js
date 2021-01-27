bd0767f98cbec632d3590d4975fa54cde0f454bc
jeremie <jeremie.jade@pix.fr>
2021-01-27 12:54:55 +0100

    Display skill count by status

--- pix-editor/app/components/competence/grid/cell-skill-workbench.js
+++ pix-editor/app/components/competence/grid/cell-skill-workbench.js
@@ -4,20 +4,34 @@ import Component from '@glimmer/component';
 export default class CompetenceGridCellSkillWorkbenchComponent extends Component {
 
   get archivedCount() {
-    const skills = this.args.tube.filledDeadSkills[parseInt(this.args.level) - 1];
-    if (!skills) {
-      return 0;
-    }
-    const archivedSkill = skills.filter(skill=> skill.isArchived);
+    const archivedSkill = this.args.skills.filter(skill=> skill.isArchived);
     return archivedSkill.length;
   }
 
   get deletedCount() {
-    const skills = this.args.tube.filledDeadSkills[parseInt(this.args.level)  - 1];
-    if (!skills) {
-      return 0;
-    }
-    const deletedSkill = skills.filter(skill=> skill.isDeleted);
+    const deletedSkill = this.args.skills.filter(skill=> skill.isDeleted);
     return deletedSkill.length;
+  }
+
+  get draftCount() {
+    const draftSkill = this.args.skills.filter(skill=> skill.isDraft);
+    return draftSkill.length;
+  }
+
+  get haveActiveSkill() {
+    const activeSkill = this.args.skills.filter(skill=> skill.isActive);
+    return activeSkill.length > 0;
+  }
+
+  get haveDraftSkill() {
+    return this.draftCount > 0;
+  }
+
+  get haveArchivedSkill() {
+    return this.archivedCount > 0;
+  }
+
+  get haveDeletedSkill() {
+    return this.deletedCount > 0;
   }
 }
---
+++ pix-editor/tests/integration/components/competence/grid/cell-skill-workbench-test.js
@@ -0,0 +1,88 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { run } from '@ember/runloop';
+
+
+module('Integration | Component | competence/grid/cell-skill-workbench', function(hooks) {
+  setupRenderingTest(hooks);
+
+  let store;
+  let skillRecord1, skillRecord2, skillRecord3, skillRecord4, skillRecord5, skillRecord6;
+  const skillName = '@skill1';
+  let tube;
+
+  hooks.beforeEach (async function() {
+    // given
+    store = this.owner.lookup('service:store');
+    skillRecord1 = store.createRecord('skill',{
+      id: 'rec654258',
+      name: skillName,
+      level: 1,
+      status:'actif',
+      challenges: [store.createRecord('challenge',{
+        id: 'recChallenge0',
+        genealogy: 'Prototype 1',
+        status: 'validé'
+      })]
+    });
+    skillRecord2 = store.createRecord('skill',{
+      id: 'rec654259',
+      level: 1,
+      name: skillName,
+      status:'archivé'
+    });
+    skillRecord3 = store.createRecord('skill',{
+      id: 'rec654260',
+      level: 1,
+      name: skillName,
+      status:'en construction',
+    });
+    skillRecord4 = store.createRecord('skill',{
+      id: 'rec654261',
+      level: 1,
+      name: skillName,
+      status:'archivé',
+    });
+    skillRecord5 = store.createRecord('skill',{
+      id: 'rec664261',
+      level: 1,
+      name: skillName,
+      status:'en construction',
+    });
+    skillRecord6 = store.createRecord('skill',{
+      id: 'rec674261',
+      level: 1,
+      name: skillName,
+      status:'périmé',
+    });
+    tube = run(() => store.createRecord('tube',{
+      id: 'rec123456',
+      name: 'tubeName',
+      rawSkills: [
+        skillRecord1
+        ,skillRecord2
+        ,skillRecord3
+        ,skillRecord4
+        ,skillRecord5
+        ,skillRecord6
+      ]
+    }));
+    this.skill = skillRecord1;
+    this.skills = [skillRecord1, skillRecord2, skillRecord3, skillRecord4, skillRecord5, skillRecord6];
+    this.tube = tube;
+
+    // when
+    await render(hbs`<Competence::Grid::CellSkillWorkbench @tube={{this.tube}} @skill={{this.skill}} @skills={{this.skills}}/>`);
+  });
+
+  test('it should display a skill count by status', async function(assert) {
+    // then
+    assert.dom('[data-test-draft-count]').hasText('2');
+    assert.dom('[data-test-active-count]').hasText('1');
+    assert.dom('[data-test-archived-count]').hasText('2');
+    assert.dom('[data-test-deleted-count]').hasText('1');
+  });
+
+});
