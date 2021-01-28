98ce6c2da3d750a80f9fc2c70cc4b8da98870e93
Thomasevano <thomas-evano@hotmail.fr>
2021-01-28 16:28:54 +0100

    Fix modify-challenge e2e test

--- end-to-end-tests/cypress/integration/modify-challenge.spec.js
+++ end-to-end-tests/cypress/integration/modify-challenge.spec.js
@@ -10,14 +10,14 @@ context('Modifying challenge', () => {
       .type('8d03a893-3967-4501-9dc4-e0aa6c6dc442{enter}');
     cy.contains('1. Information', {timeout: 30000}).click();
     cy.contains('1.1').click()
-    cy.contains('@eval2', {timeout: 6000}).click();
+    cy.contains('@eval2', {timeout: 12000}).click();
     cy.contains('Modifier').click();
     cy.contains('Enregistrer').click();
     cy.server();
     cy.route('POST', '/api/**').as('route');
 
     // when
-    cy.contains('Enregistrer').click();
+    cy.contains('Enregistrer', {timeout: 2000}).click();
 
     // then
     cy.wait('@route').its('status').should('be.within', 200, 204);
