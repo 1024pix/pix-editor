ed17b3bbdde3db20007fbde6e911991e26cdcce1
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-01-28 22:58:33 +0100

    Properly import `setupMirage` so `Mirage logging` checkbox is back in qunit test runnner html page

--- pix-editor/tests/acceptance/modify-challenge/modify-challenge-test.js
+++ pix-editor/tests/acceptance/modify-challenge/modify-challenge-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { visit, findAll, click, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { later } from '@ember/runloop';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Acceptance | Modify-Challenge', function(hooks) {
   setupApplicationTest(hooks);
