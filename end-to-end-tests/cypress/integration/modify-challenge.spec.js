fccd0a18b1edfef05da7e8463f33af87342d85ce
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-01-30 15:12:01 +0100

    Define all timeout values in config file

--- end-to-end-tests/cypress/integration/modify-challenge.spec.js
+++ end-to-end-tests/cypress/integration/modify-challenge.spec.js
@@ -8,15 +8,15 @@ context('Modifying challenge', () => {
     cy.visit('http://localhost:4300');
     cy.get('#login-api-key')
       .type('8d03a893-3967-4501-9dc4-e0aa6c6dc442{enter}');
-    cy.contains('1. Information', {timeout: 30000}).click();
+    cy.contains('1. Information').click();
     cy.contains('1.1').click()
-    cy.contains('@eval2', {timeout: 12000}).click();
+    cy.contains('@eval2').click();
     cy.contains('Modifier').click();
     cy.contains('Enregistrer').click();
     cy.intercept('POST', '/api/**').as('route');
 
     // when
-    cy.contains('Enregistrer', {timeout: 2000}).click();
+    cy.contains('Enregistrer').click();
 
     // then
     cy.wait('@route').its('response.statusCode').should('be.within', 200, 204);
