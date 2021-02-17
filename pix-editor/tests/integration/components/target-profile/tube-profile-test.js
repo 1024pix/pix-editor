5f92b513ea2553ed2c6ad87465e1a17ab55b8135
jeremie <jeremie.jade@pix.fr>
2021-02-17 14:57:34 +0100

    Fix filter for target profile

--- pix-editor/tests/integration/components/target-profile/tube-profile-test.js
+++ pix-editor/tests/integration/components/target-profile/tube-profile-test.js
@@ -5,85 +5,50 @@ import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | target-profile/competence-thematic-result', function (hooks) {
   setupRenderingTest(hooks);
-  let tube, showTubeDetails;
+  let tube;
   hooks.beforeEach(async function () {
     //given
     tube = {
       id: 'rec123456',
-      name: 'tube1',
+      name: '@tube1',
       practicalDescriptionFr: 'practicalDescriptionFr',
       practicalTitleFr: 'practicalTitleFr',
     };
 
-    //when
     this.tube = tube;
     this.clickOnTube = () => {
     };
   });
-  module('if `showTubeDetails` is false', function (hooks) {
-    hooks.beforeEach(async function () {
-      //given
-      showTubeDetails = false;
 
-      //when
-      this.showTubeDetails = showTubeDetails;
-    });
-    test('it should display a `tube.practicalTitleFr` and a `tube.practicalDescriptionFr`', async function (assert) {
-      //when
-      const expectedTubePracticalTitleFr = tube.practicalTitleFr;
-      const expectedDescription = tube.practicalDescriptionFr;
-      await render(hbs`<TargetProfile::TubeProfile @tube={{this.tube}}
-                                                   @clickAction={{this.clickOnTube}}
-                                                   @showTubeDetails={{this.showTubeDetails}}/>`);
-      //then
-      assert.ok(this.element.querySelector('.practicalTitle-profile').textContent.trim() === expectedTubePracticalTitleFr);
-      assert.ok(this.element.querySelector('.practicalDescription-profile').textContent.trim() === expectedDescription);
-    });
-    test('it should be selected if tube have a `selectedSkillLevel`', async function (assert) {
-      //given
-      const selectedLevel = 6;
+  test('it should be selected if tube have a `selectedSkillLevel` if `showTubeDetails` is `false`', async function (assert) {
+    //given
+    this.selectedLevel = 6;
+    this.showTubeDetails = false;
 
-      //when
-      this.selectedLevel = selectedLevel;
-      await render(hbs`<TargetProfile::TubeProfile @tube={{this.tube}}
-                                                   @clickAction={{this.clickOnTube}}
-                                                   @selectedSkillLevel={{this.selectedLevel}}
-                                                   @showTubeDetails={{this.showTubeDetails}}/>`);
-      //then
-      assert.dom(this.element.querySelector('[data-test-tube-profile]')).hasClass('active');
-      assert.dom(this.element.querySelector('.square.icon')).hasClass('active');
-      assert.dom(this.element.querySelector('.square.icon')).hasClass('check');
-    });
+    //when
+    await render(hbs`<TargetProfile::TubeProfile @tube={{this.tube}}
+                                                 @clickAction={{this.clickOnTube}}
+                                                 @selectedSkillLevel={{this.selectedLevel}}
+                                                 @showTubeDetails={{this.showTubeDetails}}/>`);
+
+    //then
+    assert.dom(this.element.querySelector('[data-test-tube-profile]')).hasClass('active');
+    assert.dom(this.element.querySelector('.square.icon')).hasClass('active');
+    assert.dom(this.element.querySelector('.square.icon')).hasClass('check');
   });
-  module('if `showTubeDetails` is true', function (hooks) {
-    hooks.beforeEach(async function () {
-      //given
-      showTubeDetails = true;
 
-      //when
-      this.showTubeDetails = showTubeDetails;
-    });
-    test('it should display a `tube.name`', async function (assert) {
-      //when
-      const expectedTubePracticalTitleFr = tube.name;
-      await render(hbs`<TargetProfile::TubeProfile @tube={{this.tube}}
-                                                   @clickAction={{this.clickOnTube}}
-                                                   @showTubeDetails={{this.showTubeDetails}}/>`);
-      //then
-      assert.ok(this.element.querySelector('.practicalTitle-profile').textContent.trim() === expectedTubePracticalTitleFr);
-    });
-    test('it should display a `selectedSkillLevel`', async function (assert) {
-      //given
-      const selectedLevel = 6;
+  test('it should display a `selectedSkillLevel` if `showTubeDetails` is `true`', async function (assert) {
+    //given
+    this.showTubeDetails = true;
+    this.selectedLevel = 6;
 
-      //when
-      this.selectedLevel = selectedLevel;
-      await render(hbs`<TargetProfile::TubeProfile @tube={{this.tube}}
-                                                   @clickAction={{this.clickOnTube}}
-                                                   @selectedSkillLevel={{this.selectedLevel}}
-                                                   @showTubeDetails={{this.showTubeDetails}}/>`);
-      //then
-      assert.ok(this.element.querySelector('.max-skill-level').textContent.trim() === `${selectedLevel}`);
-    });
+    //when
+    await render(hbs`<TargetProfile::TubeProfile @tube={{this.tube}}
+                                                 @clickAction={{this.clickOnTube}}
+                                                 @selectedSkillLevel={{this.selectedLevel}}
+                                                 @showTubeDetails={{this.showTubeDetails}}/>`);
+
+    //then
+    assert.dom('.max-skill-level').hasText('6');
   });
 });
