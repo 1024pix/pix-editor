50deeb5ddc50f167a0a88426af0e72e8a2b7b420
jeremie <jeremie.jade@pix.fr>
2021-02-22 12:27:06 +0100

    Display empty tubes only on workbench view

--- pix-editor/app/components/competence/competence-grid.js
+++ pix-editor/app/components/competence/competence-grid.js
@@ -2,6 +2,6 @@ import Component from '@glimmer/component';
 
 export default class CompetenceGrid extends Component {
   get displayAllTubes() {
-    return this.args.section === 'skills' || this.args.view === 'workbench';
+    return this.args.view === 'workbench';
   }
 }
