81eb5abfcc4aed70d2aac58a47214bb8f04fbc1c
jeremie <jeremie.jade@pix.fr>
2021-02-23 12:41:50 +0100

    Create a route to display Themes

---
+++ pix-editor/app/routes/competence/themes/single.js
@@ -0,0 +1,27 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
+
+export default class CompetenceThemesSingleRoute extends Route {
+  model(params) {
+    return this.store.findRecord('theme', params.theme_id);
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.edition = false;
+    const competenceController = this.controllerFor('competence');
+    competenceController.setSection('skills');
+    competenceController.setView('workbench');
+  }
+
+  @action
+  willTransition(transition) {
+    if (this.controllerFor('competence.tubes.single').edition &&
+      !confirm('Êtes-vous sûr de vouloir abandonner la modification en cours ?')) {
+      transition.abort();
+    } else {
+      return true;
+    }
+  }
+}
