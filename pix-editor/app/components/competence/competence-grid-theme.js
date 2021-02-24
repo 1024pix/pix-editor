0c9b3bf0155b689c874d6be9cd91cc8463703b8c
jeremie <jeremie.jade@pix.fr>
2021-02-24 15:20:55 +0100

    Display theme with no tubes

--- pix-editor/app/components/competence/competence-grid-theme.js
+++ pix-editor/app/components/competence/competence-grid-theme.js
@@ -1,13 +1,13 @@
 import Component from '@glimmer/component';
 
 export default class CompetenceCompetenceGridThemeComponent extends Component {
-  get displayAllTubes() {
-    return this.args.view === 'workbench';
+  get isWorkbenchSkillView() {
+    return this.args.view === 'workbench' && this.args.section === 'skills';
   }
 
   get tubesOrProductionTubes() {
     const theme = this.args.theme;
-    if (this.displayAllTubes) {
+    if (this.args.view === 'workbench') {
       return theme.tubes;
     }
     return theme.productionTubes;
@@ -15,5 +15,10 @@ export default class CompetenceCompetenceGridThemeComponent extends Component {
 
   get rowSpanTheme() {
     return this.tubesOrProductionTubes.length;
+  }
+
+  get hasNoTubes() {
+    const theme = this.args.theme;
+    return theme.tubes.length === 0;
   }
 }
--- pix-editor/tests/integration/components/competence/competence-grid-theme-test.js
+++ pix-editor/tests/integration/components/competence/competence-grid-theme-test.js
@@ -4,67 +4,35 @@ import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import EmberObject from '@ember/object';
 
-module('Integration | Component | competence/competence-grid-theme', function(hooks) {
+module('Integration | Component | competence/competence-grid-theme', function (hooks) {
+
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
-    const productionTube1 = EmberObject.create({
-      name: '@productionTube1'
+  module('if theme have tube', function (hooks) {
+    hooks.beforeEach(function () {
+      const productionTube1 = EmberObject.create({
+        name: '@productionTube1'
+      });
+      const productionTube2 = EmberObject.create({
+        name: '@productionTube2'
+      });
+
+      const draftTube = EmberObject.create({
+        name: '@draftTube'
+      });
+
+      const theme = EmberObject.create({
+        name: 'Theme',
+        tubes: [productionTube1, productionTube2, draftTube],
+        productionTubes: [productionTube1, productionTube2]
+      });
+      this.set('theme', theme);
     });
-    const productionTube2 = EmberObject.create({
-      name: '@productionTube2'
-    });
 
-    const draftTube = EmberObject.create({
-      name: '@draftTube'
-    });
-
-    const theme = EmberObject.create({
-      name: 'Theme',
-      tubes: [productionTube1, productionTube2, draftTube],
-      productionTubes: [productionTube1, productionTube2]
-    });
-    this.set('theme', theme);
-  });
-
-  test('it should display a theme cell with appropriate rowspan', async function (assert) {
-    // given
-    this.set('view', 'workbench');
-    this.set('section', 'challenges');
-
-    // when
-    await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
-                                                       @view={{this.view}}
-                                                       @theme={{this.theme}}/>`);
-
-    // then
-    assert.dom('[data-test-theme-cell]').hasText('Theme');
-    assert.dom('[data-test-theme-cell]').hasAttribute('rowspan', '3');
-
-  });
-
-  ['skills', 'challenges', 'i18n', 'quality'].forEach(section => {
-    test(`it should display production tubes if section is set on ${section} and view is production`, async function(assert) {
-      // given
-      this.set('view', 'production');
-      this.set('section', section);
-
-      // when
-      await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
-                                                         @view={{this.view}}
-                                                         @theme={{this.theme}}/>`);
-
-      // then
-      assert.dom('[data-test-tube-cell]').exists({ count: 2 });
-
-    });
-  });
-
-  ['skills', 'challenges'].forEach(section => {
-    test(`it should display all tubes if section is set on ${section} and view is workbench`, async function(assert) {
+    test('it should display a theme cell with appropriate rowspan', async function (assert) {
       // given
       this.set('view', 'workbench');
-      this.set('section', section);
+      this.set('section', 'challenges');
 
       // when
       await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
@@ -72,9 +40,103 @@ module('Integration | Component | competence/competence-grid-theme', function(ho
                                                          @theme={{this.theme}}/>`);
 
       // then
-      assert.dom('[data-test-tube-cell]').exists({ count: 3 });
+      assert.dom('[data-test-theme-cell]').hasText('Theme');
+      assert.dom('[data-test-theme-cell]').hasAttribute('rowspan', '3');
 
+    });
+
+    test('it should display a link to modify theme if section is skills and view is workbench', async function (assert) {
+      // given
+      this.set('view', 'workbench');
+      this.set('section', 'skills');
+
+      // when
+      await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
+                                                         @view={{this.view}}
+                                                         @theme={{this.theme}}/>`);
+
+      // then
+      assert.dom('[data-test-theme-cell] a').hasText('Theme');
+
+    });
+
+    ['skills', 'challenges', 'i18n', 'quality'].forEach(section => {
+      test(`it should display production tubes if section is set on ${section} and view is production`, async function (assert) {
+        // given
+        this.set('view', 'production');
+        this.set('section', section);
+
+        // when
+        await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
+                                                           @view={{this.view}}
+                                                           @theme={{this.theme}}/>`);
+
+        // then
+        assert.dom('[data-test-tube-cell]').exists({ count: 2 });
+
+      });
+    });
+
+    ['skills', 'challenges'].forEach(section => {
+      test(`it should display all tubes if section is set on ${section} and view is workbench`, async function (assert) {
+        // given
+        this.set('view', 'workbench');
+        this.set('section', section);
+
+        // when
+        await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
+                                                           @view={{this.view}}
+                                                           @theme={{this.theme}}/>`);
+
+        // then
+        assert.dom('[data-test-tube-cell]').exists({ count: 3 });
+
+      });
     });
   });
 
+  module('if theme have no tube', function (hooks) {
+
+    hooks.beforeEach(function () {
+      const theme = EmberObject.create({
+        name: 'Theme',
+        tubes: [],
+        productionTubes: []
+      });
+      this.set('theme', theme);
+    });
+
+    [{ section: 'skills', view: 'production' },
+      { section: 'challenges', view: 'production' },
+      { section: 'challenges', view: 'workbench' },
+      { section: 'i18n', view: null },
+      { section: 'quality', view: null }].forEach(item => {
+      test(`it should not be display if section is ${item.section} and view is ${item.view}`, async function (assert) {
+        // given
+        this.set('view', item.view);
+        this.set('section', item.section);
+
+        // when
+        await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
+                                                           @view={{this.view}}
+                                                           @theme={{this.theme}}/>`);
+        // then
+        assert.dom('tr').doesNotExist();
+      });
+    });
+
+    test('it should display theme name and empty cell if section is skills and view is workbench', async function (assert) {
+      // given
+      this.set('view', 'workbench');
+      this.set('section', 'skills');
+
+      // when
+      await render(hbs`<Competence::CompetenceGridTheme  @section={{this.section}}
+                                                           @view={{this.view}}
+                                                           @theme={{this.theme}}/>`);
+      // then
+      assert.dom('[data-test-theme-cell] a').hasText('Theme');
+      assert.dom('[data-test-empty-row]').exists();
+    });
+  });
 });
