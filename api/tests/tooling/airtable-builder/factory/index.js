d3156cf589348c842182154222aa833b4cfe9fd6
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-03-18 10:41:36 +0100

    Use the attachments table to fill the challenges attachments and illustration
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

--- api/tests/tooling/airtable-builder/factory/index.js
+++ api/tests/tooling/airtable-builder/factory/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   buildArea: require('./build-area'),
+  buildAttachment: require('./build-attachment'),
   buildChallenge: require('./build-challenge'),
   buildCompetence: require('./build-competence'),
   buildCourse: require('./build-course'),
