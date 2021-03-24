331ec850d917597c120e6a57923029286f266a25
jeremie <jeremie.jade@pix.fr>
2021-03-24 12:31:34 +0100

    Add test on tube edition and creation

--- pix-editor/tests/unit/controllers/competence/tubes/new-test.js
+++ pix-editor/tests/unit/controllers/competence/tubes/new-test.js
@@ -1,12 +1,118 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
+import sinon from 'sinon';
+import EmberObject from '@ember/object';
+
 
 module('Unit | Controller | competence/tubes/new', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:competence/tubes/new');
-    assert.ok(controller);
+  let controller, notifyMessageStub, notifyErrorStub, loaderStartStub, loaderStopStub, deleteRecordStub, themeRollbackAttributesStub;
+
+  hooks.beforeEach(function () {
+    notifyMessageStub = sinon.stub();
+    notifyErrorStub = sinon.stub();
+
+    class NotifyService extends Service {
+      message = notifyMessageStub;
+      error = notifyErrorStub;
+    }
+
+    this.owner.register('service:notify', NotifyService);
+
+    loaderStartStub = sinon.stub();
+    loaderStopStub = sinon.stub();
+
+    class LoaderService extends Service {
+      start = loaderStartStub;
+      stop = loaderStopStub;
+    }
+
+    this.owner.register('service:loader', LoaderService);
+
+    class CurrentDataService extends Service {
+      getCompetence() {
+        return {
+          name:'Competence'
+        };
+      }
+    }
+    this.owner.register('service:currentData', CurrentDataService);
+
+    deleteRecordStub = sinon.stub();
+
+    class StoreService extends Service {
+      deleteRecord = deleteRecordStub;
+    }
+    this.owner.register('service:store', StoreService);
+
+    controller = this.owner.lookup('controller:competence/tubes/new');
+
+    themeRollbackAttributesStub = sinon.stub();
+    controller.model = EmberObject.create({
+      theme: {
+        rollbackAttributes: themeRollbackAttributesStub,
+      }
+    });
+    controller.edition = true;
+  });
+
+  test('it should cancel creation', async function(assert) {
+    // given
+    controller.model.name = 'newTube';
+    const parentControllerSendStub = sinon.stub();
+    controller.parentController = {
+      send: parentControllerSendStub,
+    };
+
+    // when
+    await controller.cancelEdit();
+
+    // then
+    assert.ok(themeRollbackAttributesStub.calledOnce);
+    assert.ok(deleteRecordStub.calledWith(controller.model));
+    assert.notOk(controller.edition);
+    assert.ok(notifyMessageStub.calledWith('Création annulée'));
+    assert.ok(parentControllerSendStub.calledWith('closeChildComponent'));
+  });
+
+  test('it should save record', async function(assert) {
+    // given
+    const transitionToRouteStub = sinon.stub();
+    controller.transitionToRoute = transitionToRouteStub;
+
+    const saveStub = sinon.stub().resolves();
+    controller.model.save = saveStub;
+
+    // when
+    await controller.save();
+
+    // then
+    assert.ok(loaderStartStub.calledOnce);
+    assert.notOk(controller.edition);
+    assert.ok(saveStub.calledOnce);
+    assert.ok(loaderStopStub.calledOnce);
+    assert.ok(notifyMessageStub.calledWith('Tube créé'));
+    assert.ok(transitionToRouteStub.calledWith('competence.tubes.single', { name:'Competence' }, controller.model));
+  });
+
+  test('it should catch an error if save action failed', async function (assert) {
+    // given
+    const errorMessage = {
+      'error': ['error-test']
+    };
+    const saveStub = sinon.stub().rejects(errorMessage);
+    controller.model.save = saveStub;
+
+    // when
+    await controller.save();
+
+    // then
+    assert.ok(loaderStartStub.calledOnce);
+    assert.ok(controller.edition);
+    assert.ok(saveStub.calledOnce);
+    assert.ok(loaderStopStub.calledOnce);
+    assert.ok(notifyErrorStub.calledWith('Erreur lors de la création du tube'));
   });
 });
--- pix-editor/tests/unit/controllers/competence/tubes/single-test.js
+++ pix-editor/tests/unit/controllers/competence/tubes/single-test.js
@@ -1,12 +1,205 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
+import sinon from 'sinon';
 
 module('Unit | Controller | competence/tubes/single', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:competence/tubes/single');
-    assert.ok(controller);
+  let controller, store, notifyMessageStub, notifyErrorStub, loaderStartStub, loaderStopStub;
+
+  hooks.beforeEach(function () {
+
+    notifyMessageStub = sinon.stub();
+    notifyErrorStub = sinon.stub();
+
+    class NotifyService extends Service {
+      message = notifyMessageStub;
+      error = notifyErrorStub;
+    }
+
+    this.owner.register('service:notify', NotifyService);
+
+    loaderStartStub = sinon.stub();
+    loaderStopStub = sinon.stub();
+
+    class LoaderService extends Service {
+      start = loaderStartStub;
+      stop = loaderStopStub;
+    }
+
+    this.owner.register('service:loader', LoaderService);
+
+    store = this.owner.lookup('service:store');
+
+    const tube = store.createRecord('tube',{
+      id: 'recTube0',
+      name: 'TubeName',
+    });
+
+    controller = this.owner.lookup('controller:competence/tubes/single');
+    controller.model = tube;
+
+  });
+
+  test('it should save modifications', async function(assert) {
+    // given
+    const reloadSkillsStub = sinon.stub();
+    controller.model.hasMany('rawSkills').reload = reloadSkillsStub;
+
+    const saveStub = sinon.stub().resolves();
+    controller.model.save = saveStub;
+    controller.edition = true;
+
+    // when
+    await controller.save();
+
+    // then
+    assert.ok(loaderStartStub.calledOnce);
+    assert.notOk(controller.edition);
+    assert.ok(saveStub.calledOnce);
+    assert.ok(loaderStopStub.calledOnce);
+    assert.ok(notifyMessageStub.calledWith('Tube mis à jour'));
+    assert.ok(reloadSkillsStub.calledOnce);
+  });
+
+  test('it should catch an error if save action failed', async function(assert) {
+    // given
+    const errorMessage = {
+      'error': ['error-test']
+    };
+    const saveStub = sinon.stub().rejects(errorMessage);
+    controller.model.save = saveStub;
+    controller.edition = true;
+
+    // when
+    await controller.save();
+
+    // then
+    assert.ok(loaderStartStub.calledOnce);
+    assert.ok(controller.edition);
+    assert.ok(saveStub.calledOnce);
+    assert.ok(loaderStopStub.calledOnce);
+    assert.ok(notifyErrorStub.calledWith('Erreur lors de la mise à jour du tube'));
+  });
+
+  test('it should start edition', function(assert) {
+    // given
+    const sendStub = sinon.stub();
+    controller.send = sendStub;
+    controller.parentController.leftMaximized = true;
+    controller.edition = false;
+    controller.wasMaximized = false;
+    // when
+    controller.edit();
+
+    // then
+    assert.ok(controller.edition);
+    assert.ok(controller.wasMaximized);
+    assert.ok(sendStub.calledWith('maximize'));
+  });
+
+  module('#cancelEdition', function(hooks) {
+    let rollbackAttributesStub;
+    hooks.beforeEach(function () {
+      rollbackAttributesStub = sinon.stub();
+      controller.model.rollbackAttributes = rollbackAttributesStub;
+      controller.edition = true;
+    });
+
+    test('it should cancel edition', function(assert) {
+      // given
+      this.wasMaximized = true;
+
+      // when
+      controller.cancelEdit();
+
+      // then
+      assert.notOk(controller.edition);
+      assert.ok(rollbackAttributesStub.calledOnce);
+      assert.ok(notifyMessageStub.calledWith('Modification annulée'));
+    });
+
+    test('it should send `minimize` if `wasMaximized` is `false`', function(assert) {
+      // given
+      const sendStub = sinon.stub();
+      controller.send = sendStub;
+      this.wasMaximized = false;
+
+      // when
+      controller.cancelEdit();
+
+      // then
+      assert.ok(sendStub.calledWith('minimize'));
+    });
+
+    test('it should close and cancel edition', function(assert) {
+      // given
+      const parentControllerSendStub = sinon.stub();
+      controller.parentController.send = parentControllerSendStub;
+
+      // when
+      controller.close();
+
+      // then
+      assert.notOk(controller.edition);
+      assert.ok(rollbackAttributesStub.calledOnce);
+      assert.ok(notifyMessageStub.calledWith('Modification annulée'));
+      assert.ok(parentControllerSendStub.calledWith('closeChildComponent'));
+    });
+  });
+
+  module('#setCompetence', function(hooks) {
+    let newTheme, newCompetence;
+    hooks.beforeEach(function () {
+      newTheme = store.createRecord('theme',{
+        id: 'recTheme0',
+        name: 'themeName',
+      });
+      newCompetence = store.createRecord('competence',{
+        id: 'recCompetence0',
+        name: 'competenceName',
+      });
+    });
+
+    test('it should set a competence and a theme', async function(assert) {
+      // given
+      const transitionToRouteStub = sinon.stub();
+      controller.transitionToRoute = transitionToRouteStub;
+      const saveStub = sinon.stub().resolves();
+      controller.model.save = saveStub;
+
+      // when
+      await controller.setCompetence(newCompetence, newTheme);
+      const competence = await controller.model.get('competence');
+      const theme = await controller.model.get('theme');
+
+      // then
+      assert.ok(loaderStartStub.calledOnce);
+      assert.ok(saveStub.calledOnce);
+      assert.ok(loaderStopStub.calledOnce);
+      assert.ok(notifyMessageStub.calledWith('Tube mis à jour'));
+      assert.ok(transitionToRouteStub.calledWith('competence.tubes.single', newCompetence, controller.model));
+      assert.deepEqual(competence, newCompetence);
+      assert.deepEqual(theme, newTheme);
+    });
+
+    test('it should catch an error if action failed',async function(assert) {
+      // given
+      const errorMessage = {
+        'error': ['error-test']
+      };
+      const saveStub = sinon.stub().rejects(errorMessage);
+      controller.model.save = saveStub;
+
+      // when
+      await controller.setCompetence(newCompetence, newTheme);
+
+      // then
+      assert.ok(loaderStartStub.calledOnce);
+      assert.ok(saveStub.calledOnce);
+      assert.ok(loaderStopStub.calledOnce);
+      assert.ok(notifyErrorStub.calledWith('Erreur lors de la mise à jour du tube'));
+    });
   });
 });
