ec709c26d6c32525603ccec1cc49bc80414246f8
François de Metz <francois.de-metz@pix.fr>
2021-04-20 10:44:37 +0200

    Add more logs on the queue
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/lib/infrastructure/scheduled-jobs/release-job.js
+++ api/lib/infrastructure/scheduled-jobs/release-job.js
@@ -8,9 +8,23 @@ const queueError = (err, message) => {
   logger.error(err, message);
   Sentry.captureException(err);
 };
+const queueMessage = (message) => {
+  logger.info(message);
+};
+
 const queue = new Queue('create-release-queue', config.scheduledJobs.redisUrl);
 queue.on('error', (err) => queueError(err, 'Queue error for creating release'));
 queue.on('failed', (job, err) => queueError(err, `Release job ${job.id} failed`));
+queue.on('waiting', (jobId) => queueMessage(`A job ${jobId} has been scheduled`));
+queue.on('active', () => queueMessage('A job has started'));
+queue.on('stalled', () => queueMessage('A job has stalled'));
+queue.on('completed', () => queueMessage('A job has finished'));
+queue.on('paused', () => queueMessage('The queue has been paused'));
+queue.on('resumed', () => queueMessage('The queue has been resumed'));
+queue.on('cleaned', () => queueMessage('The queue has been cleaned'));
+queue.on('drained', () => queueMessage('The queue has been drained'));
+queue.on('removed', () => queueMessage('A job has been removed'));
+
 queue.process(createRelease);
 
 module.exports = {
