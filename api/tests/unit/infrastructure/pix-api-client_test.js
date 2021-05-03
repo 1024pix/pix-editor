cbadaaa6d97049584e88637ac5c03986d59aa5cd
François de Metz <francois.de-metz@pix.fr>
2021-05-03 15:12:42 +0200

    Refresh the token if the request returns a 401
    Co-authored-by: Jonas Grilleres <jonas.grilleres@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/tests/unit/infrastructure/pix-api-client_test.js
+++ api/tests/unit/infrastructure/pix-api-client_test.js
@@ -27,6 +27,74 @@ describe('Unit | Infrastructure | PIX API Client', () => {
         // then
         expect(requestInterceptor.isDone()).to.be.true;
       });
+
+      it('should refresh the token when receiving a 401', async () => {
+        // given
+        const payload = 'payload';
+        const token = 'token';
+        const newToken = 'myNewToken';
+        sinon.stub(cache, 'get').withArgs('pix-api-token').returns(token);
+
+        const firstRequestInterceptor = nock('https://api.test.pix.fr')
+          .patch('/api/cache/model/id', payload)
+          .matchHeader('Authorization', `Bearer ${token}`)
+          .reply(401);
+
+        const authInterceptor = nock('https://api.test.pix.fr')
+          .post('/api/token', 'username=adminUser&password=123')
+          .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
+          .reply(200, { 'access_token': newToken });
+
+        const secondRequestInterceptor = nock('https://api.test.pix.fr')
+          .patch('/api/cache/model/id', payload)
+          .matchHeader('Authorization', `Bearer ${newToken}`)
+          .reply(200);
+
+        // when
+        await pixApiClient.request({ url: '/api/cache/model/id', payload });
+
+        // then
+        expect(firstRequestInterceptor.isDone()).to.be.true;
+        expect(authInterceptor.isDone()).to.be.true;
+        expect(secondRequestInterceptor.isDone()).to.be.true;
+      });
+
+      it('should not refresh the token forever and returns the error', async () => {
+        // given
+        const payload = 'payload';
+        const token = 'token';
+        const newToken = 'myNewToken';
+        sinon.stub(cache, 'get').withArgs('pix-api-token').returns(token);
+
+        const firstRequestInterceptor = nock('https://api.test.pix.fr')
+          .patch('/api/cache/model/id', payload)
+          .matchHeader('Authorization', `Bearer ${token}`)
+          .reply(401);
+
+        const authInterceptor = nock('https://api.test.pix.fr')
+          .post('/api/token', 'username=adminUser&password=123')
+          .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
+          .reply(200, { 'access_token': newToken });
+
+        const secondRequestInterceptor = nock('https://api.test.pix.fr')
+          .patch('/api/cache/model/id', payload)
+          .matchHeader('Authorization', `Bearer ${newToken}`)
+          .reply(401);
+
+        try {
+          // when
+          await pixApiClient.request({ url: '/api/cache/model/id', payload });
+          expect.fail('should raise an error');
+        } catch (e) {
+          expect(e.response.status).to.equal(401);
+        }
+
+        // then
+        expect(firstRequestInterceptor.isDone()).to.be.true;
+        expect(authInterceptor.isDone()).to.be.true;
+        expect(secondRequestInterceptor.isDone()).to.be.true;
+
+      });
     });
 
     context('when token is not in cache', () => {
