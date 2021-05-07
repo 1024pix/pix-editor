cd1ea6b78066bd46b5f17ec38cc11ae25e0d76e3
jeremie <jeremie.jade@pix.fr>
2021-05-07 15:08:57 +0200

    Add framework on current data service

--- pix-editor/app/services/current-data.js
+++ pix-editor/app/services/current-data.js
@@ -5,8 +5,18 @@ export default class CurrentDataService extends Service {
   @tracked _areas = null;
   @tracked _competence = null;
   @tracked _prototype = null;
+  @tracked _frameworks = null;
+  @tracked _framework = null;
   @tracked _sources = null;
   @tracked _source = null;
+
+  setFrameworks(frameworks) {
+    this._frameworks = frameworks;
+  }
+
+  setFramework(framework) {
+    this._framework = framework;
+  }
 
   setAreas(areas) {
     this._areas = areas;
@@ -20,6 +30,14 @@ export default class CurrentDataService extends Service {
     this._prototype = prototype;
   }
 
+  getFrameworks() {
+    return this._frameworks;
+  }
+
+  getFramework() {
+    return this._framework;
+  }
+
   setSources(sources) {
     this._sources = sources;
   }
@@ -28,11 +46,18 @@ export default class CurrentDataService extends Service {
     this._source = source;
   }
 
+  get isPixFramework() {
+    return this._framework.name === 'Pix';
+  }
+
   getAreas(filteredBySource = true) {
-    if (filteredBySource && this._areas)
+    if (filteredBySource && this._framework) {
+      return this._framework.areas;
+    }
+    if (filteredBySource && this._areas) {
       return this._areas.filter(area => area.source === this._source);
-    else
-      return this._areas;
+    }
+    return this._areas;
   }
 
   getSources() {
--- pix-editor/tests/unit/services/current-data-test.js
+++ pix-editor/tests/unit/services/current-data-test.js
@@ -1,12 +1,106 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
 
 module('Unit | Service | current-data', function(hooks) {
   setupTest(hooks);
+  let service, pixFramework, pixFranceFramework, pixArea, pixFranceArea, competence, prototype;
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const service = this.owner.lookup('service:current-data');
-    assert.ok(service);
+  hooks.beforeEach(function() {
+    // Given
+    prototype = EmberObject.create({
+      id: 'prototype'
+    });
+    competence = EmberObject.create({
+      id: 'competence'
+    });
+    pixFranceArea = EmberObject.create({
+      id: 'pixFranceArea'
+    });
+    pixArea = EmberObject.create({
+      id: 'pixArea'
+    });
+    pixFranceFramework = EmberObject.create({
+      id: 'pixFranceFramework',
+      name: 'France',
+      areas: [pixFranceArea]
+    });
+    pixFramework = EmberObject.create({
+      id: 'pixFramework',
+      name: 'Pix',
+      areas: [pixArea]
+    });
+    service = this.owner.lookup('service:current-data');
+    service.setFrameworks([pixFramework, pixFranceFramework]);
+    service.setFramework(pixFramework);
+    service.setAreas([pixArea, pixFranceArea]);
+    service.setCompetence(competence);
+    service.setPrototype(prototype);
+  });
+
+  test('it should return frameworks', function(assert) {
+    // when
+    const frameworks = service.getFrameworks();
+
+    // then
+    assert.deepEqual(frameworks, [pixFramework, pixFranceFramework]);
+  });
+
+  test('it should return framework', function(assert) {
+    // when
+    const framework = service.getFramework();
+
+    // then
+    assert.deepEqual(framework, pixFramework);
+  });
+
+  test('it should return all areas when argument is `false`', function(assert) {
+    // when
+    const areas = service.getAreas(false);
+
+    // then
+    assert.deepEqual(areas, [pixArea, pixFranceArea]);
+  });
+
+  test('it should return areas of set framework when have no argument ', async function(assert) {
+    // when
+    const areas = service.getAreas();
+    // then
+    assert.deepEqual(areas, [pixArea]);
+  });
+
+  test('it should return competence', function(assert) {
+    // when
+    const competenceResult = service.getCompetence();
+
+    // then
+    assert.deepEqual(competenceResult, competence);
+  });
+
+  test('it should return prototype', function(assert) {
+    // when
+    const prototypeResult = service.getPrototype();
+
+    // then
+    assert.deepEqual(prototypeResult, prototype);
+  });
+
+  test('it should return `true` if is a pix framework', function(assert) {
+    // when
+    const isPixFrameworkResult = service.isPixFramework;
+
+    // then
+    assert.ok(isPixFrameworkResult);
+  });
+
+  test('it should return `false` if is not a pix framework', function(assert) {
+    // given
+    service.setFramework(pixFranceFramework);
+
+    // when
+    const isPixFrameworkResult = service.isPixFramework;
+
+    // then
+    assert.notOk(isPixFrameworkResult);
   });
 });
