c1184d620e38889536eeb3b6c76037b5ef43f490
François de Metz <francois.de-metz@pix.fr>
2021-05-19 09:40:32 +0200

    Use async/await in challenge route

--- pix-editor/app/routes/challenge.js
+++ pix-editor/app/routes/challenge.js
@@ -1,30 +1,30 @@
 import Route from '@ember/routing/route';
 
 export default class ChallengeRoute extends Route {
-  model(params) {
-    return this.store.query('challenge', { filterByFormula:`AND(FIND('${params.challenge_id}', {id persistant}))`, maxRecords: 1 })
-      .then(challenges => {
-        return challenges.firstObject;
-      });
+  async model(params) {
+    const challenges = await this.store.query('challenge', {
+      filterByFormula:`AND(FIND('${params.challenge_id}', {id persistant}))`,
+      maxRecords: 1
+    });
+    return challenges.firstObject;
   }
 
-  afterModel(model) {
+  async afterModel(model) {
     if (model) {
-      return model.skills
-        .then(() => {
-          const firstSkill = model.firstSkill;
-          return firstSkill.challenges // in order to load model.relatedPrototype later on
-            .then(() => firstSkill.tube);
-        })
-        .then(tube => tube.competence)
-        .then(competence => {
-          if (model.isPrototype) {
-            return this.transitionTo('competence.prototypes.single', competence, model);
-          } else {
-            return this.transitionTo('competence.prototypes.single.alternatives.single', competence, model.relatedPrototype, model);
-          }
-        })
-        .catch(() => this.transitionTo('index'));
+      try {
+        await model.skills;
+        const firstSkill = model.firstSkill;
+        await firstSkill.challenges; // in order to load model.relatedPrototype later on
+        const tube = await firstSkill.tube;
+        const competence = await tube.competence;
+        if (model.isPrototype) {
+          return this.transitionTo('competence.prototypes.single', competence, model);
+        } else {
+          return this.transitionTo('competence.prototypes.single.alternatives.single', competence, model.relatedPrototype, model);
+        }
+      } catch (e) {
+        this.transitionTo('index');
+      }
     } else {
       return this.transitionTo('index');
     }
