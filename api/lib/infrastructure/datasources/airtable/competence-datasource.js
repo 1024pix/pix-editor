f43c3da7beec4293e6cc946c757c94f6309ea47a
François de Metz <francois.de-metz@pix.fr>
2021-06-01 15:31:57 +0200

    Fix the type of the origin field
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/lib/infrastructure/datasources/airtable/competence-datasource.js
+++ api/lib/infrastructure/datasources/airtable/competence-datasource.js
@@ -32,7 +32,7 @@ module.exports = datasource.extend({
       descriptionEnUs: airtableRecord.get('Description en-us') || airtableRecord.get('Description'),
       areaId: airtableRecord.get('Domaine (id persistant)') ? airtableRecord.get('Domaine (id persistant)')[0] : '',
       skillIds: airtableRecord.get('Acquis (via Tubes) (id persistant)') || [],
-      origin: airtableRecord.get('Origine2'),
+      origin: airtableRecord.get('Origine2')[0],
     };
   },
 
