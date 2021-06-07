967d1ae15dde58effd1b10bc1286f0005e685623
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-06-07 11:55:57 +0200

    Refactor the release creation to use a challenge transformer
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/lib/infrastructure/repositories/release-repository.js
+++ api/lib/infrastructure/repositories/release-repository.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const areaDatasource = require('../datasources/airtable/area-datasource');
 const competenceDatasource = require('../datasources/airtable/competence-datasource');
 const tubeDatasource = require('../datasources/airtable/tube-datasource');
@@ -8,6 +7,7 @@ const tutorialDatasource = require('../datasources/airtable/tutorial-datasource'
 const courseDatasource = require('../datasources/airtable/course-datasource');
 const attachmentDatasource = require('../datasources/airtable/attachment-datasource');
 const airtableSerializer = require('../serializers/airtable-serializer');
+const createChallengeTransformer = require('../transformers/challenge-transformer');
 
 const { knex } = require('../../../db/knex-database-connection');
 
@@ -49,22 +49,28 @@ module.exports = {
     if (model === attachmentDatasource.path()) {
       const rawChallenge = await challengeDatasource.filterById(updatedRecord.challengeId);
       const attachments = await attachmentDatasource.filterByChallengeId(updatedRecord.challengeId);
-      _assignAttachmentsToChallenge(rawChallenge, attachments);
-      const challenge = _filterChallengeFields(rawChallenge);
+      const learningContent = {
+        attachments,
+      };
+      const challengeTransformer = createChallengeTransformer(learningContent);
+      const challenge = challengeTransformer(rawChallenge);
+
       return { updatedRecord: challenge, model: challengeDatasource.path() };
     }
 
     if (model === challengeDatasource.path()) {
       const attachments = await attachmentDatasource.filterByChallengeId(updatedRecord.id);
-      _assignAttachmentsToChallenge(updatedRecord, attachments);
-      const challenge = _filterChallengeFields(updatedRecord);
+      const learningContent = {
+        attachments,
+      };
+      const challengeTransformer = createChallengeTransformer(learningContent);
+      const challenge = challengeTransformer(updatedRecord);
+
       return { updatedRecord: challenge, model };
     }
 
     return { updatedRecord, model };
   },
-
-  assignAttachmentsToChallenges,
 };
 
 async function _getCurrentContent() {
@@ -87,9 +93,19 @@ async function _getCurrentContent() {
     courseDatasource.list(),
     attachmentDatasource.list(),
   ]);
+  const learningContent = {
+    areas,
+    competences,
+    tubes,
+    skills,
+    challengesWithoutAttachments,
+    tutorials,
+    courses,
+    attachments,
+  };
+  const challengeTransformer = createChallengeTransformer(learningContent);
+  const challenges = challengesWithoutAttachments.map(challengeTransformer);
 
-  let challenges = assignAttachmentsToChallenges(challengesWithoutAttachments, attachments);
-  challenges = _filterChallengesFields(challenges);
   return {
     areas,
     competences,
@@ -100,64 +116,3 @@ async function _getCurrentContent() {
     courses,
   };
 }
-
-function _filterChallengesFields(challenges) {
-  return challenges.map(_filterChallengeFields);
-}
-
-function _filterChallengeFields(challenge) {
-  const fieldsToInclude = [
-    'id',
-    'instruction',
-    'proposals',
-    'type',
-    'solution',
-    'solutionToDisplay',
-    't1Status',
-    't2Status',
-    't3Status',
-    'scoring',
-    'status',
-    'skillIds',
-    'embedUrl',
-    'embedTitle',
-    'embedHeight',
-    'timer',
-    'competenceId',
-    'format',
-    'autoReply',
-    'locales',
-    'alternativeInstruction',
-    'attachments',
-    'illustrationAlt',
-    'illustrationUrl',
-    'focusable',
-  ];
-
-  return _.pick(challenge, fieldsToInclude);
-}
-
-function _assignAttachmentToChallenge(challenge, attachment) {
-  if (attachment.type === 'illustration') {
-    challenge.illustrationAlt = attachment.alt;
-    challenge.illustrationUrl = attachment.url;
-  } else {
-    if (!challenge.attachments) {
-      challenge.attachments = [];
-    }
-    challenge.attachments.push(attachment.url);
-  }
-}
-
-function _assignAttachmentsToChallenge(challenge, attachments) {
-  attachments.forEach((attachment) => _assignAttachmentToChallenge(challenge, attachment));
-}
-
-function assignAttachmentsToChallenges(challenges, attachments) {
-  attachments.forEach((attachment) => {
-    const challenge = challenges.find((challenge) => challenge.id === attachment.challengeId);
-    _assignAttachmentToChallenge(challenge, attachment);
-  });
-  return challenges;
-}
-
