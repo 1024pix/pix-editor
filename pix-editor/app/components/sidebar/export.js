7709f1b5b633c6fcc99efb90d88e1bcab196436c
jeremie <jeremie.jade@pix.fr>
2021-06-17 15:35:45 +0200

    Refacto export

--- pix-editor/app/components/sidebar/export.js
+++ pix-editor/app/components/sidebar/export.js
@@ -9,52 +9,57 @@ export default class SidebarExportComponent extends Component {
   @service loader;
 
   @action
-  shareAreas() {
+  async shareAreas() {
     this.loader.start('Récupération des sujets');
-    const areas = this.args.areas;
+    try {
+      const areas = this.args.areas;
+      await this._loadContent(areas);
+      const csvContent = this._buildCSVContent(areas);
+      const fileName = `Export_Sujets_${(new Date()).toLocaleString('fr-FR')}.csv`;
+      this.fileSaver.saveAs(csvContent, fileName);
+      this.notify.message('Sujets exportés');
+    } catch (e) {
+      console.error(e);
+      this.notify.error('Erreur lors de l\'exportation des sujets');
+    } finally {
+      this.loader.stop();
+    }
+  }
+
+  async _loadContent(areas) {
     const getCompetences = areas.map(area => area.competences);
-    return Promise.all(getCompetences)
-      .then(areaCompetences => {
-        const getTubes = areaCompetences.map(competences => competences.map(competence => competence.rawTubes)).flat();
-        return Promise.all(getTubes);
-      })
-      .then(competenceTubes => {
-        const getSkills = competenceTubes.map(tubes => tubes.map(tube => tube.rawSkills)).flat();
-        return Promise.all(getSkills);
-      })
-      .then(tubeSkills => {
-        const getChallenges = tubeSkills.map(skills => skills.map(skill => skill.challenges)).flat();
-        return Promise.all(getChallenges);
-      })
-      .then(() => {
-        const csvContent = areas.reduce((content, area) => {
-          return area.sortedCompetences.reduce((content, competence) => {
-            return competence.productionTubes.reduce((content, tube) => {
-              let fields = [
-                area.name,
-                competence.name,
-                tube.name,
-                tube.title,
-                tube.description,
-                tube.practicalTitleFr,
-                tube.practicalDescriptionFr,
-                tube.productionSkills.reduce((table, skill) => {
-                  table[skill.level - 1] = skill.name;
-                  return table;
-                }, ['░', '░', '░', '░', '░', '░', '░', '░']).join(',')
-              ];
-              fields = fields.map(field => this._formatCSVString(field));
-              return content + '\n' + fields.join(',');
-            }, content);
-          }, content);
-        }, '"Domaine","Compétence","Tube","Titre","Description","Titre pratique","Description pratique","Liste des acquis"');
-        const fileName = `Export_Sujets_${(new Date()).toLocaleString('fr-FR')}.csv`;
-        this.fileSaver.saveAs(csvContent, fileName);
-        this.notify.message('Sujets exportés');
-      })
-      .finally(() => {
-        this.loader.stop();
-      });
+    const areaCompetences = await Promise.all(getCompetences);
+    const getTubes = areaCompetences.map(competences => competences.map(competence => competence.rawTubes)).flat();
+    const competenceTubes = await Promise.all(getTubes);
+    const getSkills = competenceTubes.map(tubes => tubes.map(tube => tube.rawSkills)).flat();
+    const tubeSkills = await Promise.all(getSkills);
+    const getChallenges = tubeSkills.map(skills => skills.map(skill => skill.challenges)).flat();
+    await Promise.all(getChallenges);
+  }
+
+  _buildCSVContent(areas) {
+    return areas.reduce((content, area) => {
+      return area.sortedCompetences.reduce((content, competence) => {
+        return competence.productionTubes.reduce((content, tube) => {
+          let fields = [
+            area.name,
+            competence.name,
+            tube.name,
+            tube.title,
+            tube.description,
+            tube.practicalTitleFr,
+            tube.practicalDescriptionFr,
+            tube.productionSkills.reduce((table, skill) => {
+              table[skill.level - 1] = skill.name;
+              return table;
+            }, ['░', '░', '░', '░', '░', '░', '░', '░']).join(',')
+          ];
+          fields = fields.map(field => this._formatCSVString(field));
+          return content + '\n' + fields.join(',');
+
+        }, content);
+      }, content);
+    }, '"Domaine","Compétence","Tube","Titre","Description","Titre pratique","Description pratique","Liste des acquis"');
   }
 
   _formatCSVString(str) {
---
+++ pix-editor/tests/unit/component/sidebar/export-test.js
@@ -0,0 +1,178 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import createGlimmerComponent from '../../../helpers/create-glimmer-component';
+import sinon from 'sinon';
+
+module('unit | Component | sidebar/export', function(hooks) {
+  setupTest(hooks);
+  let component, areas;
+
+  hooks.beforeEach(function() {
+    component = createGlimmerComponent('component:sidebar/export');
+
+    const productionSkill_1_1 = [
+      {
+        name: 'skill1_1', level: 1
+      }, {
+        name: 'skill1_3', level: 3
+      }, {
+        name: 'skill1_6', level: 6
+      }
+    ];
+
+    const productionSkill_1_2 = [
+      {
+        name: 'skill2_2', level: 2
+      }, {
+        name: 'skill2_3', level: 3
+      }, {
+        name: 'skill2_4', level: 4
+      }
+    ];
+
+    const productionSkill_2_1 = [
+      {
+        name: 'skill3_1', level: 1
+      }, {
+        name: 'skill3_5', level: 5
+      }, {
+        name: 'skill3_6', level: 6
+      }
+    ];
+
+    const productionTubes_1_1 = {
+      name: 'tube1',
+      title: 'title_tube1',
+      description: 'description_tube1',
+      practicalTitleFr: 'practicalTitleFr_tube1',
+      practicalDescriptionFr: 'practicalDescriptionFr_tube1',
+      productionSkills: productionSkill_1_1,
+      rawSkills: productionSkill_1_1,
+    };
+
+    const productionTubes_1_2 = {
+      name: 'tube2',
+      title: 'title_tube2',
+      description: 'description_tube2',
+      practicalTitleFr: 'practicalTitleFr_tube2',
+      practicalDescriptionFr: 'practicalDescriptionFr_tube2',
+      productionSkills: productionSkill_1_2,
+      rawSkills: productionSkill_1_2,
+    };
+
+    const productionTubes_2_1 = {
+      name: 'tube3',
+      title: 'title_tube3',
+      description: 'description_tube3',
+      practicalTitleFr: 'practicalTitleFr_tube3',
+      practicalDescriptionFr: 'practicalDescriptionFr_tube3',
+      productionSkills: productionSkill_2_1,
+      rawSkills: productionSkill_2_1,
+    };
+
+    const competence1 = {
+      name: 'competence1',
+      productionTubes: [productionTubes_1_1, productionTubes_1_2],
+      rawTubes: [productionTubes_1_1, productionTubes_1_2],
+    };
+
+    const competence2 = {
+      name: 'competence2',
+      productionTubes: [productionTubes_2_1],
+      rawTubes: [productionTubes_2_1],
+    };
+
+    areas = [{
+      name: 'area',
+      sortedCompetences: [competence1, competence2],
+      competences: [competence1, competence2],
+    }];
+  });
+
+  test('it should return formatted cvs content', function(assert) {
+    // given
+    const expectedCsvContent = `"Domaine","Compétence","Tube","Titre","Description","Titre pratique","Description pratique","Liste des acquis"
+"area","competence1","tube1","title_tube1","description_tube1","practicalTitleFr_tube1","practicalDescriptionFr_tube1","skill1_1,░,skill1_3,░,░,skill1_6,░,░"
+"area","competence1","tube2","title_tube2","description_tube2","practicalTitleFr_tube2","practicalDescriptionFr_tube2","░,skill2_2,skill2_3,skill2_4,░,░,░,░"
+"area","competence2","tube3","title_tube3","description_tube3","practicalTitleFr_tube3","practicalDescriptionFr_tube3","skill3_1,░,░,░,skill3_5,skill3_6,░,░"`
+
+    // when
+    const csvContent = component._buildCSVContent(areas);
+
+    // then
+    assert.equal(csvContent, expectedCsvContent);
+  });
+
+  test('it should escape `"` by `"`', function(assert) {
+    // given
+    const string = "hello";
+    const expectedResult = "\"hello\"";
+
+    // when
+    const result = component._formatCSVString(string);
+
+    // then
+    assert.equal(result, expectedResult);
+  });
+
+  module('#export', function(hooks) {
+    let notifyMessageStub, notifyErrorStub, loaderStartStub, loaderStopStub, buildCSVContentStub;
+
+    hooks.beforeEach(function () {
+      notifyMessageStub = sinon.stub();
+      notifyErrorStub = sinon.stub();
+
+      component.notify = {
+        message: notifyMessageStub,
+        error: notifyErrorStub,
+      };
+
+      loaderStartStub = sinon.stub();
+      loaderStopStub = sinon.stub();
+
+      component.loader = {
+        start: loaderStartStub,
+        stop: loaderStopStub,
+      };
+
+      buildCSVContentStub = sinon.stub().returns('content');
+      component._buildCSVContent = buildCSVContentStub;
+      component.args.areas = areas;
+    });
+
+    test('it should export subjects', async function(assert) {
+      // given
+      const saveAsStub = sinon.stub();
+      component.fileSaver = {
+        saveAs: saveAsStub,
+      };
+      // when
+      await component.shareAreas();
+
+      // then
+      assert.ok(loaderStartStub.calledWith('Récupération des sujets'));
+      assert.ok(buildCSVContentStub.calledWith(areas));
+      assert.equal(saveAsStub.getCall(0).args[0], 'content');
+      assert.ok(notifyMessageStub.calledWith('Sujets exportés'));
+      assert.ok(loaderStopStub.calledOnce);
+    });
+
+    test('it should catch an error if export subject failed', async function(assert) {
+      // given
+      const saveAsStub = sinon.stub().throws();
+      component.fileSaver = {
+        saveAs: saveAsStub,
+      };
+
+      // when
+      await component.shareAreas();
+
+      // then
+      assert.ok(loaderStartStub.calledWith('Récupération des sujets'));
+      assert.ok(loaderStopStub.calledOnce);
+      assert.ok(notifyErrorStub.calledWith('Erreur lors de l\'exportation des sujets'));
+    });
+  });
+
+
+});
