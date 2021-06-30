331e43c3f7b8d5a8deac90c1e5162fb869425b39
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-06-30 15:06:33 +0200

    Add GET /challenges/:id route
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- api/lib/application/challenges/index.js
+++ api/lib/application/challenges/index.js
@@ -1,4 +1,5 @@
 const qs = require('qs');
+const Boom = require('@hapi/boom');
 const challengeRepository = require('../../infrastructure/repositories/challenge-repository');
 const challengeSerializer = require('../../infrastructure/serializers/jsonapi/challenge-serializer');
 
@@ -16,8 +17,21 @@ exports.register = async function(server) {
         },
       },
     },
+    {
+      method: 'GET',
+      path: '/api/challenges/{id}',
+      config: {
+        handler: async function(request) {
+          const filter = { ids: [request.params.id] };
+          const challenges = await challengeRepository.filter(filter);
+          if (challenges.length === 0) {
+            return Boom.notFound();
+          }
+          return challengeSerializer.serialize(challenges[0]);
+        },
+      },
+    },
   ]);
 };
 
 exports.name = 'challenges';
-
--- api/tests/acceptance/application/challenges/challenges_test.js
+++ api/tests/acceptance/application/challenges/challenges_test.js
@@ -227,4 +227,186 @@ describe('Acceptance | Controller | challenges-controller', () => {
 
   });
 
+  describe('GET /challenges/:id', () => {
+    let user;
+    beforeEach(async function() {
+      user = databaseBuilder.factory.buildAdminUser();
+      await databaseBuilder.commit();
+    });
+
+    it('should return given challenge', async () => {
+      // Given
+      const challenge = domainBuilder.buildChallenge({ id: 'challengeId' });
+      const airtableChallenge = airtableBuilder.factory.buildChallenge(challenge);
+      const airtableCall = nock('https://api.airtable.com')
+        .get('/v0/airtableBaseValue/Epreuves')
+        .query({
+          fields: {
+            '': [
+              'id persistant',
+              'Compétences (via tube) (id persistant)',
+              'Timer',
+              'Consigne',
+              'Propositions',
+              'Type d\'épreuve',
+              'Bonnes réponses',
+              'Bonnes réponses à afficher',
+              'T1 - Espaces, casse & accents',
+              'T2 - Ponctuation',
+              'T3 - Distance d\'édition',
+              'Scoring',
+              'Statut',
+              'Acquix (id persistant)',
+              'Embed URL',
+              'Embed title',
+              'Embed height',
+              'Format',
+              'Réponse automatique',
+              'Langues',
+              'Consigne alternative',
+              'Focalisée',
+              'Record ID',
+              'Acquix',
+              'Généalogie',
+              'Type péda',
+              'Auteur',
+              'Déclinable',
+              'Preview',
+              'Version prototype',
+              'Version déclinaison',
+              'Non voyant',
+              'Daltonien',
+              'Spoil',
+              'Responsive',
+              'Géographie'
+            ],
+          },
+          filterByFormula: 'OR(\'challengeId\' = {id persistant})'
+        })
+        .reply(200, {
+          records: [
+            airtableChallenge,
+          ]
+        });
+
+      const server = await createServer();
+
+      // When
+      const response = await server.inject({
+        method: 'GET',
+        url: '/api/challenges/challengeId',
+        headers: generateAuthorizationHeader(user)
+      });
+
+      // Then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result).to.deep.equal({
+        data: {
+          type: 'challenges',
+          id: 'challengeId',
+          attributes: {
+            'airtable-id': challenge.airtableId,
+            instruction: 'Les moteurs de recherche affichent certains liens en raison d\'un accord commercial.\n\nDans quels encadrés se trouvent ces liens ?',
+            'alternative-instruction': '',
+            type: 'QCM',
+            format: 'mots',
+            proposals: '- 1\n- 2\n- 3\n- 4\n- 5',
+            solution: '1, 5',
+            'solution-to-display': '1',
+            't1-status': 'Activé',
+            't2-status': 'Désactivé',
+            't3-status': 'Activé',
+            pedagogy: 'q-situation',
+            author: ['SPS'],
+            declinable: 'facilement',
+            version: 1,
+            genealogy: 'Prototype 1',
+            status: 'validé',
+            preview: 'http://staging.pix.fr/challenges/recwWzTquPlvIl4So/preview',
+            scoring: '1: @outilsTexte2\n2: @outilsTexte4',
+            timer: 1234,
+            'embed-url': 'https://github.io/page/epreuve.html',
+            'embed-title': 'Epreuve de selection de dossier',
+            'embed-height': 500,
+            'alternative-version': 2,
+            accessibility1: 'OK',
+            accessibility2: 'RAS',
+            spoil: 'Non Sp',
+            responsive:  'non',
+            locales: [],
+            area: 'France',
+            'auto-reply': false,
+            focusable: false,
+          }
+        },
+      });
+      expect(airtableCall.isDone()).to.be.true;
+    });
+
+    it('should return a 404 error when the challenge doesn\'t exist', async () => {
+      const airtableCall = nock('https://api.airtable.com')
+        .get('/v0/airtableBaseValue/Epreuves')
+        .query({
+          fields: {
+            '': [
+              'id persistant',
+              'Compétences (via tube) (id persistant)',
+              'Timer',
+              'Consigne',
+              'Propositions',
+              'Type d\'épreuve',
+              'Bonnes réponses',
+              'Bonnes réponses à afficher',
+              'T1 - Espaces, casse & accents',
+              'T2 - Ponctuation',
+              'T3 - Distance d\'édition',
+              'Scoring',
+              'Statut',
+              'Acquix (id persistant)',
+              'Embed URL',
+              'Embed title',
+              'Embed height',
+              'Format',
+              'Réponse automatique',
+              'Langues',
+              'Consigne alternative',
+              'Focalisée',
+              'Record ID',
+              'Acquix',
+              'Généalogie',
+              'Type péda',
+              'Auteur',
+              'Déclinable',
+              'Preview',
+              'Version prototype',
+              'Version déclinaison',
+              'Non voyant',
+              'Daltonien',
+              'Spoil',
+              'Responsive',
+              'Géographie'
+            ],
+          },
+          filterByFormula: 'OR(\'challengeId2\' = {id persistant})'
+        })
+        .reply(200, {
+          records: []
+        });
+
+      const server = await createServer();
+
+      // When
+      const response = await server.inject({
+        method: 'GET',
+        url: '/api/challenges/challengeId2',
+        headers: generateAuthorizationHeader(user)
+      });
+
+      // Then
+      expect(response.statusCode).to.equal(404);
+      expect(airtableCall.isDone()).to.be.true;
+    });
+
+  });
+
 });
