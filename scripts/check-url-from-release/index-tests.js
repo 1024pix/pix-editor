f2ed54550c360c521b0d8e5d12e6685f9100aa4b
Clarisse Damon <clarisse.damon@octo.com>
2021-10-05 17:15:36 +0200

    Add main function

--- scripts/check-url-from-release/index-tests.js
+++ scripts/check-url-from-release/index-tests.js
@@ -14,6 +14,16 @@ describe('Check urls from release', function() {
       expect(urls).to.deep.equal([]);
     });
 
+    it('should not find url from a challenge when there is no instructions', function() {
+      const challenge = {
+        id: 'challenge123',
+        instruction: null,
+      };
+      const urls = findUrlsFromChallenge(challenge);
+
+      expect(urls).to.deep.equal([]);
+    });
+
     it('should find url from a challenge', function() {
       const challenge = {
         id: 'challenge123',
@@ -43,35 +53,35 @@ describe('Check urls from release', function() {
           }
         ]
       };
-      
+
       const expectedOutput = [
-        { id: 'challenge1', url: 'https://example.net/' }, 
-        { id: 'challenge1', url: 'https://other_example.net/' }, 
+        { id: 'challenge1', url: 'https://example.net/' },
+        { id: 'challenge1', url: 'https://other_example.net/' },
         { id: 'challenge3', url: 'https://example.com/' },
-      ]
+      ];
 
       const urls = findUrlsFromRelease(release);
-      
+
       expect(urls).to.deep.equal(expectedOutput);
     });
 
   });
-});
 
-describe('#buildCSV', function() {
-  it('should build a CSV with the found URLs', function() {
-    const urlList = [
-      { id: 'challenge1', url: 'https://example.net/' }, 
-      { id: 'challenge1', url: 'https://other_example.net/' }, 
-      { id: 'challenge3', url: 'https://example.com/' },
-    ];
+  describe('#buildCSV', function() {
+    it('should build a CSV with the found URLs', function() {
+      const urlList = [
+        { id: 'challenge1', url: 'https://example.net/' },
+        { id: 'challenge1', url: 'https://other_example.net/' },
+        { id: 'challenge3', url: 'https://example.com/' },
+      ];
 
-    const expectedOutput = `challenge1,https://example.net/
+      const expectedOutput = `challenge1,https://example.net/
 challenge1,https://other_example.net/
 challenge3,https://example.com/`;
 
-    const csv = buildCsv(urlList);
+      const csv = buildCsv(urlList);
 
-    expect(csv).to.equal(expectedOutput);
+      expect(csv).to.equal(expectedOutput);
+    });
   });
 });
--- scripts/check-url-from-release/index.js
+++ scripts/check-url-from-release/index.js
@@ -1,7 +1,20 @@
 const urlRegex = require('url-regex-safe');
+const axios = require('axios');
+const fs = require('fs');
+
+async function main() {
+  const url = process.env.RELEASE_URL;
+  const token = process.env.TOKEN_LCMS;
+  const release = await getRelease(url, token);
+
+  const urlList = findUrlsFromRelease(release);
+  const csv = buildCsv(urlList);
+
+  fs.writeFileSync('urlList.csv', csv);
+}
 
 function findUrlsFromChallenge(challenge) {
-  const urls = challenge.instruction.match(urlRegex());
+  const urls = (challenge.instruction || '').match(urlRegex());
   if (!urls) {
     return [];
   }
@@ -10,12 +23,23 @@ function findUrlsFromChallenge(challenge) {
 
 function findUrlsFromRelease(release) {
   return release.challenges.flatMap((challenge) => {
-    return findUrlsFromChallenge(challenge).map(url => ({ id: challenge.id, url }));
+    return findUrlsFromChallenge(challenge).map((url) => ({ id: challenge.id, url }));
   });
 }
 
 function buildCsv(urlList) {
-  return urlList.map(currentUrl => `${currentUrl.id},${currentUrl.url}`).join('\n');
+  return urlList.map((currentUrl) => `${currentUrl.id},${currentUrl.url}`).join('\n');
+}
+
+async function getRelease(url, token) {
+  const { data: { content: content } } = await axios.get(url, {
+    headers: { 'Authorization': 'Bearer ' + token }
+  });
+  return content;
+}
+
+if (process.env.NODE_ENV !== 'test') {
+  main();
 }
 
 module.exports = {
@@ -23,3 +47,4 @@ module.exports = {
   findUrlsFromRelease,
   buildCsv,
 };
+
