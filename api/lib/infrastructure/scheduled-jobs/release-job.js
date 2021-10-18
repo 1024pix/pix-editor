ca7176282a5eba6b0c039d82f542e03dd23118da
François de Metz <francois.de-metz@pix.fr>
2021-10-18 16:13:10 +0200

    Refactor release-job to use the createQueue function
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>

--- api/lib/infrastructure/scheduled-jobs/release-job.js
+++ api/lib/infrastructure/scheduled-jobs/release-job.js
@@ -1,47 +1,12 @@
-const Queue = require('bull');
-const Sentry = require('@sentry/node');
+const createQueue = require('./create-queue');
 const config = require('../../config');
 const logger = require('../logger');
 const releaseRepository = require('../repositories/release-repository.js');
 const SlackNotifier = require('../notifications/SlackNotifier');
 const learningContentNotification = require('../../domain/services/learning-content-notification');
 
-const queueError = (err, ...messages) => {
-  logger.error(err, ...messages);
-  Sentry.captureException(err);
-};
-const queueMessage = (message) => {
-  logger.info(message);
-};
-
-const queue = new Queue('create-release-queue', config.scheduledJobs.redisUrl);
-queue.on('error', (err, additionalError) => queueError(err, 'Queue error for creating release', additionalError));
-queue.on('failed', (job, err) => queueError(err, `Release job ${job.id} failed`));
-queue.on('waiting', (jobId) => queueMessage(`A job ${jobId} has been scheduled`));
-queue.on('active', () => queueMessage('A job has started'));
-queue.on('stalled', () => queueMessage('A job has stalled'));
-queue.on('completed', () => queueMessage('A job has finished'));
-queue.on('paused', () => queueMessage('The queue has been paused'));
-queue.on('resumed', () => queueMessage('The queue has been resumed'));
-queue.on('cleaned', () => queueMessage('The queue has been cleaned'));
-queue.on('drained', () => queueMessage('The queue has been drained'));
-queue.on('removed', () => queueMessage('A job has been removed'));
-
+const queue = createQueue('create-release-queue');
 queue.process(createRelease);
-
-module.exports = {
-  schedule() {
-    if (!_isScheduledReleaseEnabled()) {
-      logger.info('Scheduled release is not enabled - check `CREATE_RELEASE_TIME` and `REDIS_URL` variables');
-      return;
-    }
-    queue.add({}, releaseJobOptions);
-  },
-
-  queue,
-
-  createRelease,
-};
 
 const releaseJobOptions = {
   attempts: config.scheduledJobs.attempts,
@@ -70,7 +35,20 @@ async function createRelease() {
   }
 }
 
+function schedule() {
+  if (!_isScheduledReleaseEnabled()) {
+    logger.info('Scheduled release is not enabled - check `CREATE_RELEASE_TIME` and `REDIS_URL` variables');
+    return;
+  }
+  queue.add({}, releaseJobOptions);
+}
+
 function _isScheduledReleaseEnabled() {
   return config.scheduledJobs.createReleaseTime && config.scheduledJobs.redisUrl;
 }
 
+module.exports = {
+  schedule,
+  queue,
+  createRelease,
+};
