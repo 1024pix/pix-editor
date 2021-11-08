0789b9e0fc0b0d96539922f5281da902a9693f3e
François de Metz <francois.de-metz@pix.fr>
2021-11-08 16:04:19 +0100

    Handle SIG{TERM,KILL} on job processor

--- api/lib/infrastructure/scheduled-jobs/check-urls-job-processor.js
+++ api/lib/infrastructure/scheduled-jobs/check-urls-job-processor.js
@@ -1,6 +1,22 @@
 const { validateUrlsFromRelease } = require('../../domain/usecases/validate-urls-from-release');
+const logger = require('../../infrastructure/logger');
+const { disconnect } = require('../../../db/knex-database-connection');
 const releaseRepository = require('../../infrastructure/repositories/release-repository');
 
 module.exports = function() {
   return validateUrlsFromRelease({ releaseRepository });
 };
+
+async function exitOnSignal(signal) {
+  logger.info(`Processor received signal ${signal}. Closing DB connections before exiting.`);
+  try {
+    await disconnect();
+    process.exit(0);
+  } catch (err) {
+    logger.error(err);
+    process.exit(1);
+  }
+}
+
+process.on('SIGTERM', () => { exitOnSignal('SIGTERM'); });
+process.on('SIGINT', () => { exitOnSignal('SIGINT'); });
