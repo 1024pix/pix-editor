1bb80e42d94b139b41b62c512207db3fd491cdfc
Clarisse Damon <clarisse.damon@octo.com>
2021-12-16 11:11:35 +0100

    Get urls from Pix Framework
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/scripts/get-external-urls-list/index.js
+++ api/scripts/get-external-urls-list/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/../../.env' });
 const _ = require('lodash');
 const { findUrlsFromChallenges } = require('../../lib/domain/usecases/validate-urls-from-release');
 const releaseRepository = require('../../lib/infrastructure/repositories/release-repository');
@@ -6,7 +6,9 @@ const { disconnect } = require('../../db/knex-database-connection');
 
 async function getExternalUrlsList() {
   const release = (await releaseRepository.getLatestRelease()).content;
-  const activeChallenges = getActiveChallenges(release);
+  const skillIdsFromPixFramework = getActiveSkillIdsFromPixFramework(release);
+  const challengesFromPixFramework = getChallengesFromPixFramework(release, skillIdsFromPixFramework);
+  const activeChallenges = getActiveChallenges(challengesFromPixFramework);
   const urlsFromChallenges = findUrlsFromChallenges(activeChallenges, release);
 
   const urls = urlsFromChallenges.map(({ url }) => {
@@ -23,9 +25,31 @@ async function getExternalUrlsList() {
   });
 }
 
-function getActiveChallenges(release) {
-  const challengeInactiveStatus = ['périmé', 'proposé'];
-  return release.challenges.filter((challenge) => !challengeInactiveStatus.includes(challenge.status));
+function getActiveSkillIdsFromPixFramework(release) {
+  const competencesFromPixFramework = release.competences.filter((competence) =>  competence.origin === 'Pix');
+  const skillIds = competencesFromPixFramework.flatMap((competence) => competence.skillIds);
+  return getActiveSkillIds(release, skillIds);
 }
 
-getExternalUrlsList().finally(() => { disconnect() });
+function getChallengesFromPixFramework(release, skillIds) {
+  return release.challenges.filter((challenge) => skillIds.includes(challenge.skillIds[0]));
+}
+
+function getActiveChallenges(challenges) {
+  const challengeInactiveStatus = ['périmé', 'proposé'];
+  return challenges.filter((challenge) =>  {
+    return !challengeInactiveStatus.includes(challenge.status) && !challenge.locales.includes('en');
+  });
+}
+
+function getActiveSkillIds(release, skillIds) {
+  const skills = release.skills;
+  return skills.reduce((acc, skill) => {
+    if (skillIds.includes(skill.id) && skill.name !== '@workbench') {
+      acc.push(skill.id);
+    }
+    return acc;
+  }, []);
+}
+
+getExternalUrlsList().finally(() => { disconnect(); });
