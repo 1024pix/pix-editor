abe5aeeddfe52f6394509a8d19def6e26e0295f1
thetinyspark <contact@the-tiny-spak.com>
2022-01-27 11:51:03 +0100

    [Feature] Add theme name en us
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- pix-editor/tests/integration/components/form/theme-test.js
+++ pix-editor/tests/integration/components/form/theme-test.js
@@ -6,10 +6,11 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | form/theme', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it should display theme name', async function(assert) {
+  test('it should display theme name in french and in english', async function(assert) {
     // given
     const theme = {
-      name: 'themeName'
+      name: 'themeName',
+      nameEnUs: 'themeNameEnUs'
     };
     this.set('theme', theme);
 
@@ -17,7 +18,10 @@ module('Integration | Component | form/theme', function(hooks) {
     await render(hbs`<Form::Theme @theme={{this.theme}}/>`);
     // then
 
-    assert.dom('[data-test-theme-name-field]').hasText('Nom :');
+    assert.dom('[data-test-theme-name-field]').hasText('Nom fr-fr :');
     assert.dom('[data-test-theme-name-field] input').hasValue('themeName');
+
+    assert.dom('[data-test-theme-name-en-us-field]').hasText('Nom en-us :');
+    assert.dom('[data-test-theme-name-en-us-field] input').hasValue('themeNameEnUs');
   });
 });
