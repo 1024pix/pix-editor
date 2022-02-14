10cde054e8e699d430f864f16720facc48a1cd91
jeremie <jeremie.jade@pix.fr>
2022-02-14 18:07:05 +0100

    Add tab to display proposal view

--- pix-editor/tests/integration/components/competence/competence-actions-test.js
+++ pix-editor/tests/integration/components/competence/competence-actions-test.js
@@ -1,10 +1,57 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 module('Integration | Component | competence/competence-actions', function(hooks) {
   setupRenderingTest(hooks);
+  let selectViewStub;
+
+  hooks.beforeEach(function () {
+    selectViewStub = sinon.stub();
+    this.selectView = selectViewStub;
+  });
+
+  module('#skillSection', function(hooks) {
+
+    hooks.beforeEach(function () {
+      this.section = 'skills';
+      this.exportSkills = sinon.stub();
+      this.refresh = sinon.stub();
+    });
+
+    test('it should display draft view', async function(assert) {
+      // when
+      await render(hbs` <Competence::CompetenceActions
+            @section={{this.section}}
+            @refresh={{this.refresh}}
+            @selectView={{this.selectView}}
+            @shareSkills={{this.exportSkills}}/>`);
+
+      await click('[data-test-select-draft-view]');
+
+      // then
+      assert.ok(selectViewStub.calledOnce);
+      assert.ok(selectViewStub.calledWith('draft'));
+    });
+
+    test('it should have draft active tab if view is set to `draft`', async function(assert) {
+      // given
+      this.view = 'draft';
+
+      // when
+      await render(hbs` <Competence::CompetenceActions
+            @section={{this.section}}
+            @refresh={{this.refresh}}
+            @selectView={{this.selectView}}
+            @view={{this.view}}
+            @shareSkills={{this.exportSkills}}/>`);
+
+      // then
+      assert.dom('[data-test-select-draft-view]').hasClass('active');
+    });
+  });
 
   test('it renders', async function(assert) {
 
