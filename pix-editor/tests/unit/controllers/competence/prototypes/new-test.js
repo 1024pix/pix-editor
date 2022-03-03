a1eb9a7a8d37deee20b07279cffe9f5302de634a
jeremie <jeremie.jade@pix.fr>
2022-03-03 17:21:21 +0100

    Fix setVersion for new prototype
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- pix-editor/tests/unit/controllers/competence/prototypes/new-test.js
+++ pix-editor/tests/unit/controllers/competence/prototypes/new-test.js
@@ -1,12 +1,71 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+import setupIntlRenderingTest from '../../../../setup-intl-rendering';
 
 module('Unit | Controller | competence/prototypes/new', function(hooks) {
-  setupTest(hooks);
+  setupIntlRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:competence/prototypes/new');
-    assert.ok(controller);
+  module('#_setVersion', function(hooks) {
+    let controller,
+      messageStub,
+      prototype1_1,
+      savePrototypeStub,
+      newPrototype1_2,
+      skill;
+
+    hooks.beforeEach(function() {
+      controller = this.owner.lookup('controller:competence/prototypes/new');
+      messageStub = sinon.stub();
+      controller._message = messageStub;
+
+      const store = this.owner.lookup('service:store');
+
+      prototype1_1 = store.createRecord('challenge', {
+        id: 'rec_proto1_1',
+        pixId: 'pix_proto1_1',
+        genealogy: 'Prototype 1',
+        version: 1,
+      });
+
+      newPrototype1_2 = store.createRecord('challenge',{
+        id: 'rec_proto1_2',
+        pixId: 'pix_proto1_1',
+        genealogy: 'Prototype 1',
+      });
+      savePrototypeStub = sinon.stub().resolves(newPrototype1_2);
+      newPrototype1_2.save = savePrototypeStub;
+
+      skill = store.createRecord('skill',{
+        id: 'rec_proto1_2',
+        pixId: 'pix_proto1_1',
+        genealogy: 'Prototype 1',
+        challenges:[prototype1_1, newPrototype1_2],
+      });
+    });
+
+    test('it should set proper version of a prototype', async function(assert) {
+      // when
+      await controller._setVersion(newPrototype1_2);
+
+      // then
+      assert.equal(newPrototype1_2.version, 2);
+      assert.ok(messageStub.calledOnce);
+      assert.ok(messageStub.calledWith('Nouvelle version : 2', true));
+      assert.ok(savePrototypeStub.calledOnce);
+    });
+
+    test('it should not set version if is workbench prototype', async function(assert) {
+      // given
+      skill.name = '@workbench';
+
+      // when
+      await controller._setVersion(newPrototype1_2);
+
+      // then
+      assert.equal(newPrototype1_2.version, undefined);
+      assert.notOk(messageStub.calledOnce);
+      assert.notOk(savePrototypeStub.calledOnce);
+    });
   });
 });
