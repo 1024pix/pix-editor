87845ace15f15968c10140abcfe8c62c7c1a28ea
jeremie <jeremie.jade@pix.fr>
2022-03-14 15:26:53 +0100

    Fix loggin form
    co-authored-by: Guillaume Lazzara <guillaume.lazzara@pix.fr>
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

---
+++ pix-editor/app/routes/authenticated.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class AuthenticatedRoute extends Route {
+  @service auth;
+  @service router;
+
+  beforeModel() {
+    if (!this.auth.connected) {
+      this.router.transitionTo('index');
+    }
+  }
+}
