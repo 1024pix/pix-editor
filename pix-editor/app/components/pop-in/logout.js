82ca8b098018d1a79f51f9179432da62ab4fecf0
jeremie <jeremie.jade@pix.fr>
2022-03-14 18:08:13 +0100

    Add userName in sentry report
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

--- pix-editor/app/components/pop-in/logout.js
+++ pix-editor/app/components/pop-in/logout.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import * as Sentry from '@sentry/ember';
 
 export default class LogoutComponent extends Component {
 
@@ -11,6 +12,7 @@ export default class LogoutComponent extends Component {
   logout() {
     event.preventDefault();
     this.auth.key = undefined;
+    Sentry.configureScope(scope => scope.setUser(null));
     this.window.reload();
   }
 }
