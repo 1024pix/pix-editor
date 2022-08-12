d1a91b653e7c94e76cb369c392c8e8d37fc276ad
Lise Quesnel <quesnel.lise@gmail.com>
2022-08-12 14:37:58 +0200

    feat(api): Add translations to Tubes

--- api/lib/domain/models/Tube.js
+++ api/lib/domain/models/Tube.js
@@ -9,15 +9,32 @@ module.exports = class Tube {
     practicalDescriptionFrFr,
     practicalDescriptionEnUs,
     competenceId,
-  } = {}) {
+  } = {}, locale) {
     this.id = id;
     this.name = name;
     this.title = title;
     this.description = description;
-    this.practicalTitleFrFr = practicalTitleFrFr;
-    this.practicalTitleEnUs = practicalTitleEnUs;
-    this.practicalDescriptionFrFr = practicalDescriptionFrFr;
-    this.practicalDescriptionEnUs = practicalDescriptionEnUs;
     this.competenceId = competenceId;
+
+    if (!locale) {
+      this.practicalTitleFrFr = practicalTitleFrFr;
+      this.practicalDescriptionFrFr = practicalDescriptionFrFr;
+      this.practicalTitleEnUs = practicalTitleEnUs;
+      this.practicalDescriptionEnUs = practicalDescriptionEnUs;
+    }
+
+    if (locale === 'fr' || locale === 'fr-fr') {
+      this.practicalTitle = practicalTitleFrFr;
+      this.practicalTitleFrFr = practicalTitleFrFr;
+      this.practicalDescription = practicalDescriptionFrFr;
+      this.practicalDescriptionFrFr = practicalDescriptionFrFr;
+    }
+
+    if (locale === 'en') {
+      this.practicalTitle = practicalTitleEnUs;
+      this.practicalTitleEnUs = practicalTitleEnUs;
+      this.practicalDescription = practicalDescriptionEnUs;
+      this.practicalDescriptionEnUs = practicalDescriptionEnUs;
+    }
   }
 };
