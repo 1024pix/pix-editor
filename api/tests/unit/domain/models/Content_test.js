c87076ce46e1ee6feb7387332d765ac74261ae28
Marie-Hélène <marie-helene.rots@pix.fr>
2022-08-19 11:57:15 +0200

    refactor(api): add Skill domain builder and use it in content test

--- api/tests/unit/domain/models/Content_test.js
+++ api/tests/unit/domain/models/Content_test.js
@@ -2,7 +2,6 @@ const { expect, domainBuilder } = require('../../../test-helper');
 const Content = require('../../../../lib/domain/models/Content');
 const Challenge = require('../../../../lib/domain/models/Challenge');
 const Course = require('../../../../lib/domain/models/Course');
-const Skill = require('../../../../lib/domain/models/Skill');
 const Tube = require('../../../../lib/domain/models/Tube');
 const Tutorial = require('../../../../lib/domain/models/Tutorial');
 
@@ -36,12 +35,29 @@ describe('Unit | Domain | Content', () => {
         descriptionEnUs: 'descriptionEnCompetenceA',
         fullName: '1.2 nameCompetenceA',
       });
+      const skillAirtable = domainBuilder.buildSkillAirtableDataObject({
+        id: 'recSkillA',
+        name: 'nameSkillA',
+        hintFrFr: 'hintFr',
+        hintEnUs: 'hintEn',
+        hintStatus: 'hintStatusA',
+        tutorialIds: ['tutorialId1'],
+        learningMoreTutorialIds: ['learningMoreTutorialId1', 'learningMoreTutorialId2'],
+        competenceId: 'competenceId1',
+        pixValue: 1.8,
+        status: 'actif',
+        tubeId: 'tubeIdB',
+        description: 'skill description A',
+        level: 2,
+        internationalisation: 'Monde',
+        version: 2,
+      });
       data = {
         areas: [areaAirtable],
         competences: [competenceAirtable],
         challenges: [{ id: 123 }],
         courses: [{ id: 123 }],
-        skills: [{ id: 123, hintFrFr: 'indice', hintEnUs: 'hint' }],
+        skills: [skillAirtable],
         tubes: [{ id: 123, practicalTitleFrFr: 'titre pratique', practicalTitleEnUs: 'practical title', practicalDescriptionFrFr: 'description pratique', practicalDescriptionEnUs: 'practical description' }],
         tutorials: [{ id: 123 }],
       };
@@ -81,11 +97,29 @@ describe('Unit | Domain | Content', () => {
         descriptionFrFr: 'descriptionFrCompetenceA',
         descriptionEnUs: 'descriptionEnCompetenceA',
       });
+      const expectedSkill = domainBuilder.buildSkill({
+        id: 'recSkillA',
+        name: 'nameSkillA',
+        hintFrFr: 'hintFr',
+        hintEnUs: 'hintEn',
+        hintStatus: 'hintStatusA',
+        tutorialIds: ['tutorialId1'],
+        learningMoreTutorialIds: ['learningMoreTutorialId1', 'learningMoreTutorialId2'],
+        competenceId: 'competenceId1',
+        pixValue: 1.8,
+        status: 'actif',
+        tubeId: 'tubeIdB',
+        description: 'skill description A',
+        level: 2,
+        internationalisation: 'Monde',
+        version: 2,
+      });
+
       expect(content.areas).to.deepEqualArray([expectedArea]);
       expect(content.competences).to.deepEqualArray([expectedCompetence]);
       expect(content.challenges[0]).to.be.instanceOf(Challenge);
       expect(content.courses[0]).to.be.instanceOf(Course);
-      expect(content.skills[0]).to.be.instanceOf(Skill);
+      expect(content.skills).to.deepEqualArray([expectedSkill]);
       expect(content.tubes[0]).to.be.instanceOf(Tube);
       expect(content.tutorials[0]).to.be.instanceOf(Tutorial);
     });
