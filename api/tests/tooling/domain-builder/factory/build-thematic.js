8af6d285d0ddb9fe6c8f28021c283b3d95fc6768
Lise Quesnel <quesnel.lise@gmail.com>
2022-08-24 17:29:52 +0200

    refactor(api): add Thematic domain builder and use it in content test

--- api/tests/tooling/domain-builder/factory/build-thematic.js
+++ api/tests/tooling/domain-builder/factory/build-thematic.js
@@ -1,18 +1,21 @@
-module.exports = function buildThematic(
-  {
-    id = 'recFvllz2Ckz',
-    name = 'Nom de la thématique',
-    nameEnUs = 'Thematic\'s name',
-    competenceId = 'recCompetence0',
-    tubeIds = ['recTube0'],
-    index = 0
-  } = {}) {
-  return {
+const Thematic = require('../../../../lib/domain/models/Thematic');
+
+const buildThematic = function({
+  id = 'recFvllz2Ckz',
+  name = 'Nom de la thématique',
+  nameEnUs = 'Thematic\'s name',
+  competenceId = 'recCompetence0',
+  tubeIds = ['recTube0'],
+  index = 0,
+} = {}) {
+  return new Thematic({
     id,
     name,
     nameEnUs,
     competenceId,
     tubeIds,
     index,
-  };
+  });
 };
+
+module.exports = buildThematic;
