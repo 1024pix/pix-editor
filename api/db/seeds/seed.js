6fe2b7e68f717ecf8fc263bd016e93da69739574
jeremie <jeremie.jade@pix.fr>
2022-09-06 10:57:51 +0200

    Add 'readpixonly' user on seed

--- api/db/seeds/seed.js
+++ api/db/seeds/seed.js
@@ -7,7 +7,7 @@ exports.seed = (knex) => {
     trigram: 'DEV',
     name: 'Utilisateur pour le développement',
     access: 'admin',
-    apiKey: process.env.REVIEW_APP_USER_API_KEY || adminUserApiKey,
+    apiKey: process.env.REVIEW_APP_ADMIN_USER_API_KEY || adminUserApiKey,
   });
 
   databaseBuilder.factory.buildUser({
@@ -15,6 +15,13 @@ exports.seed = (knex) => {
     name: 'Editeur pour le développement',
     access: 'editor',
     apiKey: process.env.REVIEW_APP_EDITOR_USER_API_KEY || defaultEditorUserApiKey,
+  });
+
+  databaseBuilder.factory.buildUser({
+    trigram: 'RPO',
+    name: 'Lecteur pix pour le développement',
+    access: 'readpixonly',
+    apiKey: process.env.REVIEW_APP_READ_PIX_ONLY_USER_API_KEY || readPixOnlyUserApiKey,
   });
 
   databaseBuilder.factory.buildTraining({
@@ -58,3 +65,4 @@ exports.seed = (knex) => {
 
 const adminUserApiKey = !process.env.REVIEW_APP && '8d03a893-3967-4501-9dc4-e0aa6c6dc442';
 const defaultEditorUserApiKey = !process.env.REVIEW_APP && 'adaf3eee-09dc-4f9a-a504-ff92e74c9d0f';
+const readPixOnlyUserApiKey = !process.env.REVIEW_APP && '09ae36c4-11e1-4212-ae51-e5719d142f57';
