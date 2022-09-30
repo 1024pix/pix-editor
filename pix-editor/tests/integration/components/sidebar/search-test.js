41d683933a65c19139e6ee0f9d5abb83220e3443
Thomasevano <thomas.evano@pix.fr>
2022-09-30 12:13:16 +0200

    Hide search field for read_pix_only users

--- pix-editor/tests/integration/components/sidebar/search-test.js
+++ pix-editor/tests/integration/components/sidebar/search-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
 
 module('Integration | Component | sidebar/search', function(hooks) {
   setupRenderingTest(hooks);
@@ -9,8 +10,9 @@ module('Integration | Component | sidebar/search', function(hooks) {
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.mayShowFrameworkList = sinon.stub().returns(true);
 
-    await render(hbs`<Sidebar::Search />`);
+    await render(hbs`<Sidebar::Search @mayShowFrameworkList={{this.mayShowFrameworkList}} />`);
 
     assert.dom('.sidebar-search').exists();
   });
