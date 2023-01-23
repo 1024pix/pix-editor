f37a4e5d2ad21445a925e5032c344e8934bfbb6d
François de Metz <francois.de-metz@pix.fr>
2023-01-23 17:41:51 +0100

    Do not run jobs in a separate process on test env

--- api/lib/infrastructure/scheduled-jobs/check-urls-job.js
+++ api/lib/infrastructure/scheduled-jobs/check-urls-job.js
@@ -2,7 +2,8 @@ const createQueue = require('./create-queue');
 const config = require('../../config');
 
 const queue = createQueue('check-urls-queue');
-queue.process(__dirname + '/check-urls-job-processor.js');
+const processFile = __dirname + '/check-urls-job-processor.js';
+queue.process(process.env.NODE_ENV === 'test' ? require(processFile) : processFile);
 
 const checkUrlsJobOptions = {
   attempts: config.scheduledJobs.attempts,
