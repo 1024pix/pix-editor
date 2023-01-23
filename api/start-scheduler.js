743c89d7c0840b6905a12138a0ac2ed8df85948d
Pierre TOP (TOPI) <ptop@octo.com>
2023-01-23 11:12:14 +0100

    Run scheduler in a separate container

---
+++ api/start-scheduler.js
@@ -0,0 +1,29 @@
+const releaseJob = require('./lib/infrastructure/scheduled-jobs/release-job');
+const logger = require('./lib/infrastructure/logger');
+const { queue: checkUrlQueue } = require('./lib/infrastructure/scheduled-jobs/check-urls-job');
+
+const main = async () => {
+  try {
+    releaseJob.schedule();
+    logger.info('Scheduler has been started');
+  } catch (err) {
+    logger.error(err);
+    process.exit(1);
+  }
+};
+async function exitOnSignal(signal) {
+  logger.info(`Received signal ${signal}. Closing queues before exiting.`);
+  try {
+    await checkUrlQueue.close();
+    await releaseJob.queue.close();
+    process.exit(0);
+  } catch (err) {
+    logger.error(err);
+    process.exit(1);
+  }
+}
+
+process.on('SIGTERM', () => { exitOnSignal('SIGTERM'); });
+process.on('SIGINT', () => { exitOnSignal('SIGINT'); });
+
+main();
