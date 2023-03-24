f7de1c7619a9b055d62c14d82c22b25fd368496e
jeremie <jeremie.jade@pix.fr>
2023-03-24 12:10:43 +0100

    Add the solutionToDisplay's urls to the url checker script

--- api/tests/unit/domain/usecases/validate-urls-from-release_test.js
+++ api/tests/unit/domain/usecases/validate-urls-from-release_test.js
@@ -5,6 +5,7 @@ const {
   findUrlsInstructionFromChallenge,
   findUrlsProposalsFromChallenge,
   findUrlsSolutionFromChallenge,
+  findUrlsSolutionToDisplayFromChallenge,
   findUrlsFromChallenges,
   getLiveChallenges,
   findUrlsFromTutorials
@@ -107,6 +108,28 @@ describe('Check urls from release', function() {
     });
   });
 
+  describe('#findUrlsSolutionToDisplayFromChallenge', function() {
+    it('should not find url solution to display from a challenge when there is no url', function() {
+      const challenge = {
+        id: 'challenge123',
+        solutionToDisplay: 'solution to display',
+      };
+      const urls = findUrlsSolutionToDisplayFromChallenge(challenge);
+
+      expect(urls).to.deep.equal([]);
+    });
+
+    it('should find url solution to display from a challenge', function() {
+      const challenge = {
+        id: 'challenge123',
+        solutionToDisplay: 'solution to display [link](https://example.com/)',
+      };
+      const urls = findUrlsSolutionToDisplayFromChallenge(challenge);
+
+      expect(urls).to.deep.equal(['https://example.com/']);
+    });
+  });
+
   describe('#findUrlsFromChallenges', function() {
     it('should find urls from challenges', function() {
       const release = {
@@ -136,6 +159,13 @@ describe('Check urls from release', function() {
           proposals: 'proposals [link](https://example.fr/)',
           skillId: undefined,
           status: 'validé',
+        },
+        {
+          id: 'challenge3',
+          instruction: 'instructions',
+          solutionToDisplay: 'solution to display https://solutionToDisplay_example.org/',
+          skillId: 'skill2',
+          status: 'validé',
         }
       ];
 
@@ -144,6 +174,7 @@ describe('Check urls from release', function() {
         { id: '@mySkill1;challenge1;validé', url: 'https://other_example.net/' },
         { id: '@mySkill1;challenge1;validé', url: 'https://solution_example.net/' },
         { id: ';challenge2;validé', url: 'https://example.fr/' },
+        { id: '@mySkill2;challenge3;validé', url: 'https://solutionToDisplay_example.org/' },
       ];
 
       const urls = findUrlsFromChallenges(challenges, release);
