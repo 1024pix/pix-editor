77fa1575964899ecd0a285dc65ddd0bd3e0755ff
Pierre TOP (TOPI) <ptop@octo.com>
2023-04-14 16:16:03 +0200

    Revert "Remove unused configuration"
    This reverts commit 2fae959b0203d42fe3e3d3e3d477b9d523358fe4.

--- api/lib/infrastructure/validate-environement-variables.js
+++ api/lib/infrastructure/validate-environement-variables.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
+  LOG_OPS_METRICS: Joi.string().valid('true', 'false').optional(),
   OPS_EVENT_EACH_SECONDS: Joi.number().integer().min(1).optional()
 }).options({ allowUnknown: true });
 
