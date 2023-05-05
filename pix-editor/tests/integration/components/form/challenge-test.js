c10f6776f0b0544b7d0e1275f98b2454b1ed75f3
Eithliu <iris.benoit@pix.fr>
2023-05-05 11:25:39 +0200

    feature(pix-editor): add checkbox and test for qcm
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- pix-editor/tests/integration/components/form/challenge-test.js
+++ pix-editor/tests/integration/components/form/challenge-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, find, findAll, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
 
@@ -33,5 +33,45 @@ module('Integration | Component | challenge-form', function(hooks) {
     ['data-test-format-field', 'data-test-tolerence-fields', 'data-test-suggestion-field'].forEach(field => {
       assert.dom(`[${field}]`).doesNotExist();
     });
+  });
+
+  test('it should display autochecked checkbox if challenge type is `QCM`', async function(assert) {
+    // Given
+    const store = this.owner.lookup('service:store');
+    const challengeData = store.createRecord('challenge',{
+      id: 'recChallenge0',
+      genealogy: 'Prototype 1',
+    });
+    this.set('challengeData', challengeData);
+
+    // When
+    await render(hbs`<Form::Challenge @challenge={{this.challengeData}} @edition={{true}}/>`);
+
+    await click(find('[data-test-select-type] .ember-basic-dropdown-trigger'));
+    await click(findAll('.ember-power-select-option')[1]);
+
+    // Then
+    assert.dom('[data-test-checkbox-shuffle]').exists();
+    assert.dom('[data-test-checkbox-shuffle] > input').isChecked();
+  });
+
+  test('it should display autochecked checkbox if challenge type is `QCU`', async function(assert) {
+    // Given
+    const store = this.owner.lookup('service:store');
+    const challengeData = store.createRecord('challenge',{
+      id: 'recChallenge0',
+      genealogy: 'Prototype 1',
+    });
+    this.set('challengeData', challengeData);
+
+    // When
+    await render(hbs`<Form::Challenge @challenge={{this.challengeData}} @edition={{true}}/>`);
+
+    await click(find('[data-test-select-type] .ember-basic-dropdown-trigger'));
+    await click(findAll('.ember-power-select-option')[0]);
+
+    // Then
+    assert.dom('[data-test-checkbox-shuffle]').exists();
+    assert.dom('[data-test-checkbox-shuffle] > input').isChecked();
   });
 });
