15884d2244e1948500bc1ca374f126a1d6d6aded
Laura Bergoens <laura.bergoens@pix.fr>
2023-06-09 16:17:01 +0200

    fix loading on child pages

--- pix-editor/config/environment.js
+++ pix-editor/config/environment.js
@@ -9,7 +9,7 @@ module.exports = function(environment) {
     modulePrefix: 'pixeditor',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
