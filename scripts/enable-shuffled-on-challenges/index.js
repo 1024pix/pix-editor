2d41e792c3b6d440016d54addbef25a5f5b8be51
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-06-12 18:01:45 +0200

    Find excluded skills ids in airtable

--- scripts/enable-shuffled-on-challenges/index.js
+++ scripts/enable-shuffled-on-challenges/index.js
@@ -1,24 +1,62 @@
+const Airtable = require('airtable');
 const { resolve } = require('path');
 const { performance } = require('perf_hooks');
 const { readFile, utils: xlsxUtils } = require('xlsx');
 
-const enableShuffledOnChallenges = async () => {
-  const excludes = await readExcludes();
-  console.log(excludes);
+const enableShuffledOnChallenges = async ({ airtableClient }) => {
+  const excludedSkillIds = await readExcludes({ airtableClient });
+  console.log(excludedSkillIds);
 };
 
-async function readExcludes() {
+/**
+ * @param {{
+ *   airtableClient: Airtable.Base
+ * }} config
+ */
+async function readExcludes({ airtableClient }) {
   const excludesFile = resolve(__dirname, 'excludes.xlsx');
   const wb = readFile(excludesFile);
   const ws = wb.Sheets[wb.SheetNames[0]];
   const excludes = xlsxUtils.sheet_to_json(ws, { header: [undefined, 'skillName'], range: 1 });
-  return excludes;
+
+  const airtableSkills = await airtableClient.table('Acquis').select({ fields:['Nom'] }).all();
+
+  return excludes.map(({ skillName }) => {
+    return {
+      skillIds: airtableSkills.filter(
+        (skill) => skillName.localeCompare(skill.get('Nom'), 'fr', { sensitivity: 'base' }) === 0
+      ).map((skill) => skill.getId()),
+      skillName,
+    };
+  }).filter((exclude) => {
+    const hasSkillId = exclude.skillIds.length > 0;
+    if (!hasSkillId) {
+      console.error(`Skill "${exclude.skillName}" from excludes not found in Airtable`);
+    }
+    return hasSkillId;
+  }).flatMap(({ skillIds }) => skillIds);
+}
+
+function createAirtableClient({ apiKey, base }) {
+  return new Airtable({ apiKey }).base(base);
 }
 
 async function main() {
   const startTime = performance.now();
-  console.log(`Script ${__filename} has started`);
-  await enableShuffledOnChallenges();
+
+  const {
+    AIRTABLE_API_KEY: airtableApiKey,
+    AIRTABLE_BASE: airtableBase,
+  } = process.env;
+
+  console.log(`Script ${__filename} has started`, {
+    airtableApiKey,
+    airtableBase,
+  });
+
+  const airtableClient = createAirtableClient({ apiKey: airtableApiKey, base: airtableBase });
+  await enableShuffledOnChallenges({ airtableClient });
+
   const endTime = performance.now();
   const duration = Math.round(endTime - startTime);
   console.log(`Script has ended: took ${duration} milliseconds`);
