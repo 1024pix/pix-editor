ba4671c1dbff2a9374d20cd5fb50c8bd93063f64
François de Metz <francois.de-metz@pix.fr>
2023-06-14 15:17:45 +0200

    Add special public test

--- api/lib/infrastructure/utils/pre-response-utils.js
+++ api/lib/infrastructure/utils/pre-response-utils.js
@@ -1,6 +1,7 @@
 const errorManager = require('./error-manager');
 const { DomainError } = require('../../domain/errors');
 const { InfrastructureError } = require('../errors');
+const config = require('../../config');
 
 function catchDomainAndInfrastructureErrors(request, h) {
   const response = request.response;
@@ -12,7 +13,7 @@ function catchDomainAndInfrastructureErrors(request, h) {
     if (response.output.statusCode === 404 &&
         request.method === 'get' &&
         !request.path.startsWith('/api')) {
-      return h.file('public/pix-editor/index.html').code(200);
+      return h.file(`${config.hapi.publicDir}/pix-editor/index.html`).code(200);
     }
   }
 
