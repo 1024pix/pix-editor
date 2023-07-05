cbbb3e1f78e70967e95d16c900a94ac4a3e7e779
Laura Bergoens <laura.bergoens@pix.fr>
2023-07-05 15:48:05 +0200

    make exports more visible

--- api/lib/application/static-courses/static-courses.js
+++ api/lib/application/static-courses/static-courses.js
@@ -1,7 +1,8 @@
 const _ = require('lodash');
+const { extractParameters } = require('../../infrastructure/utils/query-params-utils');
 const staticCourseRepository = require('../../infrastructure/repositories/static-course-repository');
 const staticCourseSerializer = require('../../infrastructure/serializers/jsonapi/static-course-serializer');
-const { extractParameters } = require('../../infrastructure/utils/query-params-utils');
+
 const DEFAULT_PAGE = {
   number: 1,
   size: 10,
@@ -9,20 +10,23 @@ const DEFAULT_PAGE = {
 };
 
 module.exports = {
-  async findSummaries(request, h) {
-    const { page } = extractParameters(request.query);
-    const { results: staticCourseSummaries, meta } = await staticCourseRepository.findSummaries({ page: _normalizePage(page) });
-    return h.response(staticCourseSerializer.serializeSummary(staticCourseSummaries, meta));
-  },
-
-  async get(request, h) {
-    const staticCourseId = request.params.id;
-    const staticCourse = await staticCourseRepository.get(staticCourseId);
-    return h.response(staticCourseSerializer.serialize(staticCourse));
-  }
+  findSummaries,
+  get,
 };
 
-function _normalizePage(page) {
+async function findSummaries(request, h) {
+  const { page } = extractParameters(request.query);
+  const { results: staticCourseSummaries, meta } = await staticCourseRepository.findSummaries({ page: normalizePage(page) });
+  return h.response(staticCourseSerializer.serializeSummary(staticCourseSummaries, meta));
+}
+
+async function get(request, h) {
+  const staticCourseId = request.params.id;
+  const staticCourse = await staticCourseRepository.get(staticCourseId);
+  return h.response(staticCourseSerializer.serialize(staticCourse));
+}
+
+function normalizePage(page) {
   return {
     number: _.isInteger(page.number) && Math.sign(page.number) === 1 ? page.number : DEFAULT_PAGE.number,
     size: _.isInteger(page.size) && Math.sign(page.size) === 1 ? Math.min(page.size, DEFAULT_PAGE.maxSize) : DEFAULT_PAGE.size,
--- api/lib/infrastructure/repositories/static-course-repository.js
+++ api/lib/infrastructure/repositories/static-course-repository.js
@@ -8,75 +8,78 @@ const challengeDatasource = require('../datasources/airtable/challenge-datasourc
 const skillDatasource = require('../datasources/airtable/skill-datasource');
 
 module.exports = {
-  // TODO: when double read will be removed, move pagination process into knex
-  async findSummaries({ page }) {
-    const staticCoursesFromPG = await knex('static_courses')
-      .select('id', 'name', 'createdAt', 'challengeIds');
-
-    const staticCoursesFromAirtable = await courseDatasource.list();
-    const staticCoursesSummariesFirstBatch = staticCoursesFromAirtable.map((staticCourseFromAirtable) => {
-      return new StaticCourseSummary({
-        id: staticCourseFromAirtable.id,
-        name: staticCourseFromAirtable.name,
-        createdAt: staticCourseFromAirtable.createdAt,
-        challengeCount: (staticCourseFromAirtable.challenges || []).length,
-      });
-    });
-
-    const staticCoursesSummariesSecondBatch = staticCoursesFromPG.map((staticCourse) => {
-      return new StaticCourseSummary({
-        id: staticCourse.id,
-        name: staticCourse.name,
-        createdAt: staticCourse.createdAt,
-        challengeCount: staticCourse.challengeIds.split(',').length,
-      });
-    });
-
-    const rowCount = staticCoursesSummariesFirstBatch.length + staticCoursesSummariesSecondBatch.length;
-    const meta = { page: page.number, pageSize: page.size, pageCount: Math.ceil(rowCount / page.size), rowCount };
-    const results = _.chain(staticCoursesSummariesFirstBatch)
-      .concat(staticCoursesSummariesSecondBatch)
-      .orderBy('createdAt', 'desc')
-      .chunk(page.size)
-      .nth(page.number - 1).value() || [];
-    return { results, meta };
-  },
-
-  async get(id) {
-    const staticCourseFromPG = await knex('static_courses')
-      .select('*')
-      .where('id', id)
-      .first();
-    if (staticCourseFromPG) {
-      const challengeIds = staticCourseFromPG.challengeIds.split(',');
-      const challengeSummaries = await _findChallengeSummaries(challengeIds);
-      return new StaticCourse({
-        id: staticCourseFromPG.id,
-        name: staticCourseFromPG.name,
-        description: staticCourseFromPG.description,
-        createdAt: staticCourseFromPG.createdAt,
-        updatedAt: staticCourseFromPG.updatedAt,
-        challengeSummaries,
-      });
-    }
-    const [staticCourseFromAirtable] = await courseDatasource.filter({ filter: { ids: [id] } });
-    if (staticCourseFromAirtable) {
-      const challengeIds = staticCourseFromAirtable.challenges || [];
-      const challengeSummaries = await _findChallengeSummaries(challengeIds);
-      return new StaticCourse({
-        id: staticCourseFromAirtable.id,
-        name: staticCourseFromAirtable.name || null,
-        description: staticCourseFromAirtable.description || null,
-        createdAt: staticCourseFromAirtable.createdAt,
-        updatedAt: staticCourseFromAirtable.updatedAt,
-        challengeSummaries,
-      });
-    }
-    return null;
-  }
+  findSummaries,
+  get,
 };
 
-async function _findChallengeSummaries(challengeIds) {
+// TODO: when double read will be removed, move pagination process into knex
+async function findSummaries({ page }) {
+  const staticCoursesFromPG = await knex('static_courses')
+    .select('id', 'name', 'createdAt', 'challengeIds');
+
+  const staticCoursesFromAirtable = await courseDatasource.list();
+  const staticCoursesSummariesFirstBatch = staticCoursesFromAirtable.map((staticCourseFromAirtable) => {
+    return new StaticCourseSummary({
+      id: staticCourseFromAirtable.id,
+      name: staticCourseFromAirtable.name,
+      createdAt: staticCourseFromAirtable.createdAt,
+      challengeCount: (staticCourseFromAirtable.challenges || []).length,
+    });
+  });
+
+  const staticCoursesSummariesSecondBatch = staticCoursesFromPG.map((staticCourse) => {
+    return new StaticCourseSummary({
+      id: staticCourse.id,
+      name: staticCourse.name,
+      createdAt: staticCourse.createdAt,
+      challengeCount: staticCourse.challengeIds.split(',').length,
+    });
+  });
+
+  const rowCount = staticCoursesSummariesFirstBatch.length + staticCoursesSummariesSecondBatch.length;
+  const meta = { page: page.number, pageSize: page.size, pageCount: Math.ceil(rowCount / page.size), rowCount };
+  const results = _.chain(staticCoursesSummariesFirstBatch)
+    .concat(staticCoursesSummariesSecondBatch)
+    .orderBy('createdAt', 'desc')
+    .chunk(page.size)
+    .nth(page.number - 1).value() || [];
+  return { results, meta };
+}
+
+async function get(id) {
+  const staticCourseFromPG = await knex('static_courses')
+    .select('*')
+    .where('id', id)
+    .first();
+  if (staticCourseFromPG) {
+    const challengeIds = staticCourseFromPG.challengeIds.split(',');
+    const challengeSummaries = await findChallengeSummaries(challengeIds);
+    return new StaticCourse({
+      id: staticCourseFromPG.id,
+      name: staticCourseFromPG.name,
+      description: staticCourseFromPG.description,
+      createdAt: staticCourseFromPG.createdAt,
+      updatedAt: staticCourseFromPG.updatedAt,
+      challengeSummaries,
+    });
+  }
+  const [staticCourseFromAirtable] = await courseDatasource.filter({ filter: { ids: [id] } });
+  if (staticCourseFromAirtable) {
+    const challengeIds = staticCourseFromAirtable.challenges || [];
+    const challengeSummaries = await findChallengeSummaries(challengeIds);
+    return new StaticCourse({
+      id: staticCourseFromAirtable.id,
+      name: staticCourseFromAirtable.name || null,
+      description: staticCourseFromAirtable.description || null,
+      createdAt: staticCourseFromAirtable.createdAt,
+      updatedAt: staticCourseFromAirtable.updatedAt,
+      challengeSummaries,
+    });
+  }
+  return null;
+}
+
+async function findChallengeSummaries(challengeIds) {
   const challengesFromAirtable = await challengeDatasource.filter({ filter: { ids: challengeIds } });
   const skillIds = challengesFromAirtable.map(({ skillId }) => skillId);
   const skillsFromAirtable = await skillDatasource.filter({ filter: { ids: skillIds } });
