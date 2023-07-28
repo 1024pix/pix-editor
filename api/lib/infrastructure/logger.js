5350443142552ff6f4689ddb081f533e19fdbcb5
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-07-28 09:54:44 +0200

    Fix prettyPrint

--- api/lib/infrastructure/logger.js
+++ api/lib/infrastructure/logger.js
@@ -3,12 +3,19 @@ const settings = require('../config');
 
 const nullDestination = { write() {} };
 
-const logger = pino({
-  level: settings.logging.logLevel,
-  redact: ['req.headers.authorization'],
-  prettyPrint: settings.logging.prettyPrint,
-},
-(settings.logging.enabled) ? pino.destination() : nullDestination);
+const logger = pino(
+  {
+    level: settings.logging.logLevel,
+    redact: ['req.headers.authorization'],
+    transport: settings.logging.prettyPrint && {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
+    },
+  },
+  settings.logging.enabled ? pino.destination() : nullDestination
+);
 
 logger.debug('DEBUG logs enabled');
 logger.trace('TRACE logs enabled');
