72ddc7017b7f41178c26d3dd31059417a4ebc2df
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-08-01 11:16:08 +0200

    :hammer: scripts: add script for alpha and delta update from csv file

---
+++ scripts/populate-alpha-and-delta-column-with-csv/index.js
@@ -0,0 +1,112 @@
+// Populate the alpha and dela columns
+// You must have 1 file:
+//  - a csv file with challengId, alpha and beta columns
+// To run the script:
+// > cd scripts
+// > npm ci
+// > AIRTABLE_API_KEY=XXX AIRTABLE_BASE=XXXX node populate-alpha-and-delta-column-with-csv/
+
+const _ = require('lodash');
+const fs = require('fs');
+const Airtable = require('airtable');
+const { parseString } = require('@fast-csv/parse');
+const ProgressBar = require('progress');
+
+const HEADERS_MAPPING = {
+  items: 'id',
+  difficulties: 'delta',
+  discriminants: 'alpha',
+};
+
+function parseData(csvData) {
+  return new Promise((resolve, reject) => {
+    const result = [];
+
+    parseString(csvData, { headers: (headers) => headers.map((h) => HEADERS_MAPPING[h]) })
+      .on('error', (error) => {
+        console.error(error);
+        reject(error);
+      })
+      .on('data', (row) => {
+        result.push(row);
+      })
+      .on('end', () => resolve(result));
+  });
+}
+
+async function findAirtableIds(base, challengesWithPersistentIds) {
+  const challengesWithPersistentIdsWithChunk = _.chunk(challengesWithPersistentIds, 100);
+  const promises = challengesWithPersistentIdsWithChunk.map((chunk) => {
+    return base
+      .select({
+        fields: ['Record ID', 'id persistant'],
+        filterByFormula: 'OR(' + chunk.map(({ id }) => `'${id}' = {id persistant}`).join(',') + ')',
+      })
+      .all();
+  });
+  const airtableRecords = (await Promise.all(promises)).flat();
+
+  return airtableRecords.map((airtableRecord) => {
+    const data = challengesWithPersistentIds.find((row) => row.id === airtableRecord.get('id persistant'));
+    return {
+      ...data,
+      id: airtableRecord.id
+    };
+  });
+}
+
+async function updateRecords(base, data) {
+  const payloadWithoutLimit = data.map(({ id, alpha, delta }) => {
+    return {
+      id,
+      fields: {
+        'Difficulté calculée': `${delta}`,
+        'Discrimination calculée': `${alpha}`
+      },
+    };
+  });
+  const payloadByChunk = _.chunk(payloadWithoutLimit, 10);
+  const bar = new ProgressBar('[:bar] :percent', {
+    total: payloadByChunk.length,
+    width: 50,
+  });
+  const promises = payloadByChunk.map((payload) => {
+    return new Promise((resolve, reject) => {
+      base.update(
+        payload,
+        (err) => {
+          bar.tick();
+          if (err) reject();
+          else resolve();
+        });
+    });
+  });
+  return Promise.all(promises);
+}
+
+function getBaseChallenges() {
+  const base = new Airtable({
+    apiKey: process.env.AIRTABLE_API_KEY
+  }).base(process.env.AIRTABLE_BASE);
+
+  return base('Epreuves');
+}
+
+async function main() {
+  const csv = fs.readFileSync('./file.csv', 'utf-8');
+
+  const base = getBaseChallenges();
+  const matchedData = await parseData(csv);
+  const matchedDataWithAirtableIds = await findAirtableIds(base, matchedData);
+  await updateRecords(base, matchedDataWithAirtableIds);
+}
+
+if (process.env.NODE_ENV !== 'test') {
+  main();
+}
+
+module.exports = {
+  parseData,
+  findAirtableIds,
+  updateRecords,
+};
