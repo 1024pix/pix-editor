daad3a13aeb7192a359bee7d93599fbf11919b06
alixdeschamps <deschamps.alix@gmail.com>
2023-08-09 17:32:53 +0200

    move to standalone scripts

---
+++ scripts/reverse-static-courses-challenges-order/index.js
@@ -0,0 +1,16 @@
+const { knex } = require('../../api/db/knex-database-connection');
+
+(async function() {
+  const staticCourses = await knex('static_courses')
+    .select('id', 'name', 'challengeIds');
+  for (const staticCourse of staticCourses) {
+    const challengeIds = staticCourse.challengeIds.split(',');
+    const reversedChallengeIds = challengeIds.reverse();
+    await knex('static_courses')
+      .where('id', staticCourse.id)
+      .update({
+        challengeIds: reversedChallengeIds.join(','),
+      });
+  }
+})();
+
