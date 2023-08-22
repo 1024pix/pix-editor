cc09b025996cffbe97a55c07a00173d861102b57
François de Metz <francois.de-metz@pix.fr>
2023-08-22 12:12:06 +0200

    Fix logging with pino-pretty enabled on tests

--- api/lib/infrastructure/logger.js
+++ api/lib/infrastructure/logger.js
@@ -3,18 +3,21 @@ const settings = require('../config');
 
 const nullDestination = { write() {} };
 
+let destination = pino.destination();
+
+if (settings.logging.prettyPrint) {
+  const pretty = require('pino-pretty');
+  destination = pretty({
+    colorize: true
+  });
+}
+
 const logger = pino(
   {
     level: settings.logging.logLevel,
     redact: ['req.headers.authorization'],
-    transport: settings.logging.prettyPrint && {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-      },
-    },
   },
-  settings.logging.enabled ? pino.destination() : nullDestination
+  settings.logging.enabled ? destination : nullDestination
 );
 
 logger.debug('DEBUG logs enabled');
