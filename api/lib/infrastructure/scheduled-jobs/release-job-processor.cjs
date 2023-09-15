d8b1d3528489ff9993a79bcc0e3b55300fcea6ca
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2023-09-15 08:52:20 +0200

    Fix scheduled jobs by passing job argument to processor
    Co-authored-by: <mickael.alibert@pix.fr>

--- api/lib/infrastructure/scheduled-jobs/release-job-processor.cjs
+++ api/lib/infrastructure/scheduled-jobs/release-job-processor.cjs
@@ -1,4 +1,4 @@
-module.exports = async function releaseJobProcessor() {
+module.exports = async function releaseJobProcessor(job) {
   const { default: processor } = await import('./release-job-processor.js');
-  return processor();
+  return processor(job);
 };
