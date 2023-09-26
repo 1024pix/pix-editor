7fcedce762733e28c7030751a8718ff403896ca4
François de Metz <francois.de-metz@pix.fr>
2023-09-26 10:26:11 +0200

    Remove airtableFields
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>

--- api/lib/infrastructure/translations/challenge.js
+++ api/lib/infrastructure/translations/challenge.js
@@ -3,11 +3,11 @@ import { Translation } from '../../domain/models/index.js';
 export const prefix = 'challenge.';
 
 const fields = [
-  { airtableField: 'Consigne', field: 'instruction' },
-  { airtableField: 'Consigne alternative', field: 'alternativeInstruction' },
-  { airtableField: 'Propositions', field: 'proposals' },
-  { airtableField: 'Bonnes réponses', field: 'solution' },
-  { airtableField: 'Bonnes réponses à afficher', field: 'solutionToDisplay' },
+  'instruction',
+  'alternativeInstruction',
+  'proposals',
+  'solution',
+  'solutionToDisplay',
 ];
 
 function getPrimaryLocaleFromChallenge(locales) {
@@ -19,8 +19,8 @@ export function extractFromChallenge(challenge) {
   const locale = getPrimaryLocaleFromChallenge(challenge.locales);
   const id = challenge.id;
   return fields
-    .filter(({ field }) => challenge[field])
-    .map(({ field }) => {
+    .filter((field) => challenge[field])
+    .map((field) => {
       return new Translation({
         key: `${prefix}${id}.${field}`,
         locale: locale,
