d9b707a830a4b14af83212c89415f435cb9081e5
François de Metz <francois.de-metz@pix.fr>
2023-10-11 11:37:11 +0200

    Remove the unused metrics plugins

--- api/lib/infrastructure/plugins/index.js
+++ api/lib/infrastructure/plugins/index.js
@@ -4,12 +4,10 @@ import Vision from '@hapi/vision';
 
 import * as config from '../../config.js';
 import * as Adminjs from './adminjs/index.js';
-import { Metrics } from './metrics.js';
 import * as Pino from './pino.js';
 import * as Sentry from './sentry.js';
 
 export const plugins = [
-  Metrics,
   Inert,
   Vision,
   Blipp,
