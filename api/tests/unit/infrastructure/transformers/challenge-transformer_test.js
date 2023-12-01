d4501085cfeb8fb1772b93258e419c20969158b7
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-12-01 15:16:33 +0100

    Add new tests on challenge transformer

--- api/tests/unit/infrastructure/transformers/challenge-transformer_test.js
+++ api/tests/unit/infrastructure/transformers/challenge-transformer_test.js
@@ -64,8 +64,160 @@ describe('Unit | Infrastructure | Challenge Transformer', function() {
             instruction: 'English instructions',
             alternativeInstruction: 'Alternative english instructions',
             proposals: '',
-          })
+          }),
         ]);
+      });
+    });
+
+    describe('when there is one localized challenge', () => {
+      it('should transform and translate challenge', () => {
+        // given
+        const attachments = [];
+        const localizedChallenge = new LocalizedChallenge({
+          id: 'nl-challenge-id',
+          challengeId: 'challenge-id',
+          locale: 'nl-be',
+        });
+        const challenge = domainBuilder.buildChallenge({
+          id: 'challenge-id',
+          translations: {
+            fr: {
+              instruction: 'Consigne',
+              alternativeInstruction: 'Consigne alternative',
+              proposals: 'Propositions',
+            },
+            'nl-be': {
+              instruction: 'Volgorde',
+              alternativeInstruction: 'Alternatieve instructie',
+              proposals: 'Voorstellen',
+            },
+          },
+          locales: ['fr', 'fr-fr'],
+        });
+
+        // when
+        const transform = challengeTransformer.createChallengeTransformer({ attachments, localizedChallenge });
+        const result = transform(challenge);
+
+        // then
+        expect(result).to.deep.equal(
+          _buildReleaseChallenge({
+            ...challenge,
+            id: 'nl-challenge-id',
+            locales: ['nl-be'],
+            instruction: 'Volgorde',
+            alternativeInstruction: 'Alternatieve instructie',
+            proposals: 'Voorstellen',
+          }),
+        );
+      });
+    });
+
+    describe('when there are no localized challenge at all', () => {
+      it('should transform challenge', () => {
+        // given
+        const attachments = [];
+        const challenge = domainBuilder.buildChallenge({
+          id: 'challenge-id',
+          translations: {
+            fr: {
+              instruction: 'Consigne',
+              alternativeInstruction: 'Consigne alternative',
+              proposals: 'Propositions',
+            },
+          },
+          locales: ['fr', 'fr-fr'],
+        });
+
+        // when
+        const transform = challengeTransformer.createChallengeTransformer({ attachments });
+        const result = transform(challenge);
+
+        // then
+        expect(result).to.deep.equal(
+          _buildReleaseChallenge(challenge),
+        );
+      });
+    });
+
+    describe('when there are attachments', () => {
+      it ('should add these on the challenge', () => {
+        // given
+        const attachments = [
+          { url: 'https://dl.example.com/attachment1.xlsx', challengeId: 'challenge-id' },
+          { url: 'https://dl.example.com/attachment1.csv', challengeId: 'challenge-id' },
+          { url: 'https://dl.example.com/attachment2.txt', challengeId: 'other-challenge-id' },
+        ];
+        const challenge = domainBuilder.buildChallenge({
+          id: 'challenge-id',
+          translations: {
+            fr: {
+              instruction: 'Consigne',
+              alternativeInstruction: 'Consigne alternative',
+              proposals: 'Propositions',
+            },
+          },
+          locales: ['fr', 'fr-fr'],
+        });
+
+        // when
+        const transform = challengeTransformer.createChallengeTransformer({ attachments });
+        const result = transform(challenge);
+
+        // then
+        expect(result).to.deep.equal(
+          _buildReleaseChallenge({
+            ...challenge,
+            attachments: [
+              'https://dl.example.com/attachment1.xlsx',
+              'https://dl.example.com/attachment1.csv',
+            ],
+          }),
+        );
+      });
+    });
+
+    describe('when there is an illustration attachments', () => {
+      it ('should add it on the challenge', () => {
+        // given
+        const attachments = [
+          {
+            type: 'illustration',
+            url: 'https://dl.example.com/illustration1.jpg',
+            alt: 'A dog making bubbles with his nose',
+            challengeId: 'challenge-id',
+          },
+          {
+            type: 'illustration',
+            url: 'https://dl.example.com/illustration2.jpg',
+            alt: 'A cat doing nothing cause he is useless',
+            challengeId: 'other-challenge-id',
+          },
+        ];
+        const challenge = domainBuilder.buildChallenge({
+          id: 'challenge-id',
+          translations: {
+            fr: {
+              instruction: 'Consigne',
+              alternativeInstruction: 'Consigne alternative',
+              proposals: 'Propositions',
+            },
+          },
+          locales: ['fr', 'fr-fr'],
+        });
+
+        // when
+        const transform = challengeTransformer.createChallengeTransformer({ attachments });
+        const result = transform(challenge);
+
+        // then
+        expect(result).to.deep.equal(
+          _buildReleaseChallenge({
+            ...challenge,
+            illustrationAlt: 'A dog making bubbles with his nose',
+            illustrationUrl: 'https://dl.example.com/illustration1.jpg',
+          }),
+        );
       });
     });
   });
