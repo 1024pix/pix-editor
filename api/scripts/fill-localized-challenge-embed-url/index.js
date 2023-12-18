11ac28d9928796b4bdc19b02ba2a9b3829ec0024
jeremie <jeremie.jade@pix.fr>
2023-12-18 15:07:02 +0100

    add script to fill localized challenges embed url
    co-authored-by: Iris Benoit <iris.benoit@pix.fr>
    Co-authored-by: Guillaume Lagorce <guillame.lagorce@pix.fr>
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

---
+++ api/scripts/fill-localized-challenge-embed-url/index.js
@@ -0,0 +1,56 @@
+import { localizedChallengeRepository } from '../../lib/infrastructure/repositories/index.js';
+import { fileURLToPath } from 'node:url';
+import Airtable from 'airtable';
+import { knex, disconnect } from '../../db/knex-database-connection.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const isLaunchedFromCommandLine = process.argv[1] === __filename;
+
+export function fillLocalizedChallengesEmbedUrl({ airtableClient }) {
+  return knex.transaction(async (transaction) => {
+    const localizedChallenges = await fetchLocalizedChallenges({ airtableClient });
+    for (const localizedChallenge of localizedChallenges) {
+      await localizedChallengeRepository.update({ localizedChallenge, transaction });
+    }
+  });
+}
+
+export async function fetchLocalizedChallenges({ airtableClient }) {
+  const allChallenges = await airtableClient
+    .table('Epreuves')
+    .select({
+      fields: [
+        'id persistant',
+        'Embed URL',
+      ],
+    })
+    .all();
+
+  return allChallenges.map((challenge) => {
+    const idPersistant = challenge.get('id persistant');
+    const embedUrl = challenge.get('Embed URL');
+    return {
+      id: idPersistant,
+      embedUrl,
+    };
+  });
+}
+
+async function main() {
+  if (!isLaunchedFromCommandLine) return;
+
+  try {
+    const airtableClient = new Airtable({
+      apiKey: process.env.AIRTABLE_API_KEY,
+    }).base(process.env.AIRTABLE_BASE);
+
+    await fillLocalizedChallengesEmbedUrl({ airtableClient });
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
+  } finally {
+    await disconnect();
+  }
+}
+
+main();
