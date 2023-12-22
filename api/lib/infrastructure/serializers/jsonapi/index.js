1ac1646af334941770dc9561f0c5f8189ef37bfa
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-12-22 17:51:31 +0100

    feat: add localized challenge serializer
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>
    Co-authored-by: Ce que tu veux <guillaume.lagorce@pix.fr>
    Co-authored-by: Iris Benoît <iris.benoit@pix.fr>

--- api/lib/infrastructure/serializers/jsonapi/index.js
+++ api/lib/infrastructure/serializers/jsonapi/index.js
@@ -1,6 +1,7 @@
 export * as challengeSerializer from './challenge-serializer.js';
 export * as configSerializer from './config-serializer.js';
 export * as errorSerializer from './error-serializer.js';
+export * as localizedChallengeSerializer from './localized-challenge-serializer.js';
 export * as missionSerializer from './mission-serializer.js';
 export * as staticCourseSerializer from './static-course-serializer.js';
 export * as userSerializer from './user-serializer.js';
