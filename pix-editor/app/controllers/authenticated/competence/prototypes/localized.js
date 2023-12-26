4c631b71c29a248d33afcfcfc72f1793051c41a1
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-12-26 15:10:01 +0100

    Add challenge-header component and use it for localized challenges
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

--- pix-editor/app/controllers/authenticated/competence/prototypes/localized.js
+++ pix-editor/app/controllers/authenticated/competence/prototypes/localized.js
@@ -1,18 +1,74 @@
-import Controller from '@ember/controller';
+import Controller, { inject as controller } from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class LocalizedController extends Controller {
+
+  @service router;
+
+  @controller('authenticated.competence') competenceController;
+  @controller('authenticated.competence.prototypes.single.alternatives') alternativesController;
 
   get previewUrl() {
     return new URL(`${this.model.challenge.get('preview')}?locale=${this.model.locale}`, window.location).href;
   }
 
   get challengeRoute() {
-    return this.model.challenge.get('isPrototype') ? 'authenticated.competence.prototypes.single' : 'authenticated.competence.prototypes.single.alternatives.single';
+    return this.isPrototype ? 'authenticated.competence.prototypes.single' : 'authenticated.competence.prototypes.single.alternatives.single';
   }
 
   get challengeModels() {
-    return this.model.challenge.get('isPrototype')
+    return this.isPrototype
       ? [this.model.challenge]
       : [this.model.challenge.get('relatedPrototype'), this.model.challenge];
+  }
+
+  get isPrototype() {
+    return this.model.challenge.get('isPrototype');
+  }
+
+  get prototypeMaximized() {
+    return this.competenceController.leftMaximized;
+  }
+
+  get alternativeMaximized() {
+    return this.alternativesController.rightMaximized;
+  }
+
+  get maximized() {
+    return this.isPrototype ? this.prototypeMaximized : this.alternativeMaximized;
+  }
+
+  @action maximize() {
+    if (this.isPrototype) {
+      this.competenceController.maximizeLeft(true);
+    } else {
+      this.alternativesController.maximizeRight(true);
+    }
+  }
+
+  @action minimize() {
+    if (this.isPrototype) {
+      this.competenceController.maximizeLeft(false);
+    } else {
+      this.alternativesController.maximizeRight(false);
+    }
+  }
+
+  @action close() {
+    if (this.isPrototype) {
+      this.router.transitionTo(
+        'authenticated.competence.prototypes',
+        this.competenceController.competence,
+        { queryParams: { leftMaximized: false } },
+      );
+    } else {
+      this.router.transitionTo(
+        'authenticated.competence.prototypes.single.alternatives',
+        this.competenceController.competence,
+        this.alternativesController.challenge,
+        { queryParams: { rightMaximized: false } },
+      );
+    }
   }
 }
