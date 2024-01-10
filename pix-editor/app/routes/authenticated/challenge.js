91bc625acc3c0e292a846305d01d909619183e7c
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2024-01-10 13:14:51 +0100

    Refactor if-else-if to decrease complexity

--- pix-editor/app/routes/authenticated/challenge.js
+++ pix-editor/app/routes/authenticated/challenge.js
@@ -25,18 +25,14 @@ export default class ChallengeRoute extends Route {
       await skill.challenges; // in order to load model.relatedPrototype later on
       const tube = await skill.tube;
       const competence = await tube.competence;
-      if (challenge.get('isPrototype')) {
-        if (localizedChallenge.isPrimaryChallenge) {
-          return this.router.transitionTo('authenticated.competence.prototypes.single', competence, challenge);
-        } else {
-          return this.router.transitionTo('authenticated.competence.prototypes.localized', competence, challenge.get('id'), localizedChallenge.get('id'));
-        }
+      if (challenge.get('isPrototype') && localizedChallenge.isPrimaryChallenge) {
+        return this.router.transitionTo('authenticated.competence.prototypes.single', competence, challenge);
+      } else if (challenge.get('isPrototype')) {
+        return this.router.transitionTo('authenticated.competence.prototypes.localized', competence, challenge.get('id'), localizedChallenge.get('id'));
+      } else if (localizedChallenge.isPrimaryChallenge) {
+        return this.router.transitionTo('authenticated.competence.prototypes.single.alternatives.single', competence, challenge.get('relatedPrototype'), challenge);
       } else {
-        if (localizedChallenge.isPrimaryChallenge) {
-          return this.router.transitionTo('authenticated.competence.prototypes.single.alternatives.single', competence, challenge.get('relatedPrototype'), challenge);
-        } else {
-          return this.router.transitionTo('authenticated.competence.prototypes.single.alternatives.localized', competence, challenge.get('relatedPrototype'), challenge.get('id'), localizedChallenge.get('id'));
-        }
+        return this.router.transitionTo('authenticated.competence.prototypes.single.alternatives.localized', competence, challenge.get('relatedPrototype'), challenge.get('id'), localizedChallenge.get('id'));
       }
     } catch (e) {
       this.router.transitionTo('authenticated');
