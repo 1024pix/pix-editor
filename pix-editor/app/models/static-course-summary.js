e65966484a1f8d6fc4121ba1037feebccc786e2b
Laura Bergoens <laura.bergoens@pix.fr>
2024-02-26 15:30:18 +0100

    display tags in static course list

--- pix-editor/app/models/static-course-summary.js
+++ pix-editor/app/models/static-course-summary.js
@@ -1,10 +1,11 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class StaticCourseSummaryModel extends Model {
   @attr name;
   @attr challengeCount;
   @attr isActive;
   @attr createdAt;
+  @hasMany('static-course-tag') tags;
 
   get previewURL() {
     return `https://app.pix.fr/courses/${this.id}`;
