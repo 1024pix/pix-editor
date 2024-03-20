7ceaee5d11fcd6251f2ca8e4ed61d7087715cbdf
Eithliu <iris.benoit@pix.fr>
2024-03-20 11:11:41 +0100

    feat: write thematic translations
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

---
+++ api/lib/infrastructure/translations/thematic.js
@@ -0,0 +1,34 @@
+import { buildTranslationsUtils } from './utils.js';
+
+export const prefix = 'thematic.';
+
+const locales = [
+  { locale: 'fr' },
+  { locale: 'en' },
+];
+
+const fields = [
+  { field: 'name' },
+];
+
+const localizedFields = [
+  {
+    airtableField: 'Nom',
+    field: 'name',
+    locale: 'fr',
+  },
+  {
+    airtableField: 'Titre en-us',
+    field: 'name',
+    locale: 'en',
+  },
+];
+
+const idField = 'id';
+
+const thematicTranslationUtils = buildTranslationsUtils({ locales, fields, localizedFields, prefix, idField });
+
+export const {
+  extractFromProxyObject,
+  prefixFor,
+} = thematicTranslationUtils;
