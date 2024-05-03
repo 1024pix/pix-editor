e0d75c19d59ee1918b9d688d324d4f69ded43c5f
Laura Bergoens <laura.bergoens@pix.fr>
2024-05-03 13:06:24 +0200

    lint template

--- api/scripts/_template.js
+++ api/scripts/_template.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { performance } from 'node:perf_hooks';
 import { fileURLToPath } from 'node:url';
-import { knex, disconnect } from '../db/knex-database-connection.js';
+import { disconnect, knex } from '../db/knex-database-connection.js';
 import { logger } from '../lib/infrastructure/logger.js';
-import yargs from 'yargs/yargs'
+import yargs from 'yargs/yargs';
 
 const __filename = fileURLToPath(import.meta.url);
 const isLaunchedFromCommandLine = process.argv[1] === __filename;
@@ -17,7 +17,7 @@ export async function doSomething({ throwError }) {
   logger.info(`Args : ${argv}`);
   const data = await knex.select('id').from('releases').first();
   return data;
-};
+}
 
 async function main() {
   const startTime = performance.now();
