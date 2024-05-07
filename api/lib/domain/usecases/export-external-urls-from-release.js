d52b038f60727e64b3d70e94cc7d5933ac2aec9e
Laura Bergoens <laura.bergoens@pix.fr>
2024-05-07 14:57:24 +0200

    light refacto

--- api/lib/domain/usecases/export-external-urls-from-release.js
+++ api/lib/domain/usecases/export-external-urls-from-release.js
@@ -3,26 +3,17 @@ import _ from 'lodash';
 export async function exportExternalUrlsFromRelease({ releaseRepository, urlErrorRepository, UrlUtils }) {
   const release = (await releaseRepository.getLatestRelease());
   const operativeChallenges = release.operativeChallenges;
-  const urlsFromChallenges = findUrlsFromChallenges(operativeChallenges, UrlUtils);
-
-  const baseUrl = function(url) {
-    const parsedUrl = new URL(url);
-    return parsedUrl.protocol + '//' + parsedUrl.host;
-  };
-
-  urlsFromChallenges.forEach((urlChallenge) => {
-    urlChallenge.origin = release.findOriginForChallenge(urlChallenge.challenge) ?? '';
-    urlChallenge.url = baseUrl(urlChallenge.url);
-    urlChallenge.tube = release.findTubeNameForChallenge(urlChallenge.challenge) ?? '';
-  });
-
-  const uniqUrls = _.uniqBy(urlsFromChallenges, 'url');
-  const dataToUpload = uniqUrls.map(({ origin, url, locales, status, tube }) =>
+  const urlsFromChallenges = findUrlsFromChallenges(operativeChallenges, release, UrlUtils);
+  const dataToUpload = urlsFromChallenges.map(({ origin, url, locales, status, tube }) =>
     [origin, tube, url, locales.join(';'), status]);
   await urlErrorRepository.exportExternalUrls(dataToUpload);
 }
 
-function findUrlsFromChallenges(challenges, UrlUtils) {
+function findUrlsFromChallenges(challenges, release, UrlUtils) {
+  const baseUrl = function(url) {
+    const parsedUrl = new URL(url);
+    return parsedUrl.protocol + '//' + parsedUrl.host;
+  };
   const urlsFromChallenges = challenges.flatMap((challenge) => {
     const functions = [
       (challenge) => UrlUtils.findUrlsInMarkdown(challenge.instruction),
@@ -31,7 +22,13 @@ function findUrlsFromChallenges(challenges, UrlUtils) {
     return functions
       .flatMap((fun) => fun(challenge))
       .map((url) => {
-        return { challenge: challenge, id: challenge.id, locales: challenge.locales, url, skillId: challenge.skillId, status: challenge.status };
+        return {
+          origin: release.findOriginForChallenge(challenge) ?? '',
+          tube: release.findTubeNameForChallenge(challenge) ?? '',
+          locales: challenge.locales,
+          url: baseUrl(url),
+          status: challenge.status,
+        };
       });
   });
   return _.uniqBy(urlsFromChallenges, 'url');
