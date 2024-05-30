e165e3f160a9c484a897606f43dee8429af2277d
Laura Bergoens <laura.bergoens@pix.fr>
2024-05-30 22:59:15 +0200

    api route

---
+++ api/lib/application/skills/index.js
@@ -0,0 +1,17 @@
+import * as securityPreHandlers from '../security-pre-handlers.js';
+import * as skillsController from './skills.js';
+
+export async function register(server) {
+  server.route([
+    {
+      method: 'PUT',
+      path: '/api/skills/clone',
+      config: {
+        pre: [{ method: securityPreHandlers.checkUserHasWriteAccess }],
+        handler: skillsController.clone,
+      },
+    },
+  ]);
+}
+
+export const name = 'skills-api';
