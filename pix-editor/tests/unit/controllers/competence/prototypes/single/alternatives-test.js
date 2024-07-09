f926fec11fa42125c3888f1db436443763868f16
Eithliu <iris.benoit@pix.fr>
2024-07-09 16:01:37 +0200

    Add unit test for alternative challenges displaying
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>

--- pix-editor/tests/unit/controllers/competence/prototypes/single/alternatives-test.js
+++ pix-editor/tests/unit/controllers/competence/prototypes/single/alternatives-test.js
@@ -3,10 +3,58 @@ import { setupTest } from 'ember-qunit';
 
 module('Unit | Controller | competence/prototypes/single/alternatives', function(hooks) {
   setupTest(hooks);
+  let controller, alternativeValidee, alternativePerimee;
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:authenticated.competence/prototypes/single/alternatives');
-    assert.ok(controller);
+  hooks.beforeEach(function() {
+    controller = this.owner.lookup('controller:authenticated.competence/prototypes/single/alternatives');
+  });
+
+  module('#alternatives', function() {
+    module('When arePerimeDeclisDisplayed is true', function() {
+      test('displays all challenges', function(assert) {
+        // given
+        alternativeValidee = {
+          id: 'id',
+          status: 'validé',
+        };
+        alternativePerimee = {
+          id: 'id2',
+          status: 'périmé',
+        };
+        controller.model = {
+          alternatives: [alternativeValidee, alternativePerimee]
+        };
+
+        // when
+        controller.arePerimeDeclisDisplayed = true;
+
+        // then
+        assert.deepEqual(controller.alternatives, [alternativeValidee, alternativePerimee]);
+
+      });
+    });
+    module('When arePerimeDeclisDisplayed is false', function() {
+      test('displays all challenges but périmés', function(assert) {
+        // given
+        alternativeValidee = {
+          id: 'id',
+          status: 'validé',
+        };
+        alternativePerimee = {
+          id: 'id2',
+          status: 'périmé',
+        };
+        controller.model = {
+          alternatives: [alternativeValidee, alternativePerimee]
+        };
+
+        // when
+        controller.arePerimeDeclisDisplayed = false;
+
+        // then
+        assert.deepEqual(controller.alternatives, [alternativeValidee]);
+
+      });
+    });
   });
 });
