a3507811ac9c2ae5bdecd83989e11491cbee70d8
Eithliu <iris.benoit@pix.fr>
2024-07-10 15:22:04 +0200

    Refacto according to PR suggestions
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>

--- pix-editor/tests/unit/controllers/competence/prototypes/single/alternatives-test.js
+++ pix-editor/tests/unit/controllers/competence/prototypes/single/alternatives-test.js
@@ -3,28 +3,31 @@ import { setupTest } from 'ember-qunit';
 
 module('Unit | Controller | competence/prototypes/single/alternatives', function(hooks) {
   setupTest(hooks);
-  let controller, alternativeValidee, alternativePerimee;
+  let controller;
 
   hooks.beforeEach(function() {
     controller = this.owner.lookup('controller:authenticated.competence/prototypes/single/alternatives');
   });
 
-  module('#alternatives', function() {
+  module('#alternatives', function(hooks) {
+    let alternativeValidee, alternativePerimee;
+
+    hooks.beforeEach(function() {
+      alternativeValidee = {
+        id: 'id',
+        status: 'validé',
+      };
+      alternativePerimee = {
+        id: 'id2',
+        status: 'périmé',
+      };
+      controller.model = {
+        alternatives: [alternativeValidee, alternativePerimee]
+      };
+    });
+
     module('When arePerimeDeclisDisplayed is true', function() {
       test('displays all challenges', function(assert) {
-        // given
-        alternativeValidee = {
-          id: 'id',
-          status: 'validé',
-        };
-        alternativePerimee = {
-          id: 'id2',
-          status: 'périmé',
-        };
-        controller.model = {
-          alternatives: [alternativeValidee, alternativePerimee]
-        };
-
         // when
         controller.arePerimeDeclisDisplayed = true;
 
@@ -35,18 +38,6 @@ module('Unit | Controller | competence/prototypes/single/alternatives', function
     });
     module('When arePerimeDeclisDisplayed is false', function() {
       test('displays all challenges but périmés', function(assert) {
-        // given
-        alternativeValidee = {
-          id: 'id',
-          status: 'validé',
-        };
-        alternativePerimee = {
-          id: 'id2',
-          status: 'périmé',
-        };
-        controller.model = {
-          alternatives: [alternativeValidee, alternativePerimee]
-        };
 
         // when
         controller.arePerimeDeclisDisplayed = false;
