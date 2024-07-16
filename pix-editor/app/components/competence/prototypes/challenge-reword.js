b9e1b1857c85d6ea7737d80da84d8882429f39d5
LionelB <lionel@lumographe.fr>
2024-07-16 12:07:28 +0200

    feat(editor): add ChallengeReword params

--- pix-editor/app/components/competence/prototypes/challenge-reword.js
+++ pix-editor/app/components/competence/prototypes/challenge-reword.js
@@ -4,6 +4,11 @@ import { action } from "@ember/object";
 export default class ChallengeReword extends Component {
   @action
   reword() {
-    console.log("reword");
+    console.log(
+      "reword",
+      this.args.instruction,
+      this.args.tubeDescription,
+      this.args.locale[0]
+    );
   }
 }
