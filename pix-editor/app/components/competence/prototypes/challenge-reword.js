cc05b3f1825ae5b46fccf710d7083d1442d8d72d
Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
2024-07-16 14:39:47 +0200

    feat(editor): add ChallengeReword modal

--- pix-editor/app/components/competence/prototypes/challenge-reword.js
+++ pix-editor/app/components/competence/prototypes/challenge-reword.js
@@ -1,14 +1,39 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 export default class ChallengeReword extends Component {
+  @tracked isLoading = false;
+  @tracked showModal = false;
+  @tracked rewordings = [];
+
   @action
-  reword() {
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+
+  @action
+  async reword() {
     console.log(
       "reword",
       this.args.instruction,
       this.args.tubeDescription,
       this.args.locale[0]
     );
+
+    this.isLoading = true;
+    this.toggleModal();
+
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1/comments"
+    );
+    const json = await response.json();
+    this.rewordings = json.map((comment) => {
+      return comment.name;
+    });
+
+    console.log(this.rewordings);
+
+    this.isLoading = false;
   }
 }
