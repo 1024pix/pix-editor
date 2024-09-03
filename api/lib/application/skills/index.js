1553e4b97c8883bf5a5bf8cdae54fed9dffc3d7a
jeremie <jeremie.jade@pix.fr>
2024-09-03 17:12:42 +0200

    Validate payload
    co-authored-by: Iris Benoit <iris.benoit@pix.fr>
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- api/lib/application/skills/index.js
+++ api/lib/application/skills/index.js
@@ -1,5 +1,9 @@
 import * as securityPreHandlers from '../security-pre-handlers.js';
 import * as skillsController from './skills.js';
+import Joi from 'joi';
+
+const tubeIdType = Joi.string().pattern(/^(rec|tube)[a-zA-Z0-9]+$/).required();
+const skillIdType = Joi.string().pattern(/^(rec|skill)[a-zA-Z0-9]+$/).required();
 
 export async function register(server) {
   server.route([
@@ -7,6 +11,17 @@ export async function register(server) {
       method: 'POST',
       path: '/api/skills/clone',
       config: {
+        validate: {
+          payload: Joi.object({
+            data: {
+              attributes: {
+                tubeDestinationId: tubeIdType,
+                skillIdToClone: skillIdType,
+                level: Joi.number().required(),
+              },
+            },
+          }),
+        },
         pre: [{ method: securityPreHandlers.checkUserHasWriteAccess }],
         handler: skillsController.clone,
       },
--- api/lib/application/skills/skills.js
+++ api/lib/application/skills/skills.js
@@ -1,11 +1,8 @@
-import _ from 'lodash';
-import { extractUserIdFromRequest } from '../../infrastructure/monitoring-tools.js';
 import {
   attachmentRepository,
   challengeRepository,
   skillRepository,
   tubeRepository,
-  userRepository,
 } from '../../infrastructure/repositories/index.js';
 import * as updatedRecordNotifier from '../../infrastructure/event-notifier/updated-record-notifier.js';
 
@@ -15,13 +12,10 @@ import * as pixApiClient from '../../infrastructure/pix-api-client.js';
 import { logger } from '../../infrastructure/logger.js';
 
 export async function clone(request, h) {
-  const userId = extractUserIdFromRequest(request);
   try {
-    const cloneCommand = normalizeCloneCommand(request.payload.data.attributes, userId);
     const newSkill = await cloneSkill({
-      cloneCommand,
+      cloneCommand: request.payload.data.attributes,
       dependencies: {
-        userRepository,
         skillRepository,
         challengeRepository,
         tubeRepository,
@@ -36,30 +30,4 @@ export async function clone(request, h) {
     logger.error(err);
     return h.response(err).code(400);
   }
-}
-
-function normalizeCloneCommand(attrs, userId) {
-  const cloneCommand = {};
-  if (!_.isString(attrs.tubeDestinationId)) {
-    throw new Error('tubeDestinationId PAS BON');
-  } else {
-    cloneCommand.tubeDestinationId = attrs.tubeDestinationId;
-  }
-  if (!_.isString(attrs.skillIdToClone)) {
-    throw new Error('skillIdToClone PAS BON');
-  } else {
-    cloneCommand.skillIdToClone = attrs.skillIdToClone;
-  }
-  if (!_.isNumber(parseInt(attrs.level))) {
-    throw new Error('level PAS BON');
-  } else {
-    cloneCommand.level = parseInt(attrs.level);
-  }
-  if (!_.isString(attrs.changelogText)) {
-    cloneCommand.changelogText = '';
-  } else {
-    cloneCommand.changelogText = attrs.changelogText;
-  }
-  cloneCommand.userId = userId;
-  return cloneCommand;
 }
