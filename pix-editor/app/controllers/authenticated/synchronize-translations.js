74541568a645357d8ed9c6ca13eceb9723b06342
jeremie <jeremie.jade@pix.fr>
2024-10-02 16:52:41 +0200

    Add phrase service
    Co-authored-by: Iris Benoit <iris.benoit@pix.fr>
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

--- pix-editor/app/controllers/authenticated/synchronize-translations.js
+++ pix-editor/app/controllers/authenticated/synchronize-translations.js
@@ -4,11 +4,12 @@ import { inject as service } from '@ember/service';
 
 export default class SynchronizeTranslationsController extends Controller {
   @service notifications;
+  @service phrase;
 
   @action
   async importFromPhrase() {
     try {
-      await this.model.download();
+      await this.phrase.download();
       this.notifications.success('Téléchargement des traductions depuis Phrase effectué.');
     } catch {
       this.notifications.error('Erreur lors du téléchargement des traductions.');
