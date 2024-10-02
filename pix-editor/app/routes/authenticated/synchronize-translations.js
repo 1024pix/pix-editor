6cc33b00ee031e54654daf006af48d46d9e79441
jeremie <jeremie.jade@pix.fr>
2024-10-02 16:53:43 +0200

    remove useless phrase model
    Co-authored-by: Iris Benoit <iris.benoit@pix.fr>
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

--- pix-editor/app/routes/authenticated/synchronize-translations.js
+++ pix-editor/app/routes/authenticated/synchronize-translations.js
@@ -1,10 +1,3 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
-export default class SynchronizeTranslationsRoute extends Route {
-  @service store;
-
-  model() {
-    return this.store.createRecord('phrase');
-  }
-}
+export default class SynchronizeTranslationsRoute extends Route {}
