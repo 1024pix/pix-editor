92a6e667e03897b81937655abfbc938704a90bf4
Eithliu <iris.benoit@pix.fr>
2024-10-24 16:34:26 +0200

    feat(api): filter competence overview by language for prototypes.
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- api/lib/domain/readmodels/CompetenceOverview.js
+++ api/lib/domain/readmodels/CompetenceOverview.js
@@ -9,12 +9,12 @@ export class CompetenceOverview {
     this.thematicOverviews = thematicOverviews;
   }
 
-  static buildForChallengesProduction({ competenceId, thematics, tubes, skills, challenges }) {
+  static buildForChallengesProduction({ competenceId, thematics, tubes, skills, challenges, locale }) {
     return new CompetenceOverview({
       id: `${competenceId}-challenges-production-fr`,
       thematicOverviews: thematics
         .sort(byIndex)
-        .map((thematic) => ThematicOverview.buildForChallengesProduction({ thematic, tubes, skills, challenges }))
+        .map((thematic) => ThematicOverview.buildForChallengesProduction({ thematic, tubes, skills, challenges, locale }))
         .filter((thematicOverview) => !thematicOverview.isEmpty),
     });
   }
@@ -31,7 +31,7 @@ class ThematicOverview {
     this.tubeOverviews = tubeOverviews;
   }
 
-  static buildForChallengesProduction({ thematic, tubes, skills, challenges }) {
+  static buildForChallengesProduction({ thematic, tubes, skills, challenges, locale }) {
     const tubesById = Object.fromEntries(tubes.map((tube) => [tube.id, tube]));
 
     return new ThematicOverview({
@@ -40,7 +40,7 @@ class ThematicOverview {
       tubeOverviews: thematic.tubeIds
         ?.map((tubeId) => tubesById[tubeId])
         .sort(byIndex)
-        .map((tube) => TubeOverview.buildForChallengesProduction({ tube, skills, challenges }))
+        .map((tube) => TubeOverview.buildForChallengesProduction({ tube, skills, challenges, locale }))
         .filter((tubeOverview) => !tubeOverview.isEmpty)
     });
   }
@@ -61,14 +61,14 @@ class TubeOverview {
     this.skillOverviews = skillOverviews;
   }
 
-  static buildForChallengesProduction({ tube, skills, challenges }) {
+  static buildForChallengesProduction({ tube, skills, challenges, locale }) {
     const skillsByTubeIdAndLevel = arrangeSkillsByTubeIdAndLevel(skills);
 
     return new TubeOverview({
       airtableId: tube.airtableId,
       name: tube.name,
       skillOverviews: skillsByTubeIdAndLevel[tube.id]
-        ?.map((skill) => SkillOverview.buildForChallengesProduction({ skill, challenges })),
+        ?.map((skill) => SkillOverview.buildForChallengesProduction({ skill, challenges, locale })),
     });
   }
 
@@ -94,10 +94,11 @@ class SkillOverview {
     this.validatedChallengesCount = validatedChallengesCount;
   }
 
-  static buildForChallengesProduction({ skill, challenges }) {
+  static buildForChallengesProduction({ skill, challenges, locale }) {
     if (!skill) return null;
 
     const productionPrototype = challenges.find(isProductionPrototypeOf(skill));
+
     const productionChallenges = challenges.filter(hasSkillIdAndVersionOf(productionPrototype));
 
     return new SkillOverview({
@@ -105,8 +106,8 @@ class SkillOverview {
       name: skill.name,
       prototypeId: productionPrototype?.id,
       isPrototypeDeclinable: productionPrototype?.isDeclinable,
-      proposedChallengesCount: countByStatus(productionChallenges, Challenge.STATUSES.PROPOSE),
-      validatedChallengesCount: countByStatus(productionChallenges, Challenge.STATUSES.VALIDE),
+      proposedChallengesCount: countChallengesByStatusAndLocale(productionChallenges, Challenge.STATUSES.PROPOSE, locale),
+      validatedChallengesCount: countChallengesByStatusAndLocale(productionChallenges, Challenge.STATUSES.VALIDE, locale),
     });
   }
 }
@@ -140,6 +141,10 @@ function hasSkillIdAndVersionOf({ skillId, version } = {}) {
   return (challenge) => challenge.skillId === skillId && challenge.version === version;
 }
 
-function countByStatus(items, status) {
-  return items.reduce((count, item) => item.status === status ? count + 1 : count, 0);
+function countChallengesByStatusAndLocale(challenges, status, locale) {
+  return challenges.reduce((count, challenge) => {
+    if (challenge.status !== status) return count;
+    if (locale && challenge.locale !== locale) return count;
+    return count + 1;
+  }, 0);
 }
