7aff5eb1c15cbf2e43f4bc16852aa752c2efb26f
Laura Bergoens <laura.bergoens@pix.fr>
2024-10-24 16:22:32 +0200

    add route POST whitelisted urls back
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>

--- api/tests/integration/infrastructure/repositories/whitelisted-url-repository_test.js
+++ api/tests/integration/infrastructure/repositories/whitelisted-url-repository_test.js
@@ -1,5 +1,5 @@
-import { describe, expect, it } from 'vitest';
-import { databaseBuilder, domainBuilder } from '../../../test-helper.js';
+import { afterEach, describe, expect, it } from 'vitest';
+import { databaseBuilder, domainBuilder, knex } from '../../../test-helper.js';
 import * as whitelistedUrlRepository from '../../../../lib/infrastructure/repositories/whitelisted-url-repository.js';
 
 describe('Integration | Repository | whitelisted-url-repository', () => {
@@ -113,6 +113,152 @@ describe('Integration | Repository | whitelisted-url-repository', () => {
     });
   });
 
+  describe('#findRead', () => {
+
+    it('should retrieve given read whitelisted url by its id', async () => {
+      // given
+      const adminUser1 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 1', access: 'admin' });
+      const adminUser2 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 2', access: 'admin' });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 123,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser2.id,
+        deletedBy: null,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2022-02-02'),
+        deletedAt: null,
+        url: 'https://www.google.com',
+        relatedEntityIds: 'recINswt85utqO5KJ,recPiCGFhfgervqr5',
+        comment: 'Je décide de whitelister ça car mon cousin travaille chez google',
+      });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 456,
+        createdBy: null,
+        latestUpdatedBy: null,
+        deletedBy: null,
+        createdAt: new Date('2020-12-12'),
+        updatedAt: new Date('2022-08-08'),
+        deletedAt: null,
+        url: 'https://www.editor.pix.fr',
+        relatedEntityIds: null,
+        comment: 'Mon site préféré',
+      });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 789,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser1.id,
+        deletedBy: adminUser1.id,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2022-02-02'),
+        deletedAt: new Date('2023-01-01'),
+        url: 'https://www.les-fruits-c-super-bon',
+        relatedEntityIds: 'reclbhuUTRGc1jZRL',
+        comment: null,
+      });
+      await databaseBuilder.commit();
+
+      // when
+      const whitelistedUrl = await whitelistedUrlRepository.findRead(123);
+
+      // then
+      expect(whitelistedUrl).toStrictEqual(domainBuilder.buildReadWhitelistedUrl({
+        id: 123,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2022-02-02'),
+        creatorName: 'Madame Admin 1',
+        latestUpdatorName: 'Madame Admin 2',
+        url: 'https://www.google.com',
+        relatedEntityIds: 'recINswt85utqO5KJ,recPiCGFhfgervqr5',
+        comment: 'Je décide de whitelister ça car mon cousin travaille chez google',
+      }));
+    });
+
+    it('should return null when whitelisted url has been deleted', async () => {
+      // given
+      const adminUser1 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 1', access: 'admin' });
+      const adminUser2 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 2', access: 'admin' });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 123,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser2.id,
+        deletedBy: null,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2022-02-02'),
+        deletedAt: null,
+        url: 'https://www.google.com',
+        relatedEntityIds: 'recINswt85utqO5KJ,recPiCGFhfgervqr5',
+        comment: 'Je décide de whitelister ça car mon cousin travaille chez google',
+      });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 456,
+        createdBy: null,
+        latestUpdatedBy: null,
+        deletedBy: null,
+        createdAt: new Date('2020-12-12'),
+        updatedAt: new Date('2022-08-08'),
+        deletedAt: null,
+        url: 'https://www.editor.pix.fr',
+        relatedEntityIds: null,
+        comment: 'Mon site préféré',
+      });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 789,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser1.id,
+        deletedBy: adminUser1.id,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2022-02-02'),
+        deletedAt: new Date('2023-01-01'),
+        url: 'https://www.les-fruits-c-super-bon',
+        relatedEntityIds: 'reclbhuUTRGc1jZRL',
+        comment: null,
+      });
+      await databaseBuilder.commit();
+
+      // when
+      const whitelistedUrl = await whitelistedUrlRepository.findRead(789);
+
+      // then
+      expect(whitelistedUrl).toStrictEqual(null);
+    });
+
+    it('should return null when no entity found for id', async () => {
+      // given
+      const adminUser1 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 1', access: 'admin' });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 123,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser1.id,
+        deletedBy: null,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2024-01-01'),
+        deletedAt: new Date('2024-01-01'),
+        url: 'https://www.google.com',
+        relatedEntityIds: 'recINswt85utqO5KJ,recPiCGFhfgervqr5',
+        comment: 'Je décide de whitelister ça car mon cousin travaille chez google',
+      });
+      databaseBuilder.factory.buildWhitelistedUrl({
+        id: 789,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser1.id,
+        deletedBy: adminUser1.id,
+        createdAt: new Date('2020-01-01'),
+        updatedAt: new Date('2023-01-01'),
+        deletedAt: new Date('2023-01-01'),
+        url: 'https://www.les-fruits-c-super-bon',
+        relatedEntityIds: 'reclbhuUTRGc1jZRL',
+        comment: null,
+      });
+      await databaseBuilder.commit();
+
+      // when
+      const whitelistedUrl = await whitelistedUrlRepository.findRead(777);
+
+      // then
+      expect(whitelistedUrl).toStrictEqual(null);
+    });
+  });
+
   describe('#find', () => {
 
     it('should retrieve given whitelisted url by its id', async () => {
@@ -213,6 +359,10 @@ describe('Integration | Repository | whitelisted-url-repository', () => {
   });
 
   describe('#save', function() {
+
+    afterEach(function() {
+      return knex('whitelisted_urls').del();
+    });
     it('should update the whitelisted url', async function() {
       // given
       const adminUser1 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 1', access: 'admin' });
@@ -249,6 +399,34 @@ describe('Integration | Repository | whitelisted-url-repository', () => {
       // then
       const savedWhitelistedUrl = await whitelistedUrlRepository.find(123);
       expect(savedWhitelistedUrl).toStrictEqual(whitelistedUrlToSave);
+    });
+    it('should insert a new whitelisted url', async function() {
+      // given
+      const adminUser1 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 1', access: 'admin' });
+      const adminUser2 = databaseBuilder.factory.buildUser({ name: 'Madame Admin 2', access: 'admin' });
+      await databaseBuilder.commit();
+      const whitelistedUrlToSave = domainBuilder.buildWhitelistedUrl({
+        id: null,
+        createdBy: adminUser1.id,
+        latestUpdatedBy: adminUser2.id,
+        deletedBy: adminUser2.id,
+        createdAt: new Date('2022-02-02'),
+        updatedAt: new Date('2023-03-03'),
+        deletedAt: new Date('2024-04-04'),
+        url: 'https://www.bing.com',
+        relatedEntityIds: 'recABC789123',
+        comment: 'Je viens de créer cette entrée',
+      });
+
+      // when
+      const id = await whitelistedUrlRepository.save(whitelistedUrlToSave);
+
+      // then
+      const savedWhitelistedUrl = await whitelistedUrlRepository.find(id);
+      expect(savedWhitelistedUrl).toStrictEqual(domainBuilder.buildWhitelistedUrl({
+        ...whitelistedUrlToSave,
+        id,
+      }));
     });
   });
 });
--- api/tests/unit/domain/models/WhitelistedUrl_test.js
+++ api/tests/unit/domain/models/WhitelistedUrl_test.js
@@ -1,8 +1,21 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { domainBuilder } from '../../../test-helper.js';
-import { User } from '../../../../lib/domain/models/index.js';
+import { User, WhitelistedUrl } from '../../../../lib/domain/models/index.js';
 
 describe('Unit | Domain | WhitelistedUrl', () => {
+  let now;
+  beforeEach(function() {
+    now = new Date('2024-10-29T03:04:00Z');
+    vi.useFakeTimers({
+      now,
+      toFake: ['Date'],
+    });
+  });
+
+  afterEach(function() {
+    vi.useRealTimers();
+  });
+
   describe('#canDelete', () => {
     describe('can', function() {
       it('should return a canExecute valid when all conditions are reunited', function() {
@@ -56,19 +69,6 @@ describe('Unit | Domain | WhitelistedUrl', () => {
   });
 
   describe('#delete', function() {
-    let now;
-    beforeEach(function() {
-      now = new Date('2024-10-29T03:04:00Z');
-      vi.useFakeTimers({
-        now,
-        toFake: ['Date'],
-      });
-    });
-
-    afterEach(function() {
-      vi.useRealTimers();
-    });
-
     it('should mark as deleted the whitelisted url', function() {
       // given
       const user = domainBuilder.buildUser({ id: 888 });
@@ -100,6 +100,170 @@ describe('Unit | Domain | WhitelistedUrl', () => {
         url: 'https://www.google.com',
         relatedEntityIds: 'recINswt85utqO5KJ,recPiCGFhfgervqr5',
         comment: 'Je décide de whitelister ça car mon cousin travaille chez google',
+      }));
+    });
+  });
+
+  describe('#static canCreate', () => {
+    describe('can', function() {
+      it('should return a canExecute valid when all conditions are reunited', function() {
+        // given
+        const user = domainBuilder.buildUser({ access: User.ROLES.ADMIN });
+        const existingWhitelistedUrls = [domainBuilder.buildReadWhitelistedUrl({
+          url: 'https://www.painperdu.com',
+        })];
+        const creationCommand1 = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: null,
+          comment: null,
+        };
+        const creationCommand2 = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: 'recTest12345678,recSuperTest789123',
+          comment: 'COucou',
+        };
+
+        // when
+        const canCreate1 = WhitelistedUrl.canCreate(creationCommand1, user, existingWhitelistedUrls);
+        const canCreate2 = WhitelistedUrl.canCreate(creationCommand2, user, []);
+
+        // then
+        expect(canCreate1.can).to.be.true;
+        expect(canCreate2.can).to.be.true;
+      });
+    });
+    describe('cannot', function() {
+      it('should return a canExecute invalid when user is not admin', function() {
+        // given
+        const user = domainBuilder.buildUser({ access: User.ROLES.EDITOR });
+        const creationCommand = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: null,
+          comment: null,
+        };
+
+        // when
+        const canCreate = WhitelistedUrl.canCreate(creationCommand, user, []);
+
+        // then
+        expect(canCreate.cannot).to.be.true;
+        expect(canCreate.errorMessage).to.equal('L\'utilisateur n\'a pas les droits pour créer une URL whitelistée');
+      });
+
+      it('should return a canExecute invalid when url is not valid in creation command', function() {
+        // given
+        const user = domainBuilder.buildUser({ access: User.ROLES.ADMIN });
+        const creationCommand1 = {
+          url: null,
+        };
+        const creationCommand2 = {
+          url: 'www.missing-protocol.com',
+        };
+        const creationCommand3 = {
+          url: 123456,
+        };
+
+        // when
+        const canCreate1 = WhitelistedUrl.canCreate(creationCommand1, user, []);
+        const canCreate2 = WhitelistedUrl.canCreate(creationCommand2, user, []);
+        const canCreate3 = WhitelistedUrl.canCreate(creationCommand3, user, []);
+
+        // then
+        expect(canCreate1.cannot).to.be.true;
+        expect(canCreate1.errorMessage).to.equal('URL invalide');
+        expect(canCreate2.cannot).to.be.true;
+        expect(canCreate2.errorMessage).to.equal('URL invalide');
+        expect(canCreate3.cannot).to.be.true;
+        expect(canCreate3.errorMessage).to.equal('URL invalide');
+      });
+
+      it('should return a canExecute invalid when relatedEntityIds is not in valid format in creation command', function() {
+        // given
+        const user = domainBuilder.buildUser({ access: User.ROLES.ADMIN });
+        const creationCommand1 = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: 123456.12,
+        };
+        const creationCommand2 = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: 'je ne suis pas une suite d ids d entités séparés par des virgules sans espaces',
+        };
+
+        // when
+        const canCreate1 = WhitelistedUrl.canCreate(creationCommand1, user, []);
+        const canCreate2 = WhitelistedUrl.canCreate(creationCommand2, user, []);
+
+        // then
+        expect(canCreate1.cannot).to.be.true;
+        expect(canCreate1.errorMessage).to.equal('Liste d\'ids invalides. Doit être une suite d\'ids séparés par des virgules ou vide');
+        expect(canCreate2.cannot).to.be.true;
+        expect(canCreate2.errorMessage).to.equal('Liste d\'ids invalides. Doit être une suite d\'ids séparés par des virgules ou vide');
+      });
+
+      it('should return a canExecute invalid when comment is not in valid format in creation command', function() {
+        // given
+        const user = domainBuilder.buildUser({ access: User.ROLES.ADMIN });
+        const creationCommand = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: null,
+          comment: 123,
+        };
+
+        // when
+        const canCreate = WhitelistedUrl.canCreate(creationCommand, user, []);
+
+        // then
+        expect(canCreate.cannot).to.be.true;
+        expect(canCreate.errorMessage).to.equal('Commentaire invalide. Doit être un texte ou vide');
+      });
+
+      it('should return a canExecute invalid when url has already been whitelisted (case sensitive, exact match)', function() {
+        // given
+        const user = domainBuilder.buildUser({ access: User.ROLES.ADMIN });
+        const existingWhitelistedUrls = [domainBuilder.buildReadWhitelistedUrl({
+          url: 'https://www.brioche.com',
+        })];
+        const creationCommand = {
+          url: 'https://www.brioche.com',
+          relatedEntityIds: null,
+          comment: null,
+        };
+
+        // when
+        const canCreate = WhitelistedUrl.canCreate(creationCommand, user, existingWhitelistedUrls);
+
+        // then
+        expect(canCreate.cannot).to.be.true;
+        expect(canCreate.errorMessage).to.equal('URL déjà whitelistée');
+      });
+    });
+  });
+
+  describe('#static create', () => {
+    it('should return a newly created whitelisted url', function() {
+      // given
+      const user = domainBuilder.buildUser({ id: 444, access: User.ROLES.ADMIN });
+      const creationCommand = {
+        url: 'https://www.brioche.com',
+        relatedEntityIds: 'recABC,redDEF',
+        comment: 'coucou maman',
+      };
+
+      // when
+      const createdWhitelistedUrl = WhitelistedUrl.create(creationCommand, user);
+
+      // then
+      expect(createdWhitelistedUrl).toStrictEqual(domainBuilder.buildWhitelistedUrl({
+        id: null,
+        createdBy: 444,
+        latestUpdatedBy: 444,
+        deletedBy: null,
+        createdAt: now,
+        updatedAt: now,
+        deletedAt: null,
+        url: 'https://www.brioche.com',
+        relatedEntityIds: 'recABC,redDEF',
+        comment: 'coucou maman',
       }));
     });
   });
