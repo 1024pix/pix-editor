08c3f8bd4b605463ed47956e9ba44d41fa60a17d
Fael BASSETTI <fael.bassetti@pix.fr>
2024-11-07 16:32:16 +0100

    Add whitelisted url UI
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

---
+++ pix-editor/app/controllers/authenticated/whitelisted-urls/new.js
@@ -0,0 +1,30 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class NewWhitelistedUrlController extends Controller {
+  @service store;
+  @service router;
+  @service notifications;
+
+  @action
+  async createWhitelistedUrl(formData) {
+    const whitelistedUrl = this.store.createRecord('whitelisted-url', formData);
+    try {
+      await whitelistedUrl.save();
+      this.notifications.success('URL ajoutée à la whitelist avec succès.');
+      this.router.transitionTo('authenticated.whitelisted-urls.list');
+    } catch (err) {
+      whitelistedUrl.deleteRecord();
+      await this.notifications.error('Une erreur est survenue lors de l\'ajout de l\'URL à la whitelist');
+      const knownErrors = err?.errors.map((error) => error.detail).join('\n');
+      const finalErrors = knownErrors ?? JSON.stringify(err);
+      throw new Error(finalErrors);
+    }
+  }
+
+  @action
+  async goBackToList() {
+    this.router.transitionTo('authenticated.whitelisted-urls.list');
+  }
+}
