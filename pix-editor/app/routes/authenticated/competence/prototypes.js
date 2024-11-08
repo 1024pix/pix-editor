29ae6a968a0f024b30b8ba7e5239207a2b6ff2bb
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2024-11-08 11:09:20 +0100

    fix(pix-editor): include challenges in workbench-list loading time
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>

--- pix-editor/app/routes/authenticated/competence/prototypes.js
+++ pix-editor/app/routes/authenticated/competence/prototypes.js
@@ -31,13 +31,13 @@ export default class PrototypesRoute extends Route {
         const themes = await competence.hasMany('rawThemes').reload();
         const themesTubes = await Promise.all(themes.map((theme) => theme.hasMany('rawTubes').reload()));
         const tubesSkills = await Promise.all(themesTubes.flatMap((tubes) => tubes.map((tube) => tube.hasMany('rawSkills').reload())));
-        Promise.all(tubesSkills.flatMap((skills) => skills.map((skill) => skill.hasMany('challenges').reload()))).catch(console.error);
+        await Promise.all(tubesSkills.flatMap((skills) => skills.map((skill) => skill.hasMany('challenges').reload())));
         this.refreshing = false;
       } else {
         const themes = await competence.rawThemes;
         const themesTubes = await Promise.all(themes.map((theme) => theme.rawTubes));
         const tubesSkills = await Promise.all(themesTubes.flatMap((tubes) => tubes.map((tube) => tube.rawSkills)));
-        Promise.all(tubesSkills.flatMap((skills) => skills.map((skill) => skill.hasMany('challenges').load()))).catch(console.error);
+        await Promise.all(tubesSkills.flatMap((skills) => skills.map((skill) => skill.hasMany('challenges').load())));
       }
       return null;
     }
