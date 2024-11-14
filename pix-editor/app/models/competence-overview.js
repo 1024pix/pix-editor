eb510ae850365cf3b4eb5bf58746d04a8d0ca9d0
Laura Bergoens <laura.bergoens@pix.fr>
2024-11-14 15:41:59 +0100

    add new attributes in model front side
    Co-authored-by: Iris Benoît <iris.benoit@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- pix-editor/app/models/competence-overview.js
+++ pix-editor/app/models/competence-overview.js
@@ -1,6 +1,8 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class CompetenceOverviewModel extends Model {
+  @attr() airtableId;
+  @attr() name;
   @attr() thematicOverviews;
   @attr('number') tubesCount;
   @attr('number') skillsCount;
