40e09c94f614fdaa52a23ce97f1b0f2269caeb6e
Laura Bergoens <laura.bergoens@pix.fr>
2025-01-27 11:30:42 +0100

    feat(front): handle navigation primary localized when details open
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- pix-editor/app/routes/authenticated/v2/challenge.js
+++ pix-editor/app/routes/authenticated/v2/challenge.js
@@ -6,6 +6,16 @@ export default class ChallengeRoute extends Route {
   @service router;
   @service store;
 
+  beforeModel(transition) {
+    // TODO quand on aura la page de détails d'une épreuve traduite, transition vers le détail
+    const locale = transition.to.queryParams.locale;
+    if (locale) {
+      const overview = transition.to.params.overview;
+      const skillId = transition.to.params.skill_id;
+      this.router.transitionTo('authenticated.v2.competence-overview.localized-challenges', overview, skillId);
+    }
+  }
+
   async model(params) {
     const { competence_id: competenceId } = this.paramsFor('authenticated.v2');
     const { challenge_id, overview, skill_id } = params;
