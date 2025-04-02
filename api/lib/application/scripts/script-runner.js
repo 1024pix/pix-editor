86d2bd103a919f1e846b74450895a71beb7a7033
rouxxi <gwenvael.laskar@pix.fr>
2025-04-02 16:25:24 +0200

    feat(api): add script logic from mono-repo pix
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

---
+++ api/lib/application/scripts/script-runner.js
@@ -0,0 +1,84 @@
+import * as url from 'node:url';
+
+import pick from 'lodash/pick.js';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
+
+import { disconnect } from '../../../db/knex-database-connection.js';
+import { child } from '../../infrastructure/logger.js';
+
+function isRunningFromCli(scriptFileUrl) {
+  const modulePath = url.fileURLToPath(scriptFileUrl);
+  return process.argv[1] === modulePath;
+}
+
+function loggerForScriptClass(ScriptClass) {
+  return child(`script:${ScriptClass.name}`, { event: ScriptClass.name });
+}
+
+function getProcessArgs() {
+  return hideBin(process.argv);
+}
+
+/**
+ * A utility class for running scripts from the command line.
+ */
+export class ScriptRunner {
+  /**
+   * Executes the provided script class if running from the command line.
+   * Parses command-line arguments and runs the script with the provided options.
+   *
+   * @template {typeof import('./script.js').Script} Script
+   * @param {string} scriptFileUrl - The file URL of the script being executed.
+   * @param {Script} ScriptClass - The script class to be instantiated and executed.
+   * @param {object} [dependencies] - The script runner dependencies (logger, isRunningFromCli, getProcessArgs)
+   */
+  static async execute(
+    scriptFileUrl,
+    ScriptClass,
+    dependencies = {
+      logger: loggerForScriptClass(ScriptClass),
+      isRunningFromCli,
+      getProcessArgs,
+    },
+  ) {
+    const { logger, isRunningFromCli, getProcessArgs } = dependencies;
+
+    if (!isRunningFromCli(scriptFileUrl)) return;
+
+    const script = new ScriptClass();
+    const { description, options = {}, commands = {} } = script.metaInfo;
+
+    try {
+      const yargsCommand = yargs(getProcessArgs()).usage(description).options(options).help().version(false);
+      Object.entries(commands).forEach(([name, { description, options }]) =>
+        yargsCommand.command(name, description, options),
+      );
+
+      const result = await yargsCommand.parseAsync();
+
+      let command;
+      if (result._[0] in commands) {
+        command = result._[0];
+      }
+
+      let optionKeys = Object.keys(options);
+      if (commands[command]?.options) {
+        optionKeys = [...optionKeys, ...Object.keys(commands[command].options)];
+      }
+      const parsedOptions = pick(result, optionKeys);
+
+      logger.info('Start script');
+
+      await script.run({ command, options: parsedOptions, logger });
+
+      logger.info('Script execution successful.');
+    } catch (error) {
+      logger.error('Script execution failed.');
+      logger.error(error);
+      process.exitCode = 1;
+    } finally {
+      await disconnect();
+    }
+  }
+}
---
+++ api/lib/infrastructure/utils/csv.js
@@ -0,0 +1,74 @@
+import fs, { promises } from 'node:fs';
+
+const { readFile, access } = promises;
+
+import lodash from 'lodash';
+
+const { isEmpty, difference } = lodash;
+
+import papa from 'papaparse';
+
+const ERRORS = {
+  MISSING_REQUIRED_FIELD_NAMES: 'MISSING_REQUIRED_FIELD_NAMES',
+  EMPTY_FILE: 'EMPTY_FILE',
+};
+const OPTIONS_WITH_HEADER = {
+  skipEmptyLines: true,
+  header: true,
+  transform: (value) => {
+    if (typeof value === 'string') {
+      value = value.trim();
+    }
+
+    return value;
+  },
+};
+
+async function checkCsvHeader({ filePath, requiredFieldNames = [] }) {
+  if (isEmpty(requiredFieldNames)) {
+    throw new Error(ERRORS.MISSING_REQUIRED_FIELD_NAMES);
+  }
+
+  const data = await _parseCsv(filePath, { skipEmptyLines: true, preview: 1 });
+  if (isEmpty(data)) {
+    throw new Error(ERRORS.EMPTY_FILE + 'Le fichier ne contient aucune donnée');
+  }
+
+  const fieldNames = data[0].map((fieldName) => fieldName?.trim());
+  const missingRequiredFieldNames = difference(requiredFieldNames, fieldNames);
+
+  if (!isEmpty(missingRequiredFieldNames)) {
+    throw new Error(
+      ERRORS.MISSING_REQUIRED_FIELD_NAMES +
+      `Colonne(s) manquante(s) ou erronée(s) : ${missingRequiredFieldNames}`,
+    );
+  }
+}
+
+function parseCsvWithHeader(filePath, options = OPTIONS_WITH_HEADER) {
+  return _parseCsv(filePath, options);
+}
+
+async function _parseCsv(filePath, options) {
+  const cleanedData = await readCsvFile(filePath);
+  return parseCsvData(cleanedData, options);
+}
+
+async function readCsvFile(filePath) {
+  try {
+    await access(filePath, fs.constants.F_OK);
+  } catch {
+    throw new Error(`Aucun fichier nommé ${filePath} n'a été trouvé !`);
+  }
+
+  const rawData = await readFile(filePath, 'utf8');
+
+  return rawData.replace(/^\uFEFF/, '');
+}
+
+async function parseCsvData(cleanedData, options) {
+  const { data } = papa.parse(cleanedData, options);
+  return data;
+}
+
+export { checkCsvHeader, parseCsvWithHeader };
