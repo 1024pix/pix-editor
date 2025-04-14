adcc9189bf38a34da3bcee0645e8921fec92dc60
Laura Bergoens <laura.bergoens@pix.fr>
2025-04-14 17:38:45 +0200

    Revert "Revert "revert(api): squashed revert of all translations related commits""
    This reverts commit ddf302138541d081f3043d112f5fc4c442493054.

--- api/tests/unit/domain/models/Translation_test.js
+++ api/tests/unit/domain/models/Translation_test.js
@@ -16,19 +16,4 @@ describe('Unit | Domain | Translation', function() {
       expect(entityId).to.equal('idDuChallenge');
     });
   });
-
-  context('get model', function() {
-    it('should return the expected model', function() {
-      // given
-      const translation = domainBuilder.buildTranslation({
-        key: 'challenge.idDuChallenge.champ',
-      });
-
-      // when
-      const model = translation.model;
-
-      // then
-      expect(model).to.equal('challenge');
-    });
-  });
 });
