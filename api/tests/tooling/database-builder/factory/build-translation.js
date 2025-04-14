ddf302138541d081f3043d112f5fc4c442493054
Laura Bergoens <laura.bergoens@pix.fr>
2025-04-14 09:23:59 +0200

    Revert "revert(api): squashed revert of all translations related commits"
    This reverts commit 27e8787945a0abd572f272a085e0dc986c8e11c8.

--- api/tests/tooling/database-builder/factory/build-translation.js
+++ api/tests/tooling/database-builder/factory/build-translation.js
@@ -1,9 +1,15 @@
 import { databaseBuffer } from '../database-buffer.js';
 
 export function buildTranslation({ key, locale, value } = {}) {
-  return databaseBuffer.pushInsertable({
+  const translation = databaseBuffer.pushInsertable({
     tableName: 'translations',
     autoId: false,
     values: { key, locale, value },
   });
+
+  return {
+    ...translation,
+    model: key.split('.')[0],
+    entityId: key.split('.')[1]
+  };
 }
