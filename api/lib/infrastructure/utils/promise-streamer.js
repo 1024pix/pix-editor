802bee5b5b8d04bbf0874a47296c203a0fe2c815
Laura Bergoens <laura.bergoens@pix.fr>
2025-04-15 17:14:07 +0200

    leave translatiosn

--- api/lib/infrastructure/utils/promise-streamer.js
+++ api/lib/infrastructure/utils/promise-streamer.js
@@ -208,7 +208,7 @@ export function promiseStreamerForRepli2(promise) {
     //callback();
     //},
     // });
-    const newData = _.omit(data, ['challenges', 'skills', 'tutorials', 'translations']);
+    const newData = _.omit(data, ['challenges', 'skills', 'tutorials']);
     pipeline(
       Readable.from(JSON.stringify(newData)),
       //jsonStringifyTransform,
