ac185a13fc410622a873e4db388b7898bc5ee807
Laura Bergoens <laura.bergoens@pix.fr>
2025-04-15 17:29:37 +0200

    Revert "Revert "Revert "Revert "Revert "tech(api): send json stringify chunk attribute by attribute"""""
    This reverts commit df275c4b890bcadc5c354f70ee4024de9a8aa358.

--- api/lib/infrastructure/utils/promise-streamer.js
+++ api/lib/infrastructure/utils/promise-streamer.js
@@ -19,17 +19,7 @@ export function promiseStreamer(promise, writableStream = getWritableStream()) {
     writableStream.write('\n');
   }, 1000);
   promise.then((data) => {
-    clearInterval(timer);
-    writableStream.write('{');
-    const keys = Object.keys(data);
-    while (keys.length > 0) {
-      const key = keys.shift();
-      writableStream.write('"' + key + '":' + JSON.stringify(data[key]));
-      if (keys.length !== 0) {
-        writableStream.write(',');
-      }
-    }
-    writableStream.write('}');
+    writableStream.write(JSON.stringify(data));
   }).catch((error) => {
     logger.error(error);
     Sentry.captureException(error);
