c4e290e350bbc4350e353715925fcd501b288c19
jeremie <jeremie.jade@pix.fr>
2025-04-15 11:04:30 +0200

    feat(api): use non breaking space for frenchy challenges
    Co-authored-by: Iris Benoit-Martin <iris.benoit-martin@pix.fr>

--- api/lib/application/challenges/index.js
+++ api/lib/application/challenges/index.js
@@ -9,7 +9,7 @@ import { attachmentDatasource } from '../../infrastructure/datasources/airtable/
 import { createChallengeTransformer } from '../../infrastructure/transformers/index.js';
 import * as pixApiClient from '../../infrastructure/pix-api-client.js';
 import * as updatedRecordNotifier from '../../infrastructure/event-notifier/updated-record-notifier.js';
-import { getPhraseTranslationsURL, previewChallenge } from '../../domain/usecases/index.js';
+import { getPhraseTranslationsURL, previewChallenge, createChallenge } from '../../domain/usecases/index.js';
 import { extractParameters } from '../../infrastructure/utils/query-params-utils.js';
 
 const challengeIdType = Joi.string().pattern(/^(rec|challenge)[a-zA-Z0-9]+$/).required();
@@ -115,7 +115,7 @@ export async function register(server) {
         handler: async function(request, h) {
           const challenge = await challengeSerializer.deserialize(request.payload);
 
-          const createdChallenge = await challengeRepository.create(challenge);
+          const createdChallenge = await createChallenge(challenge, { challengeRepository });
           return h.response(challengeSerializer.serialize(createdChallenge)).created();
         },
       },
