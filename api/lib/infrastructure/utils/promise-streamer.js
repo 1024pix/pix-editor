d4f45dd058bf949300fd5ac40e5086b6163b0b4b
jeremie <jeremie.jade@pix.fr>
2025-04-16 17:39:25 +0200

    Revert "logg"
    This reverts commit 0f4078d4e514082434f4e34c9af2d1a18c8113e2.

--- api/lib/infrastructure/utils/promise-streamer.js
+++ api/lib/infrastructure/utils/promise-streamer.js
@@ -72,7 +72,7 @@ export function promiseStreamerForRepli2(promise) {
       { event: 'lcms:debug-epipe' },prefixWithDate('Clearing interval'));
     clearInterval(timer);
     logger.info(
-      { event: 'lcms:debug-epipe' },prefixWithDate('Creating the readable stream'));
+      { event: 'lcms:debug-epipe' },prefixWithDate('Start sending data into stream'));
     const readableStream = Readable.from(JSON.stringify(data));
     readableStream.on('close', () => {
       logger.info(
@@ -102,8 +102,6 @@ export function promiseStreamerForRepli2(promise) {
       logger.info(
         { event: 'lcms:debug-epipe' },prefixWithDate('ReadableStream event: resume'));
     });
-    logger.info(
-      { event: 'lcms:debug-epipe' },prefixWithDate('Start sending data into stream'));
     pipeline(
       readableStream,
       writableStream,
