69bc97e56fd4d0d7ca69e17835c1c2ad5e84e887
Laura Bergoens <laura.bergoens@pix.fr>
2025-04-17 11:19:13 +0200

    tech(api): integrate normalizeChallenge operations in map loop
    Co-authored-by: Iris Benoît-Martin <iris.benoit-martin@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/lib/domain/usecases/get-learning-content-for-replication.js
+++ api/lib/domain/usecases/get-learning-content-for-replication.js
@@ -77,14 +77,17 @@ export async function getLearningContentForReplication() {
           translationForChallenge.entityId = localizedChallenge.id;
           translationForChallenge.sourceEntityId = challenge.id;
         }
+        localizedChallenge.area = localizedChallenge.geography;
+        delete localizedChallenge.localizedChallenges;
         return localizedChallenge;
       });
+      challenge.area = challenge.geography;
+      delete challenge.localizedChallenges;
       return [
         challenge,
         ...translatedChallenges,
       ];
-    })
-    .map(normalizeChallenge);
+    });
 
   const translatedAttachments = attachments.map((attachment) => ({
     ...attachment,
@@ -122,10 +125,4 @@ async function _getCoursesFromPGForReplication() {
   return knex('static_courses')
     .select(['id', 'name'])
     .orderBy('id');
-}
-
-function normalizeChallenge(challenge) {
-  delete challenge.localizedChallenges;
-  challenge.area = challenge.geography;
-  return challenge;
 }
