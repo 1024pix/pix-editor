664f476dd1c8857f4c76c4dd5df38d9c61a02bfc
Laura Bergoens <laura.bergoens@pix.fr>
2025-04-17 11:53:03 +0200

    tech(api): chunk json string to avoid overflowing stream
    Co-authored-by: Iris Benoît-Martin <iris.benoit-martin@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/lib/infrastructure/utils/promise-streamer.js
+++ api/lib/infrastructure/utils/promise-streamer.js
@@ -1,6 +1,6 @@
-import * as Sentry from '@sentry/node';
-import { logger } from '../logger.js';
-import { PassThrough } from 'node:stream';
+import { PassThrough, pipeline } from 'node:stream';
+
+const NB_CHARS_PER_CHUNK = 65_536;
 
 function getWritableStream() {
   const writableStream = new PassThrough();
@@ -18,15 +18,25 @@ export function promiseStreamer(promise, writableStream = getWritableStream()) {
   const timer = setInterval(() => {
     writableStream.write('\n');
   }, 1000);
+
   promise.then((data) => {
-    writableStream.write(JSON.stringify(data));
-  }).catch((error) => {
-    logger.error(error);
-    Sentry.captureException(error);
-    writableStream.write('error');
-  }).finally(() => {
     clearInterval(timer);
-    writableStream.end();
+    pipeline(
+      chunk(data),
+      writableStream,
+    );
+  }).catch(() => {
+    if (!writableStream.closed) {
+      writableStream.write('error');
+    }
+    clearInterval(timer);
   });
   return writableStream;
+}
+
+function* chunk(data) {
+  const stringifiedData = JSON.stringify(data);
+  for (let i = 0; i < stringifiedData.length; i += NB_CHARS_PER_CHUNK) {
+    yield stringifiedData.slice(i, i + NB_CHARS_PER_CHUNK);
+  }
 }
