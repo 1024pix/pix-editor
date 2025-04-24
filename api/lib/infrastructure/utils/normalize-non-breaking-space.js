8a9467d676de6eb06fe39ba543e6d9587536c00c
Eithliu <iris.benoit@pix.fr>
2025-04-24 11:09:05 +0200

    feat(api): if there's a space before €$% chars, it will be replaced by a NNBSP. If there's no space before, nothing will happen.

--- api/lib/infrastructure/utils/normalize-non-breaking-space.js
+++ api/lib/infrastructure/utils/normalize-non-breaking-space.js
@@ -1,6 +1,5 @@
 export function normalizeNonBreakingSpace(str) {
   return str
-    .replaceAll(/ ?([€$%])/g, ' $1')
     .replaceAll(/ ?(°C)/g, ' $1')
-    .replaceAll(/ ([;?!])/g, ' $1');
+    .replaceAll(/ ([;?!€$%])/g, ' $1');
 }
