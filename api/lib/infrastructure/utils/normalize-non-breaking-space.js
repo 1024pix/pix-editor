b55fefd4f166b4af2d2bcc5f956943d3e29e8b7e
jeremie <jeremie.jade@pix.fr>
2025-05-22 11:28:00 +0200

    feat(api): Do not create spaces if don't have

--- api/lib/infrastructure/utils/normalize-non-breaking-space.js
+++ api/lib/infrastructure/utils/normalize-non-breaking-space.js
@@ -1,9 +1,9 @@
 export function normalizeNonBreakingSpace(str) {
   if (!str) return str;
   return str
-    .replaceAll(/\p{Zs}?(°C)/gu, ' $1')
+    .replaceAll(/\p{Zs}(°C)/gu, ' $1')
     .replaceAll(/\p{Zs}([;?!€$%])/gu, ' $1')
     .replaceAll(/\p{Zs}(:)/gu, ' $1')
-    .replaceAll(/\p{Zs}?(»)/gu, ' $1')
-    .replaceAll(/(«)\p{Zs}?/gu, '$1 ');
+    .replaceAll(/\p{Zs}(»)/gu, ' $1')
+    .replaceAll(/(«)\p{Zs}/gu, '$1 ');
 }
