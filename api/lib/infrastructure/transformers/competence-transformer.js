84f9b655a355950a9ef0a4c2cc13cd917d4d6c2a
Laura Bergoens <laura.bergoens@pix.fr>
2025-06-14 23:03:49 +0200

    tech(api): delete dead code

--- api/lib/infrastructure/transformers/competence-transformer.js
+++ api/lib/infrastructure/transformers/competence-transformer.js
@@ -28,29 +28,3 @@ export function forReplication(competences) {
   }
   return new CompetenceForReplication(competences);
 }
-
-export function filterCompetencesFields(competences) {
-  return competences.map(filterCompetenceFields);
-}
-
-export function filterCompetenceFields({
-  id,
-  index,
-  name_i18n,
-  description_i18n,
-  areaId,
-  skillIds,
-  thematicIds,
-  origin,
-}) {
-  return {
-    id,
-    index,
-    name_i18n,
-    description_i18n,
-    areaId,
-    skillIds,
-    thematicIds,
-    origin,
-  };
-}
