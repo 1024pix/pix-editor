6a7b728d414859a383a2f2774dcfe5bb2c27074b
Laura Bergoens <laura.bergoens@pix.fr>
2025-06-14 22:39:59 +0200

    tech(api): delete dead code

--- api/lib/infrastructure/transformers/framework-transformer.js
+++ api/lib/infrastructure/transformers/framework-transformer.js
@@ -28,11 +28,3 @@ export function forReplication(frameworks) {
   }
   return new FrameworkForReplication(frameworks);
 }
-
-export function filterFrameworkFields({ id, name }) {
-  return { id, name };
-}
-
-export function filterFrameworksFields(frameworks) {
-  return frameworks.map(filterFrameworkFields);
-}
