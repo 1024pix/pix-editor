4a25f556a4a20046e7ee41b5a0ed7c67a37017f0
Laura Bergoens <laura.bergoens@pix.fr>
2025-06-14 15:19:13 +0200

    tech(api): add forReplication and forRelease methods on frameworkTransformer

--- api/lib/domain/models/replication/index.js
+++ api/lib/domain/models/replication/index.js
@@ -1,2 +1,3 @@
+export * from './FrameworkForReplication.js';
 export * from './TubeForReplication.js';
 export * from './TutorialForReplication.js';
--- api/lib/infrastructure/transformers/framework-transformer.js
+++ api/lib/infrastructure/transformers/framework-transformer.js
@@ -1,7 +1,38 @@
+import { FrameworkForRelease } from '../../domain/models/release/index.js';
+import { FrameworkForReplication } from '../../domain/models/replication/index.js';
+
+/**
+ * @typedef {import('../../../lib/domain/models').Framework} Framework
+ * @typedef {import('../../../lib/domain/models/release').FrameworkForRelease} FrameworkForRelease
+ * @typedef {import('../../../lib/domain/models/replication').FrameworkForReplication} FrameworkForReplication
+ */
+
 export function filterFrameworkFields({ id, name }) {
   return { id, name };
 }
 
 export function filterFrameworksFields(frameworks) {
   return frameworks.map(filterFrameworkFields);
+}
+
+/**
+ * @param {Framework|Framework[]} frameworks
+ * @returns {FrameworkForRelease|FrameworkForRelease[]}
+ */
+export function forRelease(frameworks) {
+  if (Array.isArray(frameworks)) {
+    return frameworks.map((framework) => new FrameworkForRelease(framework));
+  }
+  return new FrameworkForRelease(frameworks);
+}
+
+/**
+ @param {Framework|Framework[]} frameworks
+ @returns {FrameworkForReplication|FrameworkForReplication[]}
+ */
+export function forReplication(frameworks) {
+  if (Array.isArray(frameworks)) {
+    return frameworks.map((framework) => new FrameworkForReplication(framework));
+  }
+  return new FrameworkForReplication(frameworks);
 }
