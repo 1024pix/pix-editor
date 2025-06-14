14363c75955edea02e63df4f6245c9e6135797c4
Laura Bergoens <laura.bergoens@pix.fr>
2025-06-14 23:02:03 +0200

    tech(api): add forRelease and forReplication methods on competence transformer

---
+++ api/lib/domain/models/replication/CompetenceForReplication.js
@@ -0,0 +1,21 @@
+export class CompetenceForReplication {
+  constructor({
+    id,
+    name_i18n,
+    index,
+    description_i18n,
+    areaId,
+    skillIds,
+    thematicIds,
+    origin,
+  }) {
+    this.id = id;
+    this.index = index;
+    this.areaId = areaId;
+    this.skillIds = skillIds;
+    this.thematicIds = thematicIds;
+    this.origin = origin;
+    this.name_i18n = name_i18n;
+    this.description_i18n = description_i18n;
+  }
+}
