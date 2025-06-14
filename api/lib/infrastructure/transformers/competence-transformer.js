b1e29464979d2a633d574024b35c4cc25b3b310b
Laura Bergoens <laura.bergoens@pix.fr>
2025-06-14 15:49:05 +0200

    tech(api): add forReplication and forRelease methods on competenceTransformer

--- api/lib/infrastructure/transformers/competence-transformer.js
+++ api/lib/infrastructure/transformers/competence-transformer.js
@@ -1,3 +1,34 @@
+import { CompetenceForRelease } from '../../domain/models/release/index.js';
+import { CompetenceForReplication } from '../../domain/models/replication/index.js';
+
+/**
+ * @typedef {import('../../../lib/domain/models').Competence} Competence
+ * @typedef {import('../../../lib/domain/models/release').CompetenceForRelease} CompetenceForRelease
+ * @typedef {import('../../../lib/domain/models/replication').CompetenceForReplication} CompetenceForReplication
+ */
+
+/**
+ * @param {Competence|Competence[]} competences
+ * @returns {CompetenceForRelease|CompetenceForRelease[]}
+ */
+export function forRelease(competences) {
+  if (Array.isArray(competences)) {
+    return competences.map((competence) => new CompetenceForRelease(competence));
+  }
+  return new CompetenceForRelease(competences);
+}
+
+/**
+ @param {Competence|Competence[]} competences
+ @returns {CompetenceForReplication|CompetenceForReplication[]}
+ */
+export function forReplication(competences) {
+  if (Array.isArray(competences)) {
+    return competences.map((competence) => new CompetenceForReplication(competence));
+  }
+  return new CompetenceForReplication(competences);
+}
+
 export function filterCompetencesFields(competences) {
   return competences.map(filterCompetenceFields);
 }
