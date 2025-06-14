50f036529f0afedd01fdcd2e06e3f0609042afda
Laura Bergoens <laura.bergoens@pix.fr>
2025-06-14 23:02:25 +0200

    tech(api): use transformer methods in release and repli

--- api/lib/domain/usecases/get-learning-content-for-replication.js
+++ api/lib/domain/usecases/get-learning-content-for-replication.js
@@ -74,7 +74,6 @@ export async function getLearningContentForReplication() {
     entityId: translation.entityId,
     sourceEntityId: null,
   }));
-  const transformedCompetences = competenceTransformer.filterCompetencesFields(competences);
   const transformedThematics = thematicTransformer.filterThematicsFields(thematics);
   const transformedTubes = tubeTransformer.transformTubes(tubes, thematics, challenges);
 
@@ -123,7 +122,7 @@ export async function getLearningContentForReplication() {
   return {
     frameworks: frameworkTransformer.forReplication(frameworks),
     areas: areaTransformer.forReplication(areas),
-    competences: transformedCompetences,
+    competences: competenceTransformer.forReplication(competences),
     thematics: transformedThematics,
     tubes: transformedTubes,
     skills: skillTransformer.forReplication(skills),
--- api/lib/infrastructure/repositories/release-repository.js
+++ api/lib/infrastructure/repositories/release-repository.js
@@ -110,14 +110,13 @@ async function _getCurrentContent() {
   const transformedTubes = tubeTransformer.transformTubes(tubes, thematics, challenges);
   const transformedThematics = thematicTransformer.filterThematicsFields(thematics);
 
-  const filteredCompetences = competenceTransformer.filterCompetencesFields(competences);
   const filteredTutorials = tutorialTransformer.filterTutorialsFields(tutorials);
   const transformedMissions = missionTransformer.transform({ missions, challenges, tubes, thematics, skills });
 
   return {
     frameworks: frameworkTransformer.forRelease(frameworks),
     areas: areaTransformer.forRelease(areas),
-    competences: filteredCompetences,
+    competences: competenceTransformer.forRelease(competences),
     thematics: transformedThematics,
     tubes: transformedTubes,
     skills: skillTransformer.forRelease(skills),
