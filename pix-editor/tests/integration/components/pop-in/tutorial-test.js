0d9daea91d616de665c48ab3320c70edd020c490
Fael BASSETTI <fael.bassetti@pix.fr>
2025-06-25 18:05:54 +0200

    test(tutorial): fix integration tests

--- pix-editor/tests/integration/components/pop-in/tutorial-test.js
+++ pix-editor/tests/integration/components/pop-in/tutorial-test.js
@@ -1,4 +1,4 @@
-import { render } from '@ember/test-helpers';
+import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 
@@ -7,56 +7,49 @@ import { setupIntlRenderingTest } from '../../../setup-intl-rendering';
 module('Integration | Component | pop-in/tutorial', function(hooks) {
   setupIntlRenderingTest(hooks);
 
+  let store;
+
   hooks.beforeEach(function() {
     const configService = this.owner.lookup('service:config');
     configService.tutorialLocaleToLanguageMap = {
       lang: 'Première langue',
     };
+    store = this.owner.lookup('service:store');
   });
 
   test('save input should be disabled if mandatory field are empty', async function(assert) {
     //given
     this.set('close', () => {});
     this.set('saveTutorial', () => {});
-    this.set('tutorial', {
-      title: '',
-      language: '',
-      link: '',
-      source: '',
-      license: '',
-      format: '',
-      duration: '',
-      level: '',
-      tags: [],
-    });
+
+    const emptyTutorial = store.createRecord('tutorial', {});
+    this.set('tutorial', emptyTutorial);
 
     //when
-    await render(hbs`<PopIn::Tutorial @close={{this.close}} @tutorial={{this.tutorial}} @saveTutorial={{this.saveTutorial}}/>`);
+    const screen = await render(hbs`<PopIn::Tutorial @close={{this.close}} @tutorial={{this.tutorial}} @saveTutorial={{this.saveTutorial}} @showModal={{true}} />`);
 
     //then
-    assert.dom('[data-test-save-tutorial-button]').isDisabled();
+    assert.dom(screen.getByRole('button', { name: 'Enregistrer' })).hasAria('disabled', 'true');
   });
 
   test('save input should not be disabled if mandatory field are empty', async function(assert) {
     //given
     this.set('close', () => {});
     this.set('saveTutorial', () => {});
-    this.set('tutorial', {
+    const filledTutorial = store.createRecord('tutorial', {
       title: 'title',
-      language: 'fr-fr',
+      language: 'fr',
       link: 'link',
       source: 'source',
-      license: '',
       format: 'image',
       duration: '00:20:00',
-      level: '',
-      tags: [],
     });
+    this.set('tutorial', filledTutorial);
 
     //when
-    await render(hbs`<PopIn::Tutorial @close={{this.close}} @tutorial={{this.tutorial}} @saveTutorial={{this.saveTutorial}}/>`);
+    const screen = await render(hbs`<PopIn::Tutorial @close={{this.close}} @tutorial={{this.tutorial}} @saveTutorial={{this.saveTutorial}} @showModal={{true}} />`);
 
-    //then
-    assert.dom('[data-test-save-tutorial-button]').isNotDisabled();
+    // then
+    assert.dom(screen.getByRole('button', { name: 'Enregistrer' })).hasAria('disabled', 'false');
   });
 });
