15472bf455b279a9c11a9945d1f486e153a54004
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-10-09 11:39:53 +0200

    tech(api): double write tutorial tags to both PG and Airtable

--- api/lib/infrastructure/repositories/tag-repository.js
+++ api/lib/infrastructure/repositories/tag-repository.js
@@ -1,10 +1,14 @@
 import { Tag } from '../../domain/models/index.js';
 import { tagDatasource } from '../datasources/airtable/index.js';
 import { generateNewId } from '../utils/id-generator.js';
+import { knex } from '../../../db/knex-database-connection.js';
 
 export async function create(tag) {
   tag.id = generateNewId('tag');
-  const datasourceTag = await tagDatasource.create(tag);
+  const [datasourceTag] = await Promise.all([
+    tagDatasource.create(tag),
+    knex('tutorial_tags').insert({ id: tag.id, title: tag.title, notes: tag.notes }),
+  ]);
   return toDomain(datasourceTag);
 }
 
