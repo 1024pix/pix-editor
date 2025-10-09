fea5b3c9b1cede9e3de507c3f59b427c0d6155ce
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-10-09 11:57:13 +0200

    tech(api): add script to migrate tutorial tags from Airtable to postgres

---
+++ api/scripts/migration-from-airtable/copy-tutorial_tags-from-airtable-to-pg.js
@@ -0,0 +1,46 @@
+import { knex } from '../../db/knex-database-connection.js';
+import { ScriptRunner } from '../../lib/application/scripts/script-runner.js';
+import { Script } from '../../lib/application/scripts/script.js';
+import * as airtable from '../../lib/infrastructure/airtable.js';
+
+export class CopyTutorialTagsFromAirtableToPg extends Script {
+
+  constructor() {
+    super({
+      description: 'Copie des tags de tutoriels de Airtable vers Postgres',
+      permanent: false,
+      options: {
+        dryRun: {
+          type: 'boolean',
+          describe: 'If true, it does not persist insert/updates made during the script.',
+          demandOption: false,
+          default: true,
+        },
+      },
+    });
+  }
+
+  async handle({ options, logger }) {
+    logger.info({ dryRun: options.dryRun }, 'Script options');
+
+    const airtableTutorialTags = await airtable.findRecords('Tags', {
+      fields: ['id persistant', 'Nom', 'Notes'],
+    });
+    logger.info({ count: airtableTutorialTags.length }, 'Loaded tutorial tags from airtable');
+
+    const tutorialTags = airtableTutorialTags.map((record) => ({
+      id: record.get('id persistant'),
+      title: record.get('Nom'),
+      notes: record.get('Notes'),
+      createdAt: record._rawJson.createdTime,
+      updatedAt: knex.fn.now(),
+    }));
+
+    if (options.dryRun) return;
+
+    await knex.insert(tutorialTags).into('tutorial_tags').onConflict('id').merge();
+    logger.info({ count: tutorialTags.length }, 'Inserted tutorial tags into postgres');
+  }
+}
+
+await ScriptRunner.execute(import.meta.url, CopyTutorialTagsFromAirtableToPg);
