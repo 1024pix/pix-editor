3add3f7699d3511bce5be796547284ed64f9a8c0
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-10-09 11:20:58 +0200

    tech(api): create tutorial_tags table

---
+++ api/db/migrations/20251009091422_create-tags-table.js
@@ -0,0 +1,22 @@
+const TABLE_NAME = 'tutorial_tags';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable(TABLE_NAME, function(table) {
+    table.string('id').notNullable().primary();
+    table.string('title').notNullable();
+    table.string('notes').nullable();
+    table.timestamps(true, true, true);
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.dropTable(TABLE_NAME);
+}
