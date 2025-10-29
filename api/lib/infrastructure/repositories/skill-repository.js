1f4082d58a5d6dd4aadac4013e4ffb5b14a07bd4
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2025-10-29 17:11:04 +0100

    fix(api): sorts skills with same order as airtable when searching by name
    Co-authored-by: Ce que tu veux <guillaume.lagorce@pix.fr>

--- api/lib/infrastructure/repositories/skill-repository.js
+++ api/lib/infrastructure/repositories/skill-repository.js
@@ -135,7 +135,7 @@ export async function search(params) {
   if (params.sort) {
     params.sort.forEach(([field, direction]) => {
       if (field === 'name') {
-        query = query.orderByRaw('?? collate ??', ['name', 'fr-x-icu']);
+        query = query.orderByRaw('?? collate ??, ??', ['tubes.name', 'fr-x-icu', 'skills.level']);
       } else {
         query = query.orderBy(field, direction);
       }
