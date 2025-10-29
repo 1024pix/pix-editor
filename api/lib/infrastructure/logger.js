f2b57ac52cc95acfa54fc8edbf74b9440b0302df
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-10-29 09:29:31 +0100

    tech(api): use same logging functions as in Pix API

--- api/lib/infrastructure/logger.js
+++ api/lib/infrastructure/logger.js
@@ -7,9 +7,15 @@ const nullDestination = { write() {} };
 
 let destination = pino.destination();
 
-if (config.logging.prettyPrint) {
+if (config.logging.logForHumans) {
+  const omitDay = 'HH:MM:ss';
   destination = pretty({
+    sync: true,
     colorize: true,
+    translateTime: omitDay,
+    ignore: 'pid,hostname',
+    messageFormat: config.logging.logForHumansCompactFormat ? messageFormatCompact : undefined,
+    hideObject: config.logging.logForHumansCompactFormat,
   });
 }
 
@@ -41,6 +47,49 @@ export const SCOPES = {
   REPLICATION: 'replication',
   RELEASE: 'release',
 };
+
+function messageFormatCompact(log, messageKey, _logLevel, { colors }) {
+  const message = log[messageKey];
+  const { err, req, res, responseTime } = log;
+
+  // compact log for errors
+  if (err) {
+    const stack = colors.red(err.stack);
+    return `${message}\n${stack}`;
+  }
+
+  // compact log for HTTP requests
+  if (req && res) {
+    const method = req.method?.toUpperCase();
+
+    const queries = req.metrics?.knexQueryCount ? `sql:${req.metrics.knexQueryCount}` : '';
+    const queriesTime = req.metrics?.knexTotalTimeSpent ? `sql-time:${req.metrics.knexTotalTimeSpent}` : '';
+
+    const statusCode = res.statusCode >= 400 ? colors.red(res.statusCode) : colors.greenBright(res.statusCode);
+    const request = colors.magentaBright([method, req.url].filter(Boolean).join(' '));
+    const details = colors.yellow([queries, queriesTime].filter(Boolean).join(' '));
+    const time = colors.gray(`(${responseTime}ms)`);
+
+    return [statusCode, request, details, time].filter(Boolean).join(' - ');
+  }
+
+  // compact log by default
+  const compactLog = omit(log, [
+    messageKey,
+    'id',
+    'level',
+    'time',
+    'pid',
+    'hostname',
+    'uri',
+    'address',
+    'event',
+    'started',
+    'created',
+  ]);
+  const details = !isEmpty(compactLog) ? colors.gray(JSON.stringify(compactLog)) : '';
+  return `${message} ${details}`;
+}
 
 logger.debug('DEBUG logs enabled');
 logger.trace('TRACE logs enabled');
