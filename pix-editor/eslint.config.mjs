3a86e6ca18ef906d9bf361baa1f0c115af70124f
mcampourcy <mireille.campourcy@pix.fr>
2025-11-03 16:40:27 +0100

    tech(pixeditor-dependencies): Add stylistic for pixeditor to manage deprecated rules from eslint

--- pix-editor/eslint.config.mjs
+++ pix-editor/eslint.config.mjs
@@ -1,4 +1,3 @@
-import pixRecommendedConfig from '@1024pix/eslint-plugin/config';
 import babelParser from '@babel/eslint-parser';
 import emberParser from 'ember-eslint-parser';
 import emberRecommendedConfig from 'eslint-plugin-ember/configs/recommended';
@@ -10,11 +9,19 @@ import globals from 'globals';
 const unconventionalJsFiles = ['blueprints/**/files/*', 'app/vendor/*'];
 const compiledOutputFiles = ['dist/*', 'tmp/*'];
 const dependenciesFiles = ['bower_components/*', 'node_modules/*'];
-const miscFiles = ['coverage/*', '!**/.*', '**/.eslintcache'];
-const emberTryFiles = ['.node_modules.ember-try/*', 'bower.json.ember-try', 'package.json.ember-try'];
+const miscFiles = [
+  'coverage/*',
+  '!**/.*',
+  '**/.eslintcache',
+];
+const emberTryFiles = [
+  '.node_modules.ember-try/*',
+  'bower.json.ember-try',
+  'package.json.ember-try',
+];
+import stylistic from '@stylistic/eslint-plugin';
 
 const nodeFiles = [
-  'eslint.config.cjs',
   '.template-lintrc.js',
   'ember-cli-build.js',
   'testem.js',
@@ -28,19 +35,45 @@ const nodeFiles = [
 ];
 
 export default [
-  ...pixRecommendedConfig,
   ...emberRecommendedConfig,
   ...emberGjsRecommendedConfig,
   qunitRecommendedConfig,
+  stylistic.configs.customize({
+    'jsx': false,
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+    'object-curly-spacing': ['error', 'always'],
+    'quote-props': ['error', 'as-needed'],
+    'quotes': 'single',
+    'semi': true,
+    'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'ignore' }],
+    'space-infix-ops': ['error'],
+  }),
   {
-    ignores: [...unconventionalJsFiles, ...compiledOutputFiles, ...dependenciesFiles, ...miscFiles, ...emberTryFiles],
+    plugins: { '@stylistic': stylistic },
+    rules: {
+      '@stylistic/array-bracket-newline': ['error', { multiline: true }],
+      '@stylistic/array-element-newline': ['error', { multiline: true, minItems: 3 }],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/curly-newline': ['error', { consistent: true, ClassBody: { minElements: 1 } }],
+      '@stylistic/object-curly-newline': ['error', { multiline: true }],
+      '@stylistic/function-call-spacing': ['error', 'never'],
+      '@stylistic/switch-colon-spacing': ['error', { after: true, before: false }],
+    },
+  },
+  {
+    ignores: [
+      ...unconventionalJsFiles,
+      ...compiledOutputFiles,
+      ...dependenciesFiles,
+      ...miscFiles,
+      ...emberTryFiles,
+    ],
   },
   {
     ignores: ['**/*.yaml'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+      globals: { ...globals.browser },
 
       parser: babelParser,
       ecmaVersion: 2018,
@@ -49,28 +82,14 @@ export default [
       parserOptions: {
         requireConfigFile: false,
 
-        babelOptions: {
-          plugins: [
-            [
-              '@babel/plugin-proposal-decorators',
-              {
-                version: 'legacy',
-              },
-            ],
-          ],
-        },
+        babelOptions: { plugins: [['@babel/plugin-proposal-decorators', { version: 'legacy' }]] },
       },
     },
 
     rules: {
       'no-setter-return': 'off',
 
-      'ember/no-controller-access-in-routes': [
-        'error',
-        {
-          allowControllerFor: true,
-        },
-      ],
+      'ember/no-controller-access-in-routes': ['error', { allowControllerFor: true }],
 
       'qunit/require-expect': ['error', 'except-simple'],
       'ember/template-no-let-reference': 'off',
@@ -88,9 +107,7 @@ export default [
     files: nodeFiles,
 
     languageOptions: {
-      globals: {
-        ...globals.node,
-      },
+      globals: { ...globals.node },
 
       ecmaVersion: 5,
       sourceType: 'script',
