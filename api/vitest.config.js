a4013197498f078806d9debf520a8cb01c0eb0fc
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-11-04 16:00:17 +0100

    refactor: set default reporter for vitest

--- api/vitest.config.js
+++ api/vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['tests/**/*_test.js'],
-    reporters: process.env.CI ? 'junit' : 'dot',
+    reporters: process.env.CI ? 'junit' : 'default',
     outputFile: process.env.CI ? './test-results/report.xml' : undefined,
     restoreMocks: true,
     poolOptions: {
