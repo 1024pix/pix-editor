1eb93ad20fd5d22b5fc982b99de16b6a1d2c18fe
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-11-04 16:00:35 +0100

    refactor: move before/after in setup-test

--- api/tests/setup-tests.js
+++ api/tests/setup-tests.js
@@ -1,3 +1,27 @@
+import { afterEach, beforeAll, afterAll } from 'vitest';
 import { loadEnvFileIfExists } from '../lib/shared/load-env-file-if-exists.js';
+import { airtableBuilder, databaseBuilder } from './test-helper.js';
+import { cache } from '../lib/infrastructure/cache.js';
+import nock from 'nock';
+import { queues } from '../lib/infrastructure/scheduled-jobs/create-queue.js';
+import { disconnect } from '../db/knex-database-connection.js';
 
 loadEnvFileIfExists();
+
+beforeAll(() => {
+  nock.disableNetConnect();
+});
+
+afterEach(async () => {
+  airtableBuilder.cleanAll();
+  await databaseBuilder.clean();
+  cache.flushAll();
+  nock.cleanAll();
+  for (const queue of queues) {
+    await queue.obliterate({ force: true });
+  }
+});
+
+afterAll(async () => {
+  await disconnect();
+});
