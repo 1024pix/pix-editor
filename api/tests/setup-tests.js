8526e29f7f7a2b12f92e5e3ff385264345b0be5e
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-11-04 14:52:16 +0100

    refactor: rework release job instanciation

--- api/tests/setup-tests.js
+++ api/tests/setup-tests.js
@@ -1,6 +1,6 @@
-import { afterEach, beforeAll, afterAll } from 'vitest';
 import { loadEnvFileIfExists } from '../lib/shared/load-env-file-if-exists.js';
 import { disconnect } from '../db/knex-database-connection.js';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
 import { queues } from '../lib/infrastructure/scheduled-jobs/index.js';
 import { cache } from '../lib/infrastructure/cache.js';
 import { airtableBuilder, databaseBuilder } from './test-helper.js';
