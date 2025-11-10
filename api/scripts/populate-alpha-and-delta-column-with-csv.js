5e00e3ba04df8d2f58edeb002af1e2dde23aa275
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-11-10 12:18:01 +0100

    refactor(api): rewrite populate-alpha-and-delta-column-with-csv to avoid airtable

--- api/scripts/populate-alpha-and-delta-column-with-csv.js
+++ api/scripts/populate-alpha-and-delta-column-with-csv.js
@@ -4,14 +4,13 @@
 // To run the script:
 // > cd scripts
 // > npm ci
-// > AIRTABLE_API_KEY=XXX AIRTABLE_BASE=XXXX node populate-alpha-and-delta-column-with-csv/
+// > node populate-alpha-and-delta-column-with-csv/
 
 import _ from 'lodash';
 
 import fs from 'fs';
-import Airtable from 'airtable';
 import { parseString } from '@fast-csv/parse';
-import ProgressBar from 'progress';
+import { knex } from '../db/knex-database-connection';
 
 const HEADERS_MAPPING = {
   items: 'id',
@@ -45,108 +44,16 @@ export function parseData(csvData) {
   });
 }
 
-export async function findAirtableIds(base, challengesWithPersistentIds) {
-  const challengesWithPersistentIdsWithChunk = _.chunk(challengesWithPersistentIds, 100);
-  const promises = challengesWithPersistentIdsWithChunk.map((chunk) => {
-    return base
-      .select({
-        fields: ['Record ID', 'id persistant'],
-        filterByFormula: 'OR(' + chunk.map(({ id }) => `'${id}' = {id persistant}`).join(',') + ')',
-      })
-      .all();
-  });
-  const airtableRecords = (await Promise.all(promises)).flat();
-
-  return airtableRecords.map((airtableRecord) => {
-    const data = challengesWithPersistentIds.find((row) => row.id === airtableRecord.get('id persistant'));
-    return {
-      ...data,
-      id: airtableRecord.id,
-    };
-  });
-}
-
-export async function updateRecords(base, data) {
-  const payloadWithoutLimit = data.map(({ id, alpha, delta }) => {
-    return {
-      id,
-      fields: {
-        'Difficulté calculée': `${delta}`,
-        'Discrimination calculée': `${alpha}`,
-      },
-    };
-  });
-  const payloadByChunk = _.chunk(payloadWithoutLimit, 10);
-  const bar = new ProgressBar('[:bar] :percent', {
-    total: payloadByChunk.length,
-    width: 50,
-  });
-  const promises = payloadByChunk.map((payload) => {
-    return new Promise((resolve, reject) => {
-      base.update(
-        payload,
-        (err) => {
-          bar.tick();
-          if (err) reject();
-          else resolve();
-        });
-    });
-  });
-  return Promise.all(promises);
-}
-
-export async function clearDifficultyAndDiscriminant(base) {
-  try {
-    const records = await base.select({
-      fields: ['Difficulté calculée', 'Discrimination calculée'],
-      filterByFormula: 'OR({Difficulté calculée}, {Discrimination calculée})',
-    }).all();
-
-    console.log(`Purging ${records.length} records`);
-
-    const recordsWithoutDifficultyAndDiscriminant = records.map(({ id }) => ({
-      id,
-      fields: {
-        'Difficulté calculée': null,
-        'Discrimination calculée': null,
-      },
-    }));
-
-    const chunks = _.chunk(recordsWithoutDifficultyAndDiscriminant, 10);
-
-    const bar = new ProgressBar('[:bar] :percent', {
-      total: chunks.length,
-      width: 50,
-    });
-
-    await Promise.all(chunks.map(async (chunk) => {
-      await base.update(chunk);
-      bar.tick();
-    }));
-
-    console.log('Records purged successfully');
-  } catch (error) {
-    console.error('Error updating records:', error);
-  }
-}
-
-function getBaseChallenges() {
-  const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE);
-
-  return base('Epreuves');
-}
-
 async function main() {
   const csv = fs.readFileSync('./file.csv', 'utf-8');
 
-  const base = getBaseChallenges();
-  await clearDifficultyAndDiscriminant(base);
+  await knex('challenges').update({ alpha: null, delta: null });
   console.log('Parsing CSV Data');
   const matchedData = await parseData(csv);
-  console.log('Mapping CSV Data ids to airtable IDs');
-  const matchedDataWithAirtableIds = await findAirtableIds(base, matchedData);
   console.log('Updating records');
-  await updateRecords(base, matchedDataWithAirtableIds);
+  for (const data of matchedData) {
+    await knex('challenges').update({ alpha: data.alpha, delta: data.delta }).where({ id: data.id });
+  }
 }
 
 if (process.env.NODE_ENV !== 'test') {
