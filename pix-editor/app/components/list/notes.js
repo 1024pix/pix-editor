48b928ad3ad9516fce1ab41df8b849fd66b9efd8
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-11-24 15:39:06 +0100

    feat(pix-editor): use PixTable for notes

--- pix-editor/app/components/list/notes.js
+++ pix-editor/app/components/list/notes.js
@@ -3,69 +3,20 @@ import { action } from '@ember/object';
 import SortedList from './sorted';
 
 export default class NoteList extends SortedList {
-  sortHandlers = {
-    date: {
-      type: 'date',
-      field: 'createdAt',
-    },
-  };
-
-  get headers() {
-    const headers = [
-      {
-        name: 'Date',
-        valuePath: 'date',
-        maxWidth: 150,
-      },
-    ];
-    if (this.displayAuthor) {
-      headers.push({
-        name: 'Auteur',
-        valuePath: 'author',
-        maxWidth: 150,
-        style: 'author-note',
-      });
-    }
-    headers.push({
-      name: 'Texte',
-      valuePath: 'text',
-    });
-    if (this.displayStatus) {
-      headers.push({
-        name: 'Statut',
-        valuePath: 'status',
-        maxWidth: 150,
-        style: 'status-note',
-
-      });
-    }
-    return headers;
-  }
-
   @action
   selectRow(row) {
     this.args.show(row);
   }
 
   get list() {
-    if (Array.isArray(this.args.list)) {
-      return this.args.list;
-    } else {
-      return [];
-    }
+    return Array.isArray(this.args.list) ? this.args.list : [];
   }
 
   get displayAuthor() {
-    if (typeof this.args.displayAuthor !== 'undefined') {
-      return this.args.displayAuthor;
-    }
-    return true;
+    return this.args.displayAuthor ?? true;
   }
 
   get displayStatus() {
-    if (typeof this.args.displayStatus !== 'undefined') {
-      return this.args.displayStatus;
-    }
-    return true;
+    return this.args.displayStatus ?? true;
   }
 }
