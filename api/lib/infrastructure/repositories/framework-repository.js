cc268fe0aabfae7b86109d2fd04a6515b37d5f8c
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-11-26 14:49:40 +0100

    fix(api): order areas by code inside frameworks

--- api/lib/infrastructure/repositories/framework-repository.js
+++ api/lib/infrastructure/repositories/framework-repository.js
@@ -5,19 +5,7 @@ import * as idGenerator from '../utils/id-generator.js';
 const TABLE_NAME = 'frameworks';
 
 export async function list() {
-  const dtos = await knex
-    .select(
-      '*',
-      knex.raw(
-        'coalesce((??), \'[]\') as "areaIds"',
-        knex
-          .select(knex.raw('json_agg(?? order by ??)', ['areas.id', 'areas.id']))
-          .from('areas')
-          .where('areas.frameworkId', '=', knex.ref(`${TABLE_NAME}.id`)),
-      ),
-    )
-    .from(TABLE_NAME)
-    .orderBy('createdAt');
+  const dtos = await selectFrameworks().orderBy('createdAt');
 
   return dtos.map(toDomain);
 }
@@ -47,7 +35,7 @@ function selectFrameworks() {
       knex.raw(
         'coalesce((??), \'[]\') as "areaIds"',
         knex
-          .select(knex.raw('json_agg(??)', knex.ref('areas.id')))
+          .select(knex.raw('json_agg(?? order by ??)', ['areas.id', 'areas.code']))
           .from('areas')
           .where('areas.frameworkId', '=', knex.ref(`${TABLE_NAME}.id`)),
       ),
