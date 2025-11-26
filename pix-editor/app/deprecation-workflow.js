3fb618705b34f0dee4db03f38a6a3043981e0bb6
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-11-26 10:15:09 +0100

    dep(pix-editor): update deprecation workflow for ember-source v6

--- pix-editor/app/deprecation-workflow.js
+++ pix-editor/app/deprecation-workflow.js
@@ -3,8 +3,36 @@ import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 setupDeprecationWorkflow({
   workflow: [
     {
-      handler: 'error', // this deprecation is only on EmberTable package
-      matchId: 'template-action',
+      handler: 'silence',
+      matchId: 'importing-inject-from-ember-service',
+    },
+    {
+      handler: 'silence',
+      matchId: 'deprecate-import--is-destroying-from-ember',
+    },
+    {
+      handler: 'silence',
+      matchId: 'deprecate-import--is-destroyed-from-ember',
+    },
+    {
+      handler: 'silence',
+      matchId: 'deprecate-import-destroy-from-ember',
+    },
+    {
+      handler: 'silence',
+      matchId: 'deprecate-import--register-destructor-from-ember',
+    },
+    {
+      handler: 'silence',
+      matchId: 'deprecate-import-onerror-from-ember',
+    },
+    {
+      handler: 'silence',
+      matchId: 'warp-drive.deprecate-tracking-package',
+    },
+    {
+      handler: 'silence',
+      matchId: 'warp-drive:deprecate-legacy-request-methods',
     },
   ],
 });
