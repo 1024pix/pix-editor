1b6c35581d7535be4ca4bee3846c7f330c091d7c
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2025-11-28 11:47:10 +0100

    tech(pix-editor): enables recommended .template-lintrc.mjs

--- pix-editor/.template-lintrc.mjs
+++ pix-editor/.template-lintrc.mjs
@@ -1,11 +1,11 @@
-'use strict';
-
-module.exports = {
+export default {
+  extends: ['recommended'],
   rules: {
     'block-indentation': false,
     'attribute-indentation': false,
     'no-invalid-interactive': false,
     'no-inline-styles': false,
     'style-concatenation': false,
+    'no-potential-path-strings': false,
   },
 };
