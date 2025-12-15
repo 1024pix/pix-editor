44c399f36ece1641af2fadce492abd5b50abd27c
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-12-15 19:33:09 +0100

    refactor(pix-editor): use a simple UUID to fake backend id generation in tests

--- pix-editor/tests/mirage/routes.js
+++ pix-editor/tests/mirage/routes.js
@@ -502,14 +502,6 @@ function _applyPagination(data, { page, pageSize }) {
   return slice(data, start, end);
 }
 
-const BASE62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const base62_encode = basex(BASE62).encode;
-
-const RECORD_ID_PREFIX = 'rec';
-
 function newId(prefix = RECORD_ID_PREFIX) {
-  const randomString = random.getRandomAsciiString(10);
-  const buf = new TextEncoder('utf-8').encode(randomString);
-  const randomBase62 = base62_encode(buf);
-  return `${prefix}${randomBase62}`;
+  return `${prefix}${window.crypto.randomUUID()}`;
 }
