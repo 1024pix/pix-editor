2912c4a6e83b1c0c6f3174d6fce9556aa50d3edf
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-12-16 11:40:54 +0100

    dep(pix-editor): migrate ember-intl to v8

--- pix-editor/app/routes/application.js
+++ pix-editor/app/routes/application.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { formats } from 'pixeditor/ember-intl';
 
 export default class ApplicationRoute extends Route {
   @service session;
+  @service intl;
 
   async beforeModel() {
     await this.session.setup();
+    this.intl.setFormats(formats);
   }
 }
