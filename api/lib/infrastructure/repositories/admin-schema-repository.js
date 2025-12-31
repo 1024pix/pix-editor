a8a625858c6e287f91314bc506a696cdaa2b24d9
Fael BASSETTI <fael.bassetti@pix.fr>
2025-12-31 12:19:30 +0100

    feat(api): add localized_challenges to admin dashboard

--- api/lib/infrastructure/repositories/admin-schema-repository.js
+++ api/lib/infrastructure/repositories/admin-schema-repository.js
@@ -1,9 +1,11 @@
+import localizedChallengeSchema from './admin-schemas/localized-challenge-schema.json' with { type: 'json' };
 import translationSchema from './admin-schemas/translation-schema.json' with { type: 'json' };
 import releaseSchema from './admin-schemas/release-schema.json' with { type: 'json' };
 import userSchema from './admin-schemas/user-schema.json' with { type: 'json' };
 
 export function list() {
   return [
+    localizedChallengeSchema,
     translationSchema,
     releaseSchema,
     userSchema,
