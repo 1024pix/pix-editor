8c8807fc27ecb3838cc29392d8dbac05fd5c7ce2
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-01-20 00:19:33 +0100

    feat(pix-editor): redirect from search to localized challenge

--- pix-editor/tests/acceptance/search-test.js
+++ pix-editor/tests/acceptance/search-test.js
@@ -19,11 +19,21 @@ module('Acceptance | Search', function (hooks) {
       instruction: 'test',
       airtableId: 'recChallenge1',
     });
-    this.server.create('challenge', { id: 'challengeChallenge1', airtableId: 'challengeChallenge1' });
+    this.server.create('challenge', {
+      id: 'challengeChallenge1',
+      airtableId: 'challengeChallenge1',
+      locales: ['fr'],
+    });
     this.server.create('localized-challenge', { id: 'challengeChallenge1', challengeId: 'challengeChallenge1' });
     this.server.create('localized-challenge', {
       id: 'challengeLocalizedChallenge1',
       challengeId: 'challengeChallenge1',
+      locale: 'en',
+    });
+    this.server.create('challenge-locale', {
+      locale: 'en',
+      challengeId: 'challengeChallenge1',
+      localizedChallengeId: 'challengeLocalizedChallenge1',
     });
     this.server.create('localized-challenge', { id: 'recChallenge1', challengeId: 'recChallenge1' });
     this.server.create('skill', {
@@ -38,6 +48,7 @@ module('Acceptance | Search', function (hooks) {
       id: 'recSkill1',
       name: '@skill1',
       challengeIds: ['recChallenge1', 'challengeChallenge1'],
+      challengesProductionIds: ['recChallenge1', 'challengeChallenge1'],
       pixId: 'recSkill1',
       version: 1,
     });
@@ -63,8 +74,39 @@ module('Acceptance | Search', function (hooks) {
                   name: skill.name,
                   prototypeId: prototype.id,
                   isPrototypeDeclinable: true,
-                  proposedChallengesCount: 1,
-                  validatedChallengesCount: 0,
+                  proposedChallengesCount: 0,
+                  validatedChallengesCount: 1,
+                },
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    this.server.create('competence-overview', {
+      id: `${competence.pixId}:challenges-production:en`,
+      thematicOverviews: [
+        {
+          id: 'pas de thématique',
+          name: "on m'a oublié :(",
+          tubeOverviews: [
+            {
+              id: tube.id,
+              name: tube.name,
+              skillOverviews: [
+                {
+                  id: skill.id,
+                  name: skill.name,
+                  prototypeId: prototype.id,
+                  isPrototypeDeclinable: true,
+                  proposedChallengesCount: 0,
+                  validatedChallengesCount: 1,
                 },
                 null,
                 null,
@@ -152,5 +194,57 @@ module('Acceptance | Search', function (hooks) {
 
     // then
     assert.strictEqual(currentURL(), expectedUrl);
+  });
+
+  module('when v2 is enabled', function (hooks) {
+    hooks.beforeEach(function () {
+      window.localStorage.setItem('v2', 'true');
+    });
+
+    test('search a challenge', async function (assert) {
+      // given
+      this.server.create('search-result', {
+        id: 'recChallenge1',
+        title: 'Le challenge avec test dans la consigne',
+        type: 'challenge',
+        status: 'périmé',
+        locale: 'fr',
+        'is-primary': true,
+      });
+      const expectedUrl =
+        '/v2/competences/recCompetence1.1/challenges-production/skills/recSkill1/challenges/recChallenge1';
+
+      // when
+      const screen = await visit('/');
+      await clickByText('Rechercher un acquis ou une épreuve...');
+      await fillByLabel('Rechercher...', 'test');
+      await click(await screen.findByRole('option', { name: '🔴 Le challenge avec test dans la consigne' }));
+
+      // then
+      assert.strictEqual(currentURL(), expectedUrl);
+    });
+
+    test('search a localized challenge', async function (assert) {
+      // given
+      this.server.create('search-result', {
+        id: 'challengeLocalizedChallenge1',
+        title: 'The challenge with ID challengeLocalizedChallenge1',
+        type: 'challenge',
+        status: 'validé',
+        locale: 'en',
+        'is-primary': false,
+      });
+      const expectedUrl =
+        '/v2/competences/recCompetence1.1/challenges-production/skills/recSkill1/localized-challenges/challengeLocalizedChallenge1?locale=en';
+
+      // when
+      const screen = await visit('/');
+      await clickByText('Rechercher un acquis ou une épreuve...');
+      await fillByLabel('Rechercher...', '  challengeLocalizedChallenge1  ');
+      await click(await screen.findByRole('option', { name: '🟢 The challenge with ID challengeLocalizedChallenge1' }));
+
+      // then
+      assert.strictEqual(currentURL(), expectedUrl);
+    });
   });
 });
