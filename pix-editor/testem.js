5e5fa97b1ed38d4bcd84d5b16a4ea90c03625b63
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-05-18 16:23:52 +0200

    tech(pix-editor): uses firefox for tests

--- pix-editor/testem.js
+++ pix-editor/testem.js
@@ -3,8 +3,8 @@ const config = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   reporter: 'dot',
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: ['Firefox'],
+  launch_in_dev: ['Firefox'],
   browser_start_timeout: 120,
   browser_args: {
     Chrome: {
@@ -20,6 +20,9 @@ const config = {
         '--remote-debugging-port=9222',
         '--window-size=1440,900',
       ].filter(Boolean),
+    },
+    Firefox: {
+      ci: ['--headless', '--remote-debugging-port=9222', '--window-size=1440,900'],
     },
   },
 };
