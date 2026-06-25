2d5e2ce098b8f84b5e7ffbe08f7ff18757399389
Eithliu <iris.benoit-martin@pix.fr>
2026-06-25 09:48:35 +0200

    Revert "feat(api): split links in several pages"
    This reverts commit ced2f15178f44f968566b75eb32fe87d95334998.

--- api/lib/application/external-urls/index.js
+++ api/lib/application/external-urls/index.js
@@ -1,6 +1,5 @@
-import Joi from 'joi';
 import * as securityPreHandlers from '../security-pre-handlers.js';
-import { getExternalLinks } from '../../domain/usecases/index.js';
+import { urlRepository } from '../../infrastructure/repositories/index.js';
 import * as externalUrlsSerializer from '../../infrastructure/serializers/html/external-urls-serializer.js';
 
 export async function register(server) {
@@ -10,10 +9,9 @@ export async function register(server) {
       path: '/api/external-urls',
       config: {
         auth: false,
-        validate: { query: Joi.object({ pageNumber: Joi.number() }) },
         pre: [{ method: securityPreHandlers.checkUserIsUrlBrokenLinksMonitor }],
         handler: async function(request, h) {
-          const externalUrls = await getExternalLinks();
+          const externalUrls = await urlRepository.get();
           return h.response(externalUrlsSerializer.serialize(externalUrls));
         },
       },
--- api/lib/domain/usecases/get-external-links.js
+++
@@ -1,5 +0,0 @@
-import { urlRepository } from '../../infrastructure/repositories/index.js';
-
-export function getExternalLinks() {
-  return urlRepository.get();
-}
