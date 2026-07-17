57d089b75bdcc081fa6c049a2064201050ac02a8
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-07-17 16:00:28 +0200

    feat(api): adds draftModuleVersionRepository.create

---
+++ api/tests/integration/infrastructure/repositories/draft-module-version-repository_test.js
@@ -0,0 +1,34 @@
+import { describe, expect, it } from 'vitest';
+import { databaseBuilder, domainBuilder, knex } from '../../../test-helper.js';
+import * as draftModuleVersionRepository from '../../../../lib/infrastructure/repositories/draft-module-version-repository.js';
+import { DraftModuleVersion } from '../../../../lib/domain/models/DraftModuleVersion.js';
+
+describe('Draft Module Version Repository', () => {
+  describe('create', () => {
+    it('create a draft module version in database', async () => {
+      // given
+      const draftModule = domainBuilder.buildDraftModule();
+      databaseBuilder.factory.buildDraftModule(draftModule);
+      await databaseBuilder.commit();
+
+      const draftModuleVersion = new DraftModuleVersion({
+        draftModuleId: draftModule.id,
+        version: draftModule.version,
+        structuredDiff: { test: 'ceci est un diff structuré de test...' },
+      });
+
+      // when
+      await draftModuleVersionRepository.create(draftModuleVersion);
+
+      // then
+      await expect(knex.select().from('draft-module-versions')).resolves.toStrictEqual([
+        {
+          id: expect.any(Number),
+          draftModuleId: draftModule.id,
+          version: draftModule.version,
+          structuredDiff: { test: 'ceci est un diff structuré de test...' },
+        },
+      ]);
+    });
+  });
+});
