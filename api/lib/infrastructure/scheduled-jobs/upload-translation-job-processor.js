c1f2fe64a26e0a6adeabcf32b550856ebddfbaa3
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-07-31 10:29:27 +0200

    refactor(api): exports usecases from index

--- api/lib/infrastructure/scheduled-jobs/upload-translation-job-processor.js
+++ api/lib/infrastructure/scheduled-jobs/upload-translation-job-processor.js
@@ -1,6 +1,5 @@
 import './job-process.js';
-import { uploadTranslationToPhrase } from '../../domain/usecases/index.js';
-import { uploadTranslationsToWeblate } from '../../domain/usecases/upload-translations-to-weblate.js';
+import { uploadTranslationsToWeblate, uploadTranslationToPhrase } from '../../domain/usecases/index.js';
 
 export default async function uploadTranslationJobProcessor() {
   await uploadTranslationToPhrase();
