bd5c3a0b5c725e899a1c7af45717fa98680e3b16
Eric Lim <eric.lim@zenika.com>
2026-08-20 14:56:08 +0200

    ⚡️(api): add update-module usecase

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -45,6 +45,7 @@ export * from './update-attachment.js';
 export * from './update-challenge.js';
 export * from './update-competence.js';
 export * from './update-draft-module.js';
+export * from './update-module.js';
 export * from './update-mission.js';
 export * from './update-skill.js';
 export * from './update-thematic.js';
