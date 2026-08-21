13a2637422b295b9b4fae906ee56688ac54de580
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2026-08-21 10:10:40 +0200

    clean: remove unused import

--- pix-editor/tests/acceptance/create-framework-test.js
+++ pix-editor/tests/acceptance/create-framework-test.js
@@ -1,5 +1,5 @@
 import { clickByName, clickByText, visit } from '@1024pix/ember-testing-library';
-import { click, currentURL, fillIn, find } from '@ember/test-helpers';
+import { click, currentURL, fillIn } from '@ember/test-helpers';
 import { authenticateSession } from 'ember-simple-auth/test-support';
 import { setupApplicationTest } from 'pixeditor/tests/setup-application-rendering';
 import { setupMirage } from 'pixeditor/tests/test-support/setup-mirage';
