c192b79916bd95700d4d09fda88304e10bb5456f
mcampourcy <mireille.campourcy@pix.fr>
2026-09-01 17:16:50 +0200

    feat(broken-url): add skills and localized challenges jsonapi relationships

--- api/lib/infrastructure/serializers/jsonapi/broken-url-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/broken-url-serializer.js
@@ -7,7 +7,18 @@ const serializer = new Serializer('broken-urls', {
     'url',
     'statusCode',
     'errorMessage',
+    'localizedChallenges',
+    'skills',
   ],
+  transform({ localizedChallengeIds, skillIds, ...brokenUrl }) {
+    return {
+      ...brokenUrl,
+      localizedChallenges: localizedChallengeIds.map((id) => ({ id })),
+      skills: skillIds.map((id) => ({ id })),
+    };
+  },
+  localizedChallenges: { ref: 'id' },
+  skills: { ref: 'id' },
 });
 
 export function serialize(brokenUrl) {
--- api/tests/integration/infrastructure/repositories/broken-url-repository_test.js
+++ api/tests/integration/infrastructure/repositories/broken-url-repository_test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { databaseBuilder, knex } from '../../../test-helper.js';
+import { databaseBuilder, domainBuilder, knex } from '../../../test-helper.js';
 import { saveNewlyBrokenUrlList, removeRepairedUrlList, deleteUnmentionedBrokenUrls, list } from '../../../../lib/infrastructure/repositories/broken-url-repository.js';
 import { BrokenUrl } from '../../../../lib/domain/readmodels/index.js';
 
@@ -152,6 +152,22 @@ describe('Integration | Repository | broken-url-repository', () => {
   describe('#list', () => {
     it('should retrieve broken url readmodels ordered by url', async () => {
       // given
+      const tutorial = databaseBuilder.factory.buildTutorial(domainBuilder.buildTutorialDatasourceObject({ tagIds: [] }));
+      const { challenge, skill } = databaseBuilder.factory.buildChallengeInGroup({ skill: { tutorialIds: [tutorial.id] } });
+      const localized2 = databaseBuilder.factory.buildLocalizedChallenge({ id: 'recLocalized2', challengeId: challenge.id, locale: 'nl-NL' });
+
+      databaseBuilder.factory.buildExternalUrl({
+        url: 'http://localhost:8080/',
+        localizedChallengeIds: [challenge.id],
+        tutorialIds: [],
+      });
+      databaseBuilder.factory.buildExternalUrl({
+        url: 'http://www.test.org',
+        localizedChallengeIds: [challenge.id, localized2.id],
+        tutorialIds: [],
+      });
+
+      databaseBuilder.factory.buildExternalUrl({ url: 'http://test.localhost:8080/', localizedChallengeIds: [], tutorialIds: [tutorial.id] });
       const notFoundUrl = databaseBuilder.factory.buildBrokenUrl({
         errorMessage: 'Not Found',
         statusCode: 404,
@@ -176,9 +192,21 @@ describe('Integration | Repository | broken-url-repository', () => {
       expect(brokenUrlList[0]).toBeInstanceOf(BrokenUrl);
 
       expect(brokenUrlList).toEqual([
-        notFoundUrl,
-        brokenUrl,
-        notAllowedUrl,
+        {
+          ...notFoundUrl,
+          localizedChallengeIds: [challenge.id],
+          skillIds: [],
+        },
+        {
+          ...brokenUrl,
+          skillIds: [skill.id],
+          localizedChallengeIds: [],
+        },
+        {
+          ...notAllowedUrl,
+          localizedChallengeIds: [challenge.id, localized2.id],
+          skillIds: [],
+        },
       ]);
     });
 
