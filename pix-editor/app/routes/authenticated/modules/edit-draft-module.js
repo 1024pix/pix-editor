79c256a4a1b7f154183c45ee05ee6432d0d59dc6
AndreiaPena <andreia.pena@pix.fr>
2026-09-09 15:27:02 +0200

    ⚡(pix-editor): do not allow user with readonly role to access
    draft module creation and modification pages
    
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>

--- pix-editor/app/routes/authenticated/modules/edit-draft-module.js
+++ pix-editor/app/routes/authenticated/modules/edit-draft-module.js
@@ -3,6 +3,14 @@ import { service } from '@ember/service';
 
 export default class EditDraftModuleRoute extends Route {
   @service store;
+  @service access;
+  @service router;
+
+  async beforeModel() {
+    if (!this.access.mayCreateOrEditModule()) {
+      this.router.transitionTo('authenticated.modules.production');
+    }
+  }
 
   async model(params) {
     const draftModule = await this.store.findRecord('draft-module', params.draft_module_id, { reload: true });
